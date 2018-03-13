859055f1fd00508ab4a04cb750f822faff26eb2c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:20:11 +0000

    First commited as before_4889_rev9

--- org.eclipse.jgit/src/org/eclipse/jgit/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/JGitText.java
@@ -483,7 +483,6 @@ public class JGitText extends TranslationBundle {
 	/***/ public String startingReadStageWithoutWrittenRequestDataPendingIsNotSupported;
 	/***/ public String statelessRPCRequiresOptionToBeEnabled;
 	/***/ public String submoduleExists;
-	/***/ public String submoduleInvalidGitdirRef;
 	/***/ public String submodulesNotSupported;
 	/***/ public String symlinkCannotBeWrittenAsTheLinkTarget;
 	/***/ public String systemConfigFileInvalid;
