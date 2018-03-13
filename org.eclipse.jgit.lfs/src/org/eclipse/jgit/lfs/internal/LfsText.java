4dd41db7f548cbfaa0d684343070de2079f535a4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:24:59 +0000

    First commited as before_88603_rev2

--- org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/internal/LfsText.java
+++ org.eclipse.jgit.lfs/src/org/eclipse/jgit/lfs/internal/LfsText.java
@@ -67,5 +67,4 @@ public class LfsText extends TranslationBundle {
 	/***/ public String repositoryNotFound;
 	/***/ public String repositoryReadOnly;
 	/***/ public String lfsUnavailable;
-	/***/ public String lfsUnathorized;
 }
