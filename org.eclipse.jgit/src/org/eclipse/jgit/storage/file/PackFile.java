64626724d010977eae0195ae2f51d32a4705eeac
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:34:43 +0000

    First commited as before_7938_rev7

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/PackFile.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/PackFile.java
@@ -1090,4 +1090,8 @@ public class PackFile implements Iterable<PackIndex.MutableEntry> {
 		String b = (dot < 0) ? p : p.substring(0, dot);
 		return new File(packFile.getParentFile(), b + '.' + ext.getExtension());
 	}
+
+	private boolean hasExt(PackExt ext) {
+		return (extensions & ext.getBit()) == ext.getBit();
+	}
 }
