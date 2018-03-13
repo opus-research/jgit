859a3e2d0f9f56b7a72cb6c5c16317385d64fdb2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:49:10 +0000

    First commited as before_12421_rev4

--- org.eclipse.jgit.archive/src/org/eclipse/jgit/archive/ZipFormat.java
+++ org.eclipse.jgit.archive/src/org/eclipse/jgit/archive/ZipFormat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013, Google Inc.
+ * Copyright (C) 2012 Google Inc.
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -40,47 +40,53 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.eclipse.jgit.archive;
 
-package org.eclipse.jgit.internal.storage.dfs;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-import java.io.ByteArrayOutputStream;
-import java.nio.ByteBuffer;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+import org.eclipse.jgit.api.ArchiveCommand;
+import org.eclipse.jgit.lib.FileMode;
+import org.eclipse.jgit.lib.ObjectLoader;
 
-class InMemoryOutputStream extends DfsOutputStream {
-	private final ByteArrayOutputStream dst = new ByteArrayOutputStream();
+/**
+ * PKWARE's ZIP format.
+ */
+public class ZipFormat implements ArchiveCommand.Format<ArchiveOutputStream> {
+	private static final List<String> SUFFIXES =
+			Collections.unmodifiableList(Arrays.asList(".zip"));
 
-	private byte[] data;
-
-	@Override
-	public void write(byte[] buf, int off, int len) {
-		data = null;
-		dst.write(buf, off, len);
+	public ArchiveOutputStream createArchiveOutputStream(OutputStream s) {
+		return new ZipArchiveOutputStream(s);
 	}
 
-	@Override
-	public int read(long position, ByteBuffer buf) {
-		byte[] d = getData();
-		int n = Math.min(buf.remaining(), d.length - (int) position);
-		if (n <= 0)
-			return -1;
-		buf.put(d, (int) position, n);
-		return n;
+	public void putEntry(ArchiveOutputStream out,
+			String path, FileMode mode, ObjectLoader loader)
+			throws IOException {
+		final ZipArchiveEntry entry = new ZipArchiveEntry(path);
+
+		if (mode == FileMode.REGULAR_FILE) {
+			// ok
+		} else if (mode == FileMode.EXECUTABLE_FILE
+				|| mode == FileMode.SYMLINK) {
+			entry.setUnixMode(mode.getBits());
+		} else {
+			// TODO(jrn): Let the caller know the tree contained
+			// an entry with unsupported mode (e.g., a submodule).
+		}
+		entry.setSize(loader.getSize());
+		out.putArchiveEntry(entry);
+		loader.copyTo(out);
+		out.closeArchiveEntry();
 	}
 
-	byte[] getData() {
-		if (data == null)
-			data = dst.toByteArray();
-		return data;
+	public Iterable<String> suffixes() {
+		return SUFFIXES;
 	}
-
-	@Override
-	public void flush() {
-		// Default implementation does nothing;
-	}
-
-	@Override
-	public void close() {
-		flush();
-	}
-
 }
