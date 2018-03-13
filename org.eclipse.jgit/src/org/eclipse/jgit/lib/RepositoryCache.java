230ea9e1a469619d8d0497e41d95b93e6843f57f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:05:37 +0000

    First commited as before_74287_rev14

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/RepositoryCache.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/RepositoryCache.java
@@ -52,8 +52,6 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jgit.annotations.NonNull;
@@ -215,17 +213,9 @@ public class RepositoryCache {
 		cache.clearAllExpired();
 	}
 
-	static void reconfigure(RepositoryCacheConfig repositoryCacheConfig) {
-		cache.configureEviction(repositoryCacheConfig);
-	}
-
 	private final ConcurrentHashMap<Key, Reference<Repository>> cacheMap;
 
 	private final Lock[] openLocks;
-
-	private ScheduledFuture<?> cleanupTask;
-
-	private volatile long expireAfter;
 
 	private RepositoryCache() {
 		cacheMap = new ConcurrentHashMap<Key, Reference<Repository>>();
@@ -233,29 +223,20 @@ public class RepositoryCache {
 		for (int i = 0; i < openLocks.length; i++) {
 			openLocks[i] = new Lock();
 		}
-		configureEviction(new RepositoryCacheConfig());
-	}
 
-	private void configureEviction(
-			RepositoryCacheConfig repositoryCacheConfig) {
-		expireAfter = repositoryCacheConfig.getExpireAfter();
-		ScheduledThreadPoolExecutor scheduler = WorkQueue.getExecutor();
-		synchronized (scheduler) {
-			if (cleanupTask != null) {
-				cleanupTask.cancel(false);
-			}
-			long delay = repositoryCacheConfig.getCleanupDelay();
-			cleanupTask = scheduler.scheduleWithFixedDelay(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						cache.clearAllExpired();
-					} catch (Throwable e) {
-						LOG.error(e.getMessage(), e);
-					}
+		Runnable terminator = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					cache.clearAllExpired();
+				} catch (Throwable e) {
+					LOG.error(e.getMessage(), e);
 				}
-			}, delay, delay, TimeUnit.MILLISECONDS);
-		}
+			}
+		};
+
+		WorkQueue.getExecutor().scheduleWithFixedDelay(terminator, 10, 10,
+				TimeUnit.SECONDS);
 	}
 
 	@SuppressWarnings("resource")
@@ -315,7 +296,7 @@ public class RepositoryCache {
 			Repository db = ref.get();
 			if (db != null && db.useCnt.get() == 0
 					&& (System.currentTimeMillis()
-							- db.closedAt.get() > expireAfter)) {
+							- db.closedAt.get() > 20000)) {
 				RepositoryCache.close(db);
 			}
 		}
