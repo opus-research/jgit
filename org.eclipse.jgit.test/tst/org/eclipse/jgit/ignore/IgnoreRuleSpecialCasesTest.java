953fb25ba7b5aa8b16bfb7f69ac8e71549faccab
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:36:08 +0000

    First commited as before_51889_rev4

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/ignore/IgnoreRuleSpecialCasesTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/ignore/IgnoreRuleSpecialCasesTest.java
@@ -836,12 +836,6 @@ public class IgnoreRuleSpecialCasesTest {
 	}
 
 	@Test
-	public void testEscapedTrailingSpaces() throws Exception {
-		assertMatch("\\ ", " ", true);
-		assertMatch("a\\ ", "a ", true);
-	}
-
-	@Test
 	public void testNotEscapingBackslash() throws Exception {
 		assertMatch("\\out", "\\out", true);
 		assertMatch("\\out", "a/\\out", true);
