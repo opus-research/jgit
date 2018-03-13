cc931d4cb84182aa5d37b9322b566072aa87a935
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:06:57 +0000

    First commited as before_2531_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/api/CloneCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CloneCommand.java
@@ -139,9 +139,9 @@ public class CloneCommand implements Callable<Git> {
 		config.update(repo.getConfig());
 
 		repo.getConfig().setString(ConfigConstants.CONFIG_BRANCH_SECTION,
-				branch, ConfigConstants.CONFIG_KEY_REMOTE, remote);
+				branch, ConfigConstants.CONFIG_REMOTE_SECTION, remote);
 		repo.getConfig().setString(ConfigConstants.CONFIG_BRANCH_SECTION,
-				branch, ConfigConstants.CONFIG_KEY_MERGE, branch);
+				branch, ConfigConstants.CONFIG_KEY_MERGE, branch); //$NON-NLS-1$ //$NON-NLS-2$
 
 		repo.getConfig().save();
 
