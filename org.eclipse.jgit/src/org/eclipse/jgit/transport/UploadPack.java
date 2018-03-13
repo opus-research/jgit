c95b53346b5d05b9b7c481a52872b497885f3c00
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:25:28 +0000

    First commited as before_45709_rev6

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
@@ -810,9 +810,9 @@ public class UploadPack {
 			adv.advertiseCapability(OPTION_ALLOW_TIP_SHA1_IN_WANT);
 		adv.advertiseCapability(OPTION_AGENT, UserAgent.get());
 		adv.setDerefTags(true);
-		Map<String, Ref> advertisedOrDefaultRefs = getAdvertisedOrDefaultRefs();
-		findSymrefs(adv, advertisedOrDefaultRefs);
-		advertised = adv.send(advertisedOrDefaultRefs);
+		Map<String, Ref> refs = getAdvertisedOrDefaultRefs();
+		findSymrefs(adv, refs);
+		advertised = adv.send(refs);
 		if (adv.isEmpty())
 			adv.advertiseId(ObjectId.zeroId(), "capabilities^{}"); //$NON-NLS-1$
 		adv.end();
@@ -1467,7 +1467,7 @@ public class UploadPack {
 			pckOut.end();
 	}
 
-	private static void findSymrefs(
+	private void findSymrefs(
 			final RefAdvertiser adv, final Map<String, Ref> refs) {
 		Ref head = refs.get(Constants.HEAD);
 		if (head != null && head.isSymbolic()) {
