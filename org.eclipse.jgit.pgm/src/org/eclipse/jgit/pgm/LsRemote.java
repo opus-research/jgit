87c7c45a0fd4b42f7ef1a7902eee10bee9ee246e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:18:47 +0000

    First commited as before_37384_rev3

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/LsRemote.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/LsRemote.java
@@ -61,9 +61,6 @@ class LsRemote extends TextBuiltin {
 	@Option(name = "--heads", usage = "usage_lsRemoteHeads")
 	private boolean heads;
 
-	@Option(name = "--tags", usage = "usage_lsRemoteTags", aliases = { "-t" })
-	private boolean tags;
-
 	@Option(name = "--timeout", metaVar = "metaVar_service", usage = "usage_abortConnectionIfNoActivity")
 	int timeout = -1;
 
@@ -73,7 +70,7 @@ class LsRemote extends TextBuiltin {
 	@Override
 	protected void run() throws Exception {
 		LsRemoteCommand command = Git.lsRemoteRepository().setRemote(remote)
-				.setTimeout(timeout).setHeads(heads).setTags(tags);
+				.setTimeout(timeout).setHeads(heads);
 		TreeSet<Ref> refs = new TreeSet<Ref>(new Comparator<Ref>() {
 
 			public int compare(Ref r1, Ref r2) {
