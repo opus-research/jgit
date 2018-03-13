319c6a2a48ef232f49786c6944ac8675fd6a7da4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:42:40 +0000

    First commited as before_58100_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -811,25 +811,9 @@ public abstract class Repository implements AutoCloseable {
 
 	/** Decrement the use count, and maybe close resources. */
 	public void close() {
-		for (;;) {
-			int v = useCnt.intValue();
-			if (v == 0) {
-				break;
-			} else if (useCnt.compareAndSet(v, v - 1)) {
-				if (v == 1) {
-					doClose();
-				}
-				break;
-			}
+		if (useCnt.decrementAndGet() == 0) {
+			doClose();
 		}
-	}
-
-	/**
-	 * @return true if this repository is closed
-	 * @since 4.2
-	 */
-	public boolean isClosed() {
-		return useCnt.get() == 0;
 	}
 
 	/**
