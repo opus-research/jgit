6e7d33ea71dc147f2c1fefebc012eeb0a721f521
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:31:29 +0000

    First commited as before_49581_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -727,7 +727,7 @@ public abstract class Repository implements AutoCloseable {
 			return ObjectId.fromString(revstr);
 
 		if (Repository.isValidRefName("x/" + revstr)) { //$NON-NLS-1$
-			Ref r = getRefDatabase().findRef(revstr);
+			Ref r = getRefDatabase().getRef(revstr);
 			if (r != null)
 				return r.getObjectId();
 		}
@@ -907,7 +907,7 @@ public abstract class Repository implements AutoCloseable {
 	 * @throws IOException
 	 */
 	public Ref getRef(final String name) throws IOException {
-		return getRefDatabase().findRef(name);
+		return getRefDatabase().getRef(name);
 	}
 
 	/**
