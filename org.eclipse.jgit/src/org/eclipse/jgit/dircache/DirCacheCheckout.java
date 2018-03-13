8fa56ae9bff5ee3e3d6835e6609a9c56eb4f35ab
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:31:54 +0000

    First commited as before_7115_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheCheckout.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheCheckout.java
@@ -962,6 +962,7 @@ public class DirCacheCheckout {
 			DirCacheEntry entry, ObjectReader or) throws IOException {
 		ObjectLoader ol = or.open(entry.getObjectId());
 		File parentDir = f.getParentFile();
+		parentDir.mkdirs();
 		File tmpFile = File.createTempFile("._" + f.getName(), null, parentDir);
 		WorkingTreeOptions opt = repo.getConfig().get(WorkingTreeOptions.KEY);
 		FileOutputStream rawChannel = new FileOutputStream(tmpFile);
@@ -1023,7 +1024,7 @@ public class DirCacheCheckout {
 	private static boolean isValidPathSegment(CanonicalTreeParser t) {
 		String osName = SystemReader.getInstance().getProperty("os.name");
 		boolean isWindows = "Windows".equals(osName);
-		boolean isOSX = SystemReader.getInstance().isMacOS();
+		boolean isOSX = "Darwin".equals(osName) || "Mac OS X".equals(osName);
 		boolean ignCase = isOSX || isWindows;
 
 		int ptr = t.getNameOffset();
