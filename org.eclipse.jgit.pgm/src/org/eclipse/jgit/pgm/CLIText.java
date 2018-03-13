f347542627c688ed492a163a99ddf28b54b5a79f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:37:25 +0000

    First commited as before_8708_rev1

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/CLIText.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/CLIText.java
@@ -67,7 +67,6 @@ public class CLIText extends TranslationBundle {
 	 * @param line
 	 *            the line to format
 	 * @return the formatted line
-	 * @since 2.2
 	 */
 	public static String formatLine(String line) {
 		return MessageFormat.format(get().lineFormat, line);
@@ -87,7 +86,6 @@ public class CLIText extends TranslationBundle {
 	/***/ public String configFileNotFound;
 	/***/ public String cannotBeRenamed;
 	/***/ public String cannotChekoutNoHeadsAdvertisedByRemote;
-	/***/ public String cannotCombineSquashWithNoff;
 	/***/ public String cannotCreateCommand;
 	/***/ public String cannotCreateOutputStream;
 	/***/ public String cannotDeatchHEAD;
@@ -122,9 +120,9 @@ public class CLIText extends TranslationBundle {
 	/***/ public String failedToLockIndex;
 	/***/ public String failedToLockTag;
 	/***/ public String fatalError;
+	/***/ public String fatalErrorTagExists;
 	/***/ public String fatalThisProgramWillDestroyTheRepository;
 	/***/ public String fileIsRequired;
-	/***/ public String ffNotPossibleAborting;
 	/***/ public String forcedUpdate;
 	/***/ public String fromURI;
 	/***/ public String initializedEmptyGitRepositoryIn;
@@ -212,9 +210,12 @@ public class CLIText extends TranslationBundle {
 	/***/ public String statusModified;
 	/***/ public String statusNewFile;
 	/***/ public String statusRemoved;
-	/***/ public String switchedToNewBranch;
-	/***/ public String switchedToBranch;
-	/***/ public String tagAlreadyExists;
+
+	/***/
+	public String switchedToNewBranch;
+
+	/***/
+	public String switchedToBranch;
 	/***/ public String tagLabel;
 	/***/ public String taggerInfo;
 	/***/ public String timeInMilliSeconds;
@@ -224,6 +225,5 @@ public class CLIText extends TranslationBundle {
 	/***/ public String unmergedPaths;
 	/***/ public String unsupportedOperation;
 	/***/ public String untrackedFiles;
-	/***/ public String updating;
 	/***/ public String warningNoCommitGivenOnCommandLine;
 }
--- org.eclipse.jgit/src/org/eclipse/jgit/api/MergeCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/MergeCommand.java
@@ -99,30 +99,6 @@ public class MergeCommand extends GitCommand<MergeResult> {
 
 	private boolean squash;
 
-	private FastForwardMode fastForwardMode = FastForwardMode.FF;
-
-	/**
-	 * The modes available for fast forward merges (corresponding to the --ff,
-	 * --no-ff and --ff-only options).
-	 */
-	public enum FastForwardMode {
-		/**
-		 * Corresponds to the default --ff option (for a fast forward update the
-		 * branch pointer only).
-		 */
-		FF,
-		/**
-		 * Corresponds to the --no-ff option (create a merge commit even for a
-		 * fast forward).
-		 */
-		NO_FF,
-		/**
-		 * Corresponds to the --ff-only option (abort unless the merge is a fast
-		 * forward).
-		 */
-		FF_ONLY;
-	}
-
 	/**
 	 * @param repo
 	 */
@@ -142,7 +118,14 @@ public class MergeCommand extends GitCommand<MergeResult> {
 			ConcurrentRefUpdateException, CheckoutConflictException,
 			InvalidMergeHeadsException, WrongRepositoryStateException, NoMessageException {
 		checkCallable();
-		checkParameters();
+
+		if (commits.size() != 1)
+			throw new InvalidMergeHeadsException(
+					commits.isEmpty() ? JGitText.get().noMergeHeadSpecified
+							: MessageFormat.format(
+									JGitText.get().mergeStrategyDoesNotSupportHeads,
+									mergeStrategy.getName(),
+									Integer.valueOf(commits.size())));
 
 		RevWalk revWalk = null;
 		DirCacheCheckout dco = null;
@@ -196,8 +179,7 @@ public class MergeCommand extends GitCommand<MergeResult> {
 				return new MergeResult(headCommit, srcCommit, new ObjectId[] {
 						headCommit, srcCommit },
 						MergeStatus.ALREADY_UP_TO_DATE, mergeStrategy, null, null);
-			} else if (revWalk.isMergedInto(headCommit, srcCommit)
-					&& fastForwardMode == FastForwardMode.FF) {
+			} else if (revWalk.isMergedInto(headCommit, srcCommit)) {
 				// FAST_FORWARD detected: skip doing a real merge but only
 				// update HEAD
 				refLogMessage.append(": " + MergeStatus.FAST_FORWARD);
@@ -228,11 +210,6 @@ public class MergeCommand extends GitCommand<MergeResult> {
 						headCommit, srcCommit }, mergeStatus, mergeStrategy,
 						null, msg);
 			} else {
-				if (fastForwardMode == FastForwardMode.FF_ONLY) {
-					return new MergeResult(headCommit, srcCommit,
-							new ObjectId[] { headCommit, srcCommit },
-							MergeStatus.ABORTED, mergeStrategy, null, null);
-				}
 				String mergeMessage = "";
 				if (!squash) {
 					mergeMessage = new MergeMessageFormatter().format(
@@ -264,10 +241,7 @@ public class MergeCommand extends GitCommand<MergeResult> {
 				} else
 					noProblems = merger.merge(headCommit, srcCommit);
 				refLogMessage.append(": Merge made by ");
-				if (!revWalk.isMergedInto(headCommit, srcCommit))
-					refLogMessage.append(mergeStrategy.getName());
-				else
-					refLogMessage.append("recursive");
+				refLogMessage.append(mergeStrategy.getName());
 				refLogMessage.append('.');
 				if (noProblems) {
 					dco = new DirCacheCheckout(repo,
@@ -329,21 +303,6 @@ public class MergeCommand extends GitCommand<MergeResult> {
 			if (revWalk != null)
 				revWalk.release();
 		}
-	}
-
-	private void checkParameters() throws InvalidMergeHeadsException {
-		if (squash && fastForwardMode == FastForwardMode.NO_FF) {
-			throw new JGitInternalException(
-					JGitText.get().cannotCombineSquashWithNoff);
-		}
-
-		if (commits.size() != 1)
-			throw new InvalidMergeHeadsException(
-					commits.isEmpty() ? JGitText.get().noMergeHeadSpecified
-							: MessageFormat.format(
-									JGitText.get().mergeStrategyDoesNotSupportHeads,
-									mergeStrategy.getName(),
-									Integer.valueOf(commits.size())));
 	}
 
 	private void updateHead(StringBuilder refLogMessage, ObjectId newHeadId,
@@ -431,21 +390,6 @@ public class MergeCommand extends GitCommand<MergeResult> {
 	public MergeCommand setSquash(boolean squash) {
 		checkCallable();
 		this.squash = squash;
-		return this;
-	}
-
-	/**
-	 * Sets the fast forward mode.
-	 *
-	 * @param fastForwardMode
-	 *            corresponds to the --ff/--no-ff/--ff-only options. --ff is the
-	 *            default option.
-	 * @return {@code this}
-	 * @since 2.2
-	 */
-	public MergeCommand setFastForward(FastForwardMode fastForwardMode) {
-		checkCallable();
-		this.fastForwardMode = fastForwardMode;
 		return this;
 	}
 }
