d20d976a1a68513ae3a1beed43e21f56415cb5cd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:12:08 +0000

    First commited as before_30128_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/merge/RecursiveMerger.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/merge/RecursiveMerger.java
@@ -212,12 +212,6 @@ public class RecursiveMerger extends ResolveMerger {
 			inCore = oldIncore;
 			dircache = oldDircache;
 			workingTreeIterator = oldWTreeIt;
-			toBeCheckedOut.clear();
-			toBeDeleted.clear();
-			modifiedFiles.clear();
-			unmergedPaths.clear();
-			mergeResults.clear();
-			failingPaths.clear();
 		}
 		return currentBase;
 	}
