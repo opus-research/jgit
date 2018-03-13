bbdf966e8a321fb4b5602ab0a62c023420bbb2c6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:02:45 +0000

    First commited as before_21135_rev2

--- org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FS_Win32_Java7Cygwin.java
+++ org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FS_Win32_Java7Cygwin.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2012, Robin Rosenberg <robin.rosenberg@dewire.com>
- * Copyright (C) 2014, Obeo.
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -47,8 +46,6 @@ package org.eclipse.jgit.util;
 import java.io.File;
 import java.io.IOException;
 
-import org.eclipse.jgit.api.errors.JGitInternalException;
-
 /**
  * FS for Java7 on Windows with Cygwin
  */
@@ -67,25 +64,13 @@ public class FS_Win32_Java7Cygwin extends FS_Win32_Cygwin {
 	}
 
 	@Override
-	public boolean supportsExecute() {
+	public boolean supportsSymlinks() {
 		return true;
 	}
 
 	@Override
-	public boolean canExecute(File f) {
-		try {
-			if (isSymLink(f)) {
-				return false;
-			}
-		} catch (IOException e) {
-			throw new JGitInternalException(e.getMessage(), e);
-		}
-		return FileUtil.canExecute(f);
-	}
-
-	@Override
-	public boolean setExecute(File f, boolean canExecute) {
-		return FileUtil.setExecute(f, canExecute);
+	public boolean isSymLink(File path) throws IOException {
+		return FileUtil.isSymlink(path);
 	}
 
 	@Override
@@ -101,6 +86,11 @@ public class FS_Win32_Java7Cygwin extends FS_Win32_Cygwin {
 	@Override
 	public void delete(File path) throws IOException {
 		FileUtil.delete(path);
+	}
+
+	@Override
+	public long length(File f) throws IOException {
+		return FileUtil.getLength(f);
 	}
 
 	@Override
@@ -126,6 +116,16 @@ public class FS_Win32_Java7Cygwin extends FS_Win32_Cygwin {
 	@Override
 	public void setHidden(File path, boolean hidden) throws IOException {
 		FileUtil.setHidden(path, hidden);
+	}
+
+	@Override
+	public String readSymLink(File path) throws IOException {
+		return FileUtil.readSymlink(path);
+	}
+
+	@Override
+	public void createSymLink(File path, String target) throws IOException {
+		FileUtil.createSymLink(path, target);
 	}
 
 	@Override
