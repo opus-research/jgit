951181207c5fb1b6816603f047e72d708f23249b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:01:25 +0000

    First commited as before_1815_rev6

--- org.eclipse.jgit/src/org/eclipse/jgit/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/JGitText.java
@@ -69,7 +69,6 @@ public class JGitText extends TranslationBundle {
 	/***/ public String aNewObjectIdIsRequired;
 	/***/ public String abbreviationLengthMustBeNonNegative;
 	/***/ public String abortingRebase;
-	/***/ public String accessFileDenied;
 	/***/ public String advertisementCameBefore;
 	/***/ public String advertisementOfCameBefore;
 	/***/ public String amazonS3ActionFailed;
@@ -313,7 +312,6 @@ public class JGitText extends TranslationBundle {
 	/***/ public String lockError;
 	/***/ public String lockOnNotClosed;
 	/***/ public String lockOnNotHeld;
-	/***/ public String lstatImplIllegalResult;
 	/***/ public String malformedpersonIdentString;
 	/***/ public String mergeStrategyAlreadyExistsAsDefault;
 	/***/ public String mergeStrategyDoesNotSupportHeads;
@@ -338,7 +336,6 @@ public class JGitText extends TranslationBundle {
 	/***/ public String noHEADExistsAndNoExplicitStartingRevisionWasSpecified;
 	/***/ public String noHMACsupport;
 	/***/ public String noMergeHeadSpecified;
-	/***/ public String noSuchFile;
 	/***/ public String noSuchRef;
 	/***/ public String noXMLParserAvailable;
 	/***/ public String notABoolean;
