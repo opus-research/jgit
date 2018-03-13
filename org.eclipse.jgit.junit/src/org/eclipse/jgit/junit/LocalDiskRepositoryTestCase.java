ad7971aa68a68d66dcde0b332552fd42d7500fe0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:00:45 +0000

    First commited as before_1672_rev3

--- org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/LocalDiskRepositoryTestCase.java
+++ org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/LocalDiskRepositoryTestCase.java
@@ -114,17 +114,23 @@ public abstract class LocalDiskRepositoryTestCase extends TestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 
-		if (shutdownHook == null) {
-			shutdownHook = new Thread() {
-				@Override
-				public void run() {
-					System.gc();
-					recursiveDelete("SHUTDOWN", trash, false, false);
-				}
-			};
-			Runtime.getRuntime().addShutdownHook(shutdownHook);
+		synchronized(this) {
+			if (shutdownHook == null) {
+				shutdownHook = new Thread() {
+					@Override
+					public void run() {
+						// On windows accidentally open files or memory
+						// mapped regions may prevent files from being deleted.
+						// Suggesting a GC increases the likelihood that our
+						// test repositories actually get removed after the
+						// tests, even in the case of failure.
+						System.gc();
+						recursiveDelete("SHUTDOWN", trash, false, false);
+					}
+				};
+				Runtime.getRuntime().addShutdownHook(shutdownHook);
+			}
 		}
-
 		recursiveDelete(testName(), trash, true, false);
 
 		mockSystemReader = new MockSystemReader();
