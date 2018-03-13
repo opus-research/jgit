69dc0b560f59d6c0ad8e53979be6b9c201c756f9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:24:29 +0000

    First commited as before_45649_rev5

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/ObjectDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/ObjectDirectory.java
@@ -114,8 +114,6 @@ public class ObjectDirectory extends FileObjectDatabase {
 	/** Maximum number of candidates offered as resolutions of abbreviation. */
 	private static final int RESOLVE_ABBREV_LIMIT = 256;
 
-	private static final String STALE_FILE_HANDLE_MSG = "stale file handle"; //$NON-NLS-1$
-
 	private final Config config;
 
 	private final File objects;
@@ -565,14 +563,10 @@ public class ObjectDirectory extends FileObjectDatabase {
 		} else if (e instanceof FileNotFoundException) {
 			warnTmpl = JGitText.get().packWasDeleted;
 			removePack(p);
-		} else if (e.getMessage() != null
-				&& e.getMessage().toLowerCase().contains(STALE_FILE_HANDLE_MSG)) {
-			warnTmpl = JGitText.get().packHandleIsStale;
-			removePack(p);
 		}
 		if (warnTmpl != null) {
 			if (LOG.isDebugEnabled()) {
-				LOG.warn(MessageFormat.format(warnTmpl,
+				LOG.debug(MessageFormat.format(warnTmpl,
 						p.getPackFile().getAbsolutePath()), e);
 			} else {
 				LOG.warn(MessageFormat.format(warnTmpl,
