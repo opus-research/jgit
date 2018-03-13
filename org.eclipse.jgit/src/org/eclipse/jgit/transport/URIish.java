f4245e23460ad14fa850efe65f04659be72c0958
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:09:08 +0000

    First commited as before_3198_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/URIish.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/URIish.java
@@ -551,9 +551,7 @@ public class URIish implements Serializable {
 	public String getHumanishName() throws IllegalArgumentException {
 		if ("".equals(getPath()) || getPath() == null)
 			throw new IllegalArgumentException();
-		String s = getPath();
-		s = s.replace('\\', '/');
-		String[] elements = s.split("/");
+		String[] elements = getPath().split("/");
 		if (elements.length == 0)
 			throw new IllegalArgumentException();
 		String result = elements[elements.length - 1];
