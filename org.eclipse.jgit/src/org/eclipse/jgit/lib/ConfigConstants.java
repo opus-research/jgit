56dba0bd355bd5e598ec716bd4deaac8d0413e3e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:04:01 +0000

    First commited as before_22160_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ConfigConstants.java
@@ -90,13 +90,6 @@ public class ConfigConstants {
 	/** The "pack" section */
 	public static final String CONFIG_PACK_SECTION = "pack";
 
-	/**
-	 * The "fetch" section
-	 *
-	 * @since 3.3
-	 */
-	public static final String CONFIG_FETCH_SECTION = "fetch";
-
 	/** The "algorithm" key */
 	public static final String CONFIG_KEY_ALGORITHM = "algorithm";
 
@@ -207,6 +200,13 @@ public class ConfigConstants {
 	/** The "indexversion" key */
 	public static final String CONFIG_KEY_INDEXVERSION = "indexversion";
 
+	/**
+	 * The "hidedotfiles" key
+	 *
+	 * @since 3.3
+	 */
+	public static final String CONFIG_KEY_HIDEDOTFILES = "hidedotfiles";
+
 	/** The "precomposeunicode" key */
 	public static final String CONFIG_KEY_PRECOMPOSEUNICODE = "precomposeunicode";
 
@@ -254,11 +254,4 @@ public class ConfigConstants {
          * @since 3.0
          */
 	public static final String CONFIG_KEY_RENAMES = "renames";
-
-	/**
-	 * The "prune" key
-	 *
-	 * @since 3.3
-	 */
-	public static final String CONFIG_KEY_PRUNE = "prune";
 }
