ce1e1e0dde215a6f0b5d6071fafd8122789447c3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:02:07 +0000

    First commited as before_71652_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/merge/ResolveMerger.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/merge/ResolveMerger.java
@@ -1005,14 +1005,13 @@ public class ResolveMerger extends ThreeWayMerger {
 		builder = dircache.builder();
 		DirCacheBuildIterator buildIt = new DirCacheBuildIterator(builder);
 
-		tw = new NameConflictTreeWalk(db, reader);
+		tw = new NameConflictTreeWalk(reader);
 		tw.addTree(baseTree);
 		tw.addTree(headTree);
 		tw.addTree(mergeTree);
-		int dciPos = tw.addTree(buildIt);
+		tw.addTree(buildIt);
 		if (workingTreeIterator != null) {
 			tw.addTree(workingTreeIterator);
-			workingTreeIterator.setDirCacheIterator(tw, dciPos);
 		} else {
 			tw.setFilter(TreeFilter.ANY_DIFF);
 		}
