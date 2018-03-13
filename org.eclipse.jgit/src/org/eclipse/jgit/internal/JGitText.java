f37fd99c530cc0bfd2581c760ece848eec3a592a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:30:16 +0000

    First commited as before_6543_rev3

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -501,8 +501,6 @@ public class JGitText extends TranslationBundle {
 	/***/ public String submoduleExists;
 	/***/ public String submodulesNotSupported;
 	/***/ public String submoduleParentRemoteUrlInvalid;
-	/***/ public String subscribeNewUpdate;
-	/***/ public String subscribeStart;
 	/***/ public String symlinkCannotBeWrittenAsTheLinkTarget;
 	/***/ public String systemConfigFileInvalid;
 	/***/ public String tagNameInvalid;
