b46789e348370d076a5bdb8430618e76a01f5473
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:56:09 +0000

    First commited as before_64678_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftree/RefTree.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftree/RefTree.java
@@ -43,6 +43,7 @@
 
 package org.eclipse.jgit.internal.storage.reftree;
 
+import static org.eclipse.jgit.lib.Constants.HEAD;
 import static org.eclipse.jgit.lib.Constants.OBJ_BLOB;
 import static org.eclipse.jgit.lib.Constants.R_REFS;
 import static org.eclipse.jgit.lib.Constants.encode;
@@ -80,6 +81,7 @@ import org.eclipse.jgit.lib.ObjectIdRef;
 import org.eclipse.jgit.lib.ObjectInserter;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.SymbolicRef;
 import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.util.RawParseUtils;
@@ -98,15 +100,15 @@ import org.eclipse.jgit.util.RawParseUtils;
  * blob storing the name of the target reference.
  * <p>
  * Annotated tags also store the peeled object using a {@code GITLINK} entry
- * with the suffix <code>"^{}"</code>, for example {@code "tags/v1.0"} stores
- * the annotated tag object, while <code>"tags/v1.0^{}"</code> stores the commit
- * the tag annotates.
+ * with the suffix <code>" ^"</code> (space carrot), for example
+ * {@code "tags/v1.0"} stores the annotated tag object, while
+ * <code>"tags/v1.0 ^"</code> stores the commit the tag annotates.
  * <p>
  * {@code HEAD} is a special case and stored as {@code "..HEAD"}.
  */
 public class RefTree {
 	/** Suffix applied to GITLINK to indicate its the peeled value of a tag. */
-	public static final String PEELED_SUFFIX = "^{}"; //$NON-NLS-1$
+	public static final String PEELED_SUFFIX = " ^"; //$NON-NLS-1$
 	static final String ROOT_DOTDOT = ".."; //$NON-NLS-1$
 
 	/**
@@ -243,6 +245,11 @@ public class RefTree {
 		try {
 			DirCacheEditor ed = contents.editor();
 			for (Command cmd : cmdList) {
+				if (!isValidRef(cmd)) {
+					cmd.setResult(REJECTED_OTHER_REASON,
+							JGitText.get().funnyRefname);
+					return abort(cmdList);
+				}
 				apply(ed, cmd);
 			}
 			ed.finish();
@@ -261,6 +268,11 @@ public class RefTree {
 		} catch (LockFailureException e) {
 			return abort(cmdList);
 		}
+	}
+
+	private static boolean isValidRef(Command cmd) {
+		String n = cmd.getRefName();
+		return HEAD.equals(n) || Repository.isValidRefName(n);
 	}
 
 	private void apply(DirCacheEditor ed, final Command cmd) {
@@ -358,7 +370,7 @@ public class RefTree {
 		return R_REFS + path;
 	}
 
-	private static String refPath(String name) {
+	static String refPath(String name) {
 		if (name.startsWith(R_REFS)) {
 			return name.substring(R_REFS.length());
 		}
