221e7e5dad1408e96a724d6d39791cea3586b165
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:48:49 +0000

    First commited as before_101358_rev4

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackedBatchRefUpdate.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/file/PackedBatchRefUpdate.java
@@ -316,6 +316,7 @@ class PackedBatchRefUpdate extends BatchRefUpdate {
 				ReceiveCommand currAdd = adds.get(addIdx);
 				if (currAdd.getRefName().compareTo(name) < 0) {
 					b.add(peeledRef(walk, currAdd));
+					byName.remove(currAdd.getRefName());
 				} else {
 					break;
 				}
