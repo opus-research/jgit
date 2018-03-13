eb029eda1e11464f3bd91ab8e4a145105af531eb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:26:54 +0000

    First commited as before_5889_rev8

--- org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheEntry.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheEntry.java
@@ -141,8 +141,8 @@ public class DirCacheEntry {
 	private byte inCoreFlags;
 
 	DirCacheEntry(final byte[] sharedInfo, final MutableInteger infoAt,
-			final InputStream in, final MessageDigest md, int smudge_s,
-			int smudge_ns) throws IOException {
+			final InputStream in, final MessageDigest md, final int smudge_s,
+			final int smudge_ns) throws IOException {
 		info = sharedInfo;
 		infoOffset = infoAt.value;
 
