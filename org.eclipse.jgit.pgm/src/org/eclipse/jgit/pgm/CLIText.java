755c9e6e5533073674ce294c2cc1ba9208b7f13f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:37:19 +0000

    First commited as before_8632_rev3

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/CLIText.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/CLIText.java
@@ -209,13 +209,8 @@ public class CLIText extends TranslationBundle {
 	/***/ public String statusModified;
 	/***/ public String statusNewFile;
 	/***/ public String statusRemoved;
-
-	/***/
-	public String switchedToNewBranch;
-
-	/***/
-	public String switchedToBranch;
-	/***/ public String tagAlreadyExists;
+	/***/ public String switchedToNewBranch;
+	/***/ public String switchedToBranch;
 	/***/ public String tagLabel;
 	/***/ public String taggerInfo;
 	/***/ public String timeInMilliSeconds;
