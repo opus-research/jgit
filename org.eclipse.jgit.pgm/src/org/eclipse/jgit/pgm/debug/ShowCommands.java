81db30c6ea8d806524a90c5d045b5883cabc479f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:04:12 +0000

    First commited as before_22194_rev1

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/debug/ShowCommands.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/debug/ShowCommands.java
@@ -43,16 +43,14 @@
 
 package org.eclipse.jgit.pgm.debug;
 
-import java.io.IOException;
 import java.net.URL;
 
+import org.kohsuke.args4j.Option;
 import org.eclipse.jgit.pgm.Command;
 import org.eclipse.jgit.pgm.CommandCatalog;
 import org.eclipse.jgit.pgm.CommandRef;
 import org.eclipse.jgit.pgm.TextBuiltin;
 import org.eclipse.jgit.pgm.internal.CLIText;
-import org.eclipse.jgit.util.io.ThrowingPrintWriter;
-import org.kohsuke.args4j.Option;
 
 @Command(usage = "usage_displayAListOfAllRegisteredJgitCommands")
 class ShowCommands extends TextBuiltin {
@@ -69,39 +67,39 @@ class ShowCommands extends TextBuiltin {
 		width += 2;
 
 		for (final CommandRef c : list) {
-			errw.print(c.isCommon() ? '*' : ' ');
-			errw.print(' ');
+			System.err.print(c.isCommon() ? '*' : ' ');
+			System.err.print(' ');
 
-			errw.print(c.getName());
+			System.err.print(c.getName());
 			for (int i = c.getName().length(); i < width; i++)
-				errw.print(' ');
+				System.err.print(' ');
 
-			pretty.print(errw, c);
-			errw.println();
+			pretty.print(c);
+			System.err.println();
 		}
-		errw.println();
+		System.err.println();
 	}
 
 	static enum Format {
 		/** */
 		USAGE {
-			void print(ThrowingPrintWriter err, final CommandRef c) throws IOException {
+			void print(final CommandRef c) {
 				String usage = c.getUsage();
 				if (usage != null && usage.length() > 0)
-					err.print(CLIText.get().resourceBundle().getString(usage));
+					System.err.print(CLIText.get().resourceBundle().getString(usage));
 			}
 		},
 
 		/** */
 		CLASSES {
-			void print(ThrowingPrintWriter err, final CommandRef c) throws IOException {
-				err.print(c.getImplementationClassName());
+			void print(final CommandRef c) {
+				System.err.print(c.getImplementationClassName());
 			}
 		},
 
 		/** */
 		URLS {
-			void print(ThrowingPrintWriter err, final CommandRef c) throws IOException {
+			void print(final CommandRef c) {
 				final ClassLoader ldr = c.getImplementationClassLoader();
 
 				String cn = c.getImplementationClassName();
@@ -109,7 +107,7 @@ class ShowCommands extends TextBuiltin {
 
 				final URL url = ldr.getResource(cn);
 				if (url == null) {
-					err.print(CLIText.get().notFound);
+					System.err.print(CLIText.get().notFound);
 					return;
 				}
 
@@ -117,10 +115,10 @@ class ShowCommands extends TextBuiltin {
 				if (rn.endsWith(cn))
 					rn = rn.substring(0, rn.length() - cn.length());
 
-				err.print(rn);
+				System.err.print(rn);
 			}
 		};
 
-		abstract void print(ThrowingPrintWriter err, CommandRef c) throws IOException;
+		abstract void print(CommandRef c);
 	}
 }
