ae1e1dd1eb3b7e3a58ceb10fe844fabb35296d34
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:53:36 +0000

    First commited as before_14871_rev8

--- org.eclipse.jgit/src/org/eclipse/jgit/api/Git.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/Git.java
@@ -138,6 +138,17 @@ public class Git {
 	}
 
 	/**
+	 * Returns a command to list remote branches/tags without a local
+	 * repository.
+	 *
+	 * @return a {@link LsRemoteCommand}
+	 * @since 3.1
+	 */
+	public static LsRemoteCommand lsRemoteRepository() {
+		return new LsRemoteCommand(null);
+	}
+
+	/**
 	 * Returns a command object to execute a {@code init} command
 	 *
 	 * @see <a
@@ -650,16 +661,6 @@ public class Git {
 	 */
 	public NameRevCommand nameRev() {
 		return new NameRevCommand(repo);
-	}
-
-	/**
-	 * Returns a command object to come up with a short name that describes a commit
-	 * in terms of the nearest git tag.
-	 *
-	 * @return 3.1
-	 */
-	public DescribeCommand describe() {
-		return new DescribeCommand(repo);
 	}
 
 	/**
