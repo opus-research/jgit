f2c19a1af158d2e8022e5a70e7df66c5ac894e35
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:40:24 +0000

    First commited as before_56263_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/diff/DiffFormatter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diff/DiffFormatter.java
@@ -665,10 +665,16 @@ public class DiffFormatter implements AutoCloseable {
 		format(res.header, res.a, res.b);
 	}
 
-	private static byte[] writeGitLinkText(AbbreviatedObjectId id)
-		throws IOException {
-		return encodeASCII("Subproject commit " + id.name() //$NON-NLS-1$
-				+ "\n");
+	private static void writeGitLinkDiffText(OutputStream o, DiffEntry ent)
+			throws IOException {
+		if (ent.getOldMode() == GITLINK) {
+			o.write(encodeASCII("-Subproject commit " + ent.getOldId().name() //$NON-NLS-1$
+					+ "\n")); //$NON-NLS-1$
+		}
+		if (ent.getNewMode() == GITLINK) {
+			o.write(encodeASCII("+Subproject commit " + ent.getNewId().name() //$NON-NLS-1$
+					+ "\n")); //$NON-NLS-1$
+		}
 	}
 
 	private String format(AbbreviatedObjectId id) {
@@ -932,7 +938,13 @@ public class DiffFormatter implements AutoCloseable {
 
 		formatHeader(buf, ent);
 
-		if (ent.getOldId() == null || ent.getNewId() == null) {
+		if (ent.getOldMode() == GITLINK || ent.getNewMode() == GITLINK) {
+			formatOldNewPaths(buf, ent);
+			writeGitLinkDiffText(buf, ent);
+			editList = new EditList();
+			type = PatchType.UNIFIED;
+
+		} else if (ent.getOldId() == null || ent.getNewId() == null) {
 			// Content not changed (e.g. only mode, pure rename)
 			editList = new EditList();
 			type = PatchType.UNIFIED;
@@ -940,15 +952,8 @@ public class DiffFormatter implements AutoCloseable {
 		} else {
 			assertHaveRepository();
 
-			byte[] aRaw, bRaw;
-
-			if (ent.getOldMode() == GITLINK || ent.getNewMode() == GITLINK) {
-				aRaw = writeGitLinkText(ent.getOldId());
-				bRaw = writeGitLinkText(ent.getNewId());
-			} else {
-				aRaw = open(OLD, ent);
-				bRaw = open(NEW, ent);
-			}
+			byte[] aRaw = open(OLD, ent);
+			byte[] bRaw = open(NEW, ent);
 
 			if (aRaw == BINARY || bRaw == BINARY //
 					|| RawText.isBinary(aRaw) || RawText.isBinary(bRaw)) {
