18352de3dc0cfe95a9a246516e4f7cfd76ffd5d7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:21:26 +0000

    First commited as before_85010_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/InMemoryRepository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/InMemoryRepository.java
@@ -54,7 +54,6 @@ public class InMemoryRepository extends DfsRepository {
 
 	private final DfsObjDatabase objdb;
 	private final RefDatabase refdb;
-	private String gitwebDescription;
 	private boolean performsAtomicTransactions = true;
 
 	/**
@@ -93,16 +92,6 @@ public class InMemoryRepository extends DfsRepository {
 	 */
 	public void setPerformsAtomicTransactions(boolean atomic) {
 		performsAtomicTransactions = atomic;
-	}
-
-	@Override
-	public String getGitwebDescription() {
-		return gitwebDescription;
-	}
-
-	@Override
-	public void setGitwebDescription(String d) {
-		gitwebDescription = d;
 	}
 
 	private class MemObjDatabase extends DfsObjDatabase {
