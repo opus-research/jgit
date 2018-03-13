32aee7895c2e645307c66bc43d8ebc345d933e8d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 16:04:51 +0000

    First commited as before_110586_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/errors/BinaryBlobException.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/errors/BinaryBlobException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016, Google Inc.
+ * Copyright (C) 2017 Google Inc.
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -40,53 +40,19 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.eclipse.jgit.http.test;
-
-import java.io.IOException;
-
-import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
-import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository;
-import org.eclipse.jgit.internal.storage.reftable.Reftable;
-import org.eclipse.jgit.lib.RefDatabase;
+package org.eclipse.jgit.errors;
 
 /**
- * An {@link InMemoryRepository} whose refs can be made unreadable for testing
- * purposes.
+ * BinaryBlobException is used to signal that binary data was found
+ * in a context that requires text (eg. for generating textual diffs).
+ *
+ * @since 4.10
  */
-class RefsUnreadableInMemoryRepository extends InMemoryRepository {
-
-	private final RefsUnreadableRefDatabase refs;
-
-	private volatile boolean failing;
-
-	RefsUnreadableInMemoryRepository(DfsRepositoryDescription repoDesc) {
-		super(repoDesc);
-		refs = new RefsUnreadableRefDatabase();
-		failing = false;
-	}
-
-	@Override
-	public RefDatabase getRefDatabase() {
-		return refs;
-	}
+public class BinaryBlobException extends Exception {
+	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Make the ref database unable to scan its refs.
-	 * <p>
-	 * It may be useful to follow a call to startFailing with a call to
-	 * {@link RefDatabase#refresh()}, ensuring the next ref read fails.
+	 * Construct a BinaryBlobException.
 	 */
-	void startFailing() {
-		failing = true;
-	}
-
-	private class RefsUnreadableRefDatabase extends MemRefDatabase {
-		@Override
-		protected Reftable reader() throws IOException {
-			if (failing) {
-				throw new IOException("disk failed, no refs found");
-			}
-			return super.reader();
-		}
-	}
+	public BinaryBlobException() {}
 }
