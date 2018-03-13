032fe97905bc84fdaed4a02ca80beaef85719281
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:05:42 +0000

    First commited as before_2310_rev6

--- org.eclipse.jgit.http.test/tst/org/eclipse/jgit/http/server/ServletUtilsTest.java
+++ org.eclipse.jgit.http.test/tst/org/eclipse/jgit/http/server/ServletUtilsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, Google Inc.
+ * Copyright (C) 2011, Google Inc.
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -41,54 +41,24 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.eclipse.jgit.storage.file;
+package org.eclipse.jgit.http.server;
 
-import java.io.File;
-import java.io.IOException;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.eclipse.jgit.errors.RepositoryNotFoundException;
-import org.eclipse.jgit.lib.BaseRepositoryBuilder;
+import org.junit.Test;
 
-/**
- * Constructs a {@link FileRepository}.
- * <p>
- * Applications must set one of {@link #setGitDir(File)} or
- * {@link #setWorkTree(File)}, or use {@link #readEnvironment()} or
- * {@link #findGitDir()} in order to configure the minimum property set
- * necessary to open a repository.
- * <p>
- * Single repository applications trying to be compatible with other Git
- * implementations are encouraged to use a model such as:
- *
- * <pre>
- * new FileRepositoryBuilder() //
- * 		.setGitDir(gitDirArgument) // --git-dir if supplied, no-op if null
- * 		.readEnviroment() // scan environment GIT_* variables
- * 		.findGitDir() // scan up the file system tree
- * 		.build()
- * </pre>
- */
-public class FileRepositoryBuilder extends
-		BaseRepositoryBuilder<FileRepositoryBuilder, FileRepository> {
-	/**
-	 * Create a repository matching the configuration in this builder.
-	 * <p>
-	 * If an option was not set, the build method will try to default the option
-	 * based on other options. If insufficient information is available, an
-	 * exception is thrown to the caller.
-	 *
-	 * @return a repository matching this configuration.
-	 * @throws IllegalArgumentException
-	 *             insufficient parameters were set.
-	 * @throws IOException
-	 *             the repository could not be accessed to configure the rest of
-	 *             the builder's parameters.
-	 */
-	@Override
-	public FileRepository build() throws IOException {
-		FileRepository repo = new FileRepository(setup());
-		if (isMustExist() && !repo.getObjectDatabase().exists())
-			throw new RepositoryNotFoundException(getGitDir());
-		return repo;
+public class ServletUtilsTest {
+	@Test
+	public void testAcceptGzip() {
+		assertFalse(ServletUtils.acceptsGzipEncoding((String) null));
+		assertFalse(ServletUtils.acceptsGzipEncoding(""));
+
+		assertTrue(ServletUtils.acceptsGzipEncoding("gzip"));
+		assertTrue(ServletUtils.acceptsGzipEncoding("deflate,gzip"));
+		assertTrue(ServletUtils.acceptsGzipEncoding("gzip,deflate"));
+
+		assertFalse(ServletUtils.acceptsGzipEncoding("gzip(proxy)"));
+		assertFalse(ServletUtils.acceptsGzipEncoding("proxy-gzip"));
 	}
 }
