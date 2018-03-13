e9bc8c19a296ca529c7ceb70f2b92816fe4c02aa
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:25:36 +0000

    First commited as before_5485_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/Daemon.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/Daemon.java
@@ -366,8 +366,7 @@ public class Daemon {
 		return null;
 	}
 
-	Repository openRepository(DaemonClient client, String name)
-			throws ServiceMayNotContinueException {
+	Repository openRepository(DaemonClient client, String name) {
 		// Assume any attempt to use \ was by a Windows client
 		// and correct to the more typical / used in Git URIs.
 		//
--- org.eclipse.jgit/src/org/eclipse/jgit/transport/DaemonService.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/DaemonService.java
@@ -130,16 +130,7 @@ public abstract class DaemonService {
 			throws IOException, ServiceNotEnabledException,
 			ServiceNotAuthorizedException {
 		final String name = commandLine.substring(command.length() + 1);
-		Repository db;
-		try {
-			db = client.getDaemon().openRepository(client, name);
-		} catch (ServiceMayNotContinueException e) {
-			// An error when opening the repo means the client is expecting a ref
-			// advertisement, so use that style of error.
-			PacketLineOut pktOut = new PacketLineOut(client.getOutputStream());
-			pktOut.writeString("ERR " + e.getMessage() + "\n");
-			db = null;
-		}
+		Repository db = client.getDaemon().openRepository(client, name);
 		if (db == null)
 			return;
 		try {
