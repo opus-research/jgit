c0f125e06eb5336c46aea52fc312c41f73c0de88
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 12:59:32 +0000

    First commited as before_1548_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/WindowCache.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/WindowCache.java
@@ -187,7 +187,7 @@ public class WindowCache {
 			oc.removeAll();
 		cache = nc;
 		streamFileThreshold = cfg.getStreamFileThreshold();
-		UnpackedObjectCache.reconfigure(cfg);
+		DeltaBaseCache.reconfigure(cfg);
 	}
 
 	static int getStreamFileThreshold() {
@@ -518,7 +518,6 @@ public class WindowCache {
 		gc();
 	}
 
-	@SuppressWarnings("unchecked")
 	private void gc() {
 		Ref r;
 		while ((r = (Ref) queue.poll()) != null) {
