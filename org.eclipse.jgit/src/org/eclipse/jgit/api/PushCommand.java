05de9dd2903289e7c039c5e1199dcc8e521f214f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:43:00 +0000

    First commited as before_9732_rev10

--- org.eclipse.jgit/src/org/eclipse/jgit/api/PushCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/PushCommand.java
@@ -43,7 +43,6 @@
 package org.eclipse.jgit.api;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -92,8 +91,6 @@ public class PushCommand extends
 	private boolean force;
 
 	private boolean thin = Transport.DEFAULT_PUSH_THIN;
-
-	private OutputStream out;
 
 	/**
 	 * @param repo
@@ -153,7 +150,7 @@ public class PushCommand extends
 						.findRemoteRefUpdatesFor(refSpecs);
 
 				try {
-					PushResult result = transport.push(monitor, toPush, out);
+					PushResult result = transport.push(monitor, toPush);
 					pushResults.add(result);
 
 				} catch (TransportException e) {
@@ -405,18 +402,6 @@ public class PushCommand extends
 	public PushCommand setForce(boolean force) {
 		checkCallable();
 		this.force = force;
-		return this;
-	}
-
-	/**
-	 * Sets the output stream to write sideband messages to
-	 *
-	 * @param out
-	 * @return {@code this}
-	 * @since 3.0
-	 */
-	public PushCommand setOutputStream(OutputStream out) {
-		this.out = out;
 		return this;
 	}
 }
