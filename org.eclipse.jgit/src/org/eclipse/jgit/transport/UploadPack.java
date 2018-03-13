c1f2fa1a04fd84c82a63c254f97e095712d097d9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:40:08 +0000

    First commited as before_55903_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
@@ -737,6 +737,8 @@ public class UploadPack {
 
 			if (depth != 0)
 				processShallow();
+			if (!clientShallowCommits.isEmpty())
+				walk.assumeShallow(clientShallowCommits);
 			sendPack = negotiate();
 		} catch (PackProtocolException err) {
 			reportErrorDuringNegotiate(err.getMessage());
@@ -786,10 +788,6 @@ public class UploadPack {
 	}
 
 	private void processShallow() throws IOException {
-		if (!clientShallowCommits.isEmpty()) {
-			verifyClientShallow();
-		}
-
 		try (DepthWalk.RevWalk depthWalk = new DepthWalk.RevWalk(
 				walk.getObjectReader(), depth)) {
 
@@ -820,33 +818,6 @@ public class UploadPack {
 			}
 		}
 		pckOut.end();
-		walk.assumeShallow(clientShallowCommits);
-	}
-
-	private void verifyClientShallow()
-			throws IOException, PackProtocolException {
-		AsyncRevObjectQueue q = walk.parseAny(clientShallowCommits, true);
-		try {
-			for (;;) {
-				try {
-					// Shallow objects named by the client must be commits.
-					RevObject o = q.next();
-					if (!(o instanceof RevCommit)) {
-						throw new PackProtocolException(
-							MessageFormat.format(
-								JGitText.get().invalidShallowObject,
-								o.name()));
-					}
-				} catch (MissingObjectException notCommit) {
-					// shallow objects not known at the server are ignored
-					// by git-core upload-pack, match that behavior.
-					clientShallowCommits.remove(notCommit.getObjectId());
-					continue;
-				}
-			}
-		} finally {
-			q.release();
-		}
 	}
 
 	/**
