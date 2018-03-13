163738ea9117feb6bb6959edce29f93d8ccc5a71
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 12:59:12 +0000

    First commited as before_1494_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/FileRepository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/FileRepository.java
@@ -170,6 +170,7 @@ public class FileRepository extends Repository {
 				options.getObjectDirectory(), //
 				options.getAlternateObjectDirectories(), //
 				getFS());
+		getListenerList().addConfigChangedListener(objectDatabase);
 
 		if (objectDatabase.exists()) {
 			final String repositoryFormatVersion = getConfig().getString(
