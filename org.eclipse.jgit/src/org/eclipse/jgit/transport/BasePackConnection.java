ac1e97b37b13ffa4008a7b22cb3ff08cfa5293a6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 12:54:01 +0000

    First commited as before_388_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/BasePackConnection.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/BasePackConnection.java
@@ -282,10 +282,8 @@ abstract class BasePackConnection extends BaseConnection {
 	public void close() {
 		if (out != null) {
 			try {
-				if (outNeedsEnd) {
-					outNeedsEnd = false;
+				if (outNeedsEnd)
 					pckOut.end();
-				}
 				out.close();
 			} catch (IOException err) {
 				// Ignore any close errors.
@@ -313,24 +311,6 @@ abstract class BasePackConnection extends BaseConnection {
 				myTimer = null;
 				timeoutIn = null;
 				timeoutOut = null;
-			}
-		}
-	}
-
-	protected void endOut() {
-		if (outNeedsEnd && out != null) {
-			try {
-				outNeedsEnd = false;
-				pckOut.end();
-			} catch (IOException e) {
-				try {
-					out.close();
-				} catch (IOException err) {
-					// Ignore any close errors.
-				} finally {
-					out = null;
-					pckOut = null;
-				}
 			}
 		}
 	}
