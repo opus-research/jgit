95489f03c91c5b7db1817b0b6e06be52a2845cae
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:46:02 +0000

    First commited as before_60097_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/RefDatabase.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/RefDatabase.java
@@ -239,11 +239,20 @@ public abstract class RefDatabase {
 	 * @since 4.1
 	 */
 	public Ref exactRef(String name) throws IOException {
-		Ref ref = getRef(name);
-		if (ref == null || !name.equals(ref.getName())) {
-			return null;
+		int slash = name.lastIndexOf('/');
+		String prefix = name.substring(0, slash + 1);
+		String rest = name.substring(slash + 1);
+		Ref result = getRefs(prefix).get(rest);
+		if (result != null || slash != -1) {
+			return result;
 		}
-		return ref;
+
+		for (Ref ref : getAdditionalRefs()) {
+			if (name.equals(ref.getName())) {
+				return ref;
+			}
+		}
+		return null;
 	}
 
 	/**
