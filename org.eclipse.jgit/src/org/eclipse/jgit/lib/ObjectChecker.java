04482f7bdc450b59ceeb50857f30821ea88e4a6f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:05:52 +0000

    First commited as before_23277_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/ObjectChecker.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/ObjectChecker.java
@@ -53,6 +53,7 @@ import java.text.MessageFormat;
 import org.eclipse.jgit.errors.CorruptObjectException;
 import org.eclipse.jgit.internal.JGitText;
 import org.eclipse.jgit.util.MutableInteger;
+import org.eclipse.jgit.util.RawParseUtils;
 
 /**
  * Verifies that an object is formatted correctly.
@@ -96,6 +97,40 @@ public class ObjectChecker {
 	private final MutableObjectId tempId = new MutableObjectId();
 
 	private final MutableInteger ptrout = new MutableInteger();
+
+	private boolean allowZeroMode;
+	private boolean windows;
+
+	/**
+	 * Enable accepting leading zero mode in tree entries.
+	 * <p>
+	 * Some broken Git libraries generated leading zeros in the mode part of
+	 * tree entries. This is technically incorrect but gracefully allowed by
+	 * git-core. JGit rejects such trees by default, but may need to accept
+	 * them on broken histories.
+	 *
+	 * @param allow allow leading zero mode.
+	 * @return {@code this}.
+	 * @since 3.4
+	 */
+	public ObjectChecker setAllowLeadingZeroFileMode(boolean allow) {
+		allowZeroMode = allow;
+		return this;
+	}
+
+	/**
+	 * Restrict trees to only names legal on Windows platforms.
+	 * <p>
+	 * Also rejects any mixed case forms of reserved names ({@code .git}).
+	 *
+	 * @param win true if Windows name checking should be performed.
+	 * @return {@code this}.
+	 * @since 3.4
+	 */
+	public ObjectChecker setSafeForWindows(boolean win) {
+		windows = win;
+		return this;
+	}
 
 	/**
 	 * Check an object for parsing errors.
@@ -308,7 +343,7 @@ public class ObjectChecker {
 					break;
 				if (c < '0' || c > '7')
 					throw new CorruptObjectException("invalid mode character");
-				if (thisMode == 0 && c == '0')
+				if (thisMode == 0 && c == '0' && !allowZeroMode)
 					throw new CorruptObjectException("mode starts with '0'");
 				thisMode <<= 3;
 				thisMode += c - '0';
@@ -326,16 +361,15 @@ public class ObjectChecker {
 					break;
 				if (c == '/')
 					throw new CorruptObjectException("name contains '/'");
+				if (windows && isInvalidOnWindows(c)) {
+					if (c > 31)
+						throw new CorruptObjectException(String.format(
+								"name contains '%c'", c));
+					throw new CorruptObjectException(String.format(
+							"name contains byte 0x%x", c & 0xff));
+				}
 			}
-			if (thisNameB + 1 == ptr)
-				throw new CorruptObjectException("zero length name");
-			if (raw[thisNameB] == '.') {
-				final int nameLen = (ptr - 1) - thisNameB;
-				if (nameLen == 1)
-					throw new CorruptObjectException("invalid name '.'");
-				if (nameLen == 2 && raw[thisNameB + 1] == '.')
-					throw new CorruptObjectException("invalid name '..'");
-			}
+			checkPathSegment(raw, thisNameB, ptr - 1);
 			if (duplicateName(raw, thisNameB, ptr - 1))
 				throw new CorruptObjectException("duplicate entry names");
 
@@ -354,6 +388,124 @@ public class ObjectChecker {
 			if (ptr > sz)
 				throw new CorruptObjectException("truncated in object id");
 		}
+	}
+
+	private void checkPathSegment(byte[] raw, int ptr, int end)
+			throws CorruptObjectException {
+		if (ptr == end)
+			throw new CorruptObjectException("zero length name");
+		if (raw[ptr] == '.') {
+			switch (end - ptr) {
+			case 1:
+				throw new CorruptObjectException("invalid name '.'");
+			case 2:
+				if (raw[ptr + 1] == '.')
+					throw new CorruptObjectException("invalid name '..'");
+				break;
+			case 4:
+				if (isDotGit(raw, ptr + 1))
+					throw new CorruptObjectException(String.format(
+							"invalid name '%s'",
+							RawParseUtils.decode(raw, ptr, end)));
+			}
+		}
+
+		if (windows) {
+			// Windows ignores space and dot at end of file name.
+			if (raw[end - 1] == ' ' || raw[end - 1] == '.')
+				throw new CorruptObjectException("invalid name ends with '"
+						+ ((char) raw[end - 1]) + "'");
+			if (end - ptr >= 3)
+				checkNotWindowsDevice(raw, ptr, end);
+		}
+	}
+
+	private static void checkNotWindowsDevice(byte[] raw, int ptr, int end)
+			throws CorruptObjectException {
+		switch (toLower(raw[ptr])) {
+		case 'a': // AUX
+			if (end - ptr >= 3
+					&& toLower(raw[ptr + 1]) == 'u'
+					&& toLower(raw[ptr + 2]) == 'x'
+					&& (end - ptr == 3 || raw[ptr + 3] == '.'))
+				throw new CorruptObjectException("invalid name 'AUX'");
+			break;
+
+		case 'c': // CON, COM[1-9]
+			if (end - ptr >= 3
+					&& toLower(raw[ptr + 2]) == 'n'
+					&& toLower(raw[ptr + 1]) == 'o'
+					&& (end - ptr == 3 || raw[ptr + 3] == '.'))
+				throw new CorruptObjectException("invalid name 'CON'");
+			if (end - ptr >= 4
+					&& toLower(raw[ptr + 2]) == 'm'
+					&& toLower(raw[ptr + 1]) == 'o'
+					&& isDigit(raw[ptr + 3])
+					&& (end - ptr == 4 || raw[ptr + 4] == '.'))
+				throw new CorruptObjectException("invalid name 'COM"
+						+ ((char) raw[ptr + 3]) + "'");
+			break;
+
+		case 'l': // LPT[1-9]
+			if (end - ptr >= 4
+					&& toLower(raw[ptr + 1]) == 'p'
+					&& toLower(raw[ptr + 2]) == 't'
+					&& isDigit(raw[ptr + 3])
+					&& (end - ptr == 4 || raw[ptr + 4] == '.'))
+				throw new CorruptObjectException("invalid name 'LPT"
+						+ ((char) raw[ptr + 3]) + "'");
+			break;
+
+		case 'n': // NUL
+			if (end - ptr >= 3
+					&& toLower(raw[ptr + 1]) == 'u'
+					&& toLower(raw[ptr + 2]) == 'l'
+					&& (end - ptr == 3 || raw[ptr + 3] == '.'))
+				throw new CorruptObjectException("invalid name 'NUL'");
+			break;
+
+		case 'p': // PRN
+			if (end - ptr >= 3
+					&& toLower(raw[ptr + 1]) == 'r'
+					&& toLower(raw[ptr + 2]) == 'n'
+					&& (end - ptr == 3 || raw[ptr + 3] == '.'))
+				throw new CorruptObjectException("invalid name 'PRN'");
+			break;
+		}
+	}
+
+	private static boolean isInvalidOnWindows(byte c) {
+		// Windows disallows "special" characters in a path component.
+		switch (c) {
+		case '"':
+		case '*':
+		case ':':
+		case '<':
+		case '>':
+		case '?':
+		case '\\':
+		case '|':
+			return true;
+		}
+		return 1 <= c && c <= 31;
+	}
+
+	private boolean isDotGit(byte[] buf, int p) {
+		if (windows)
+			return toLower(buf[p]) == 'g'
+					&& toLower(buf[p + 1]) == 'i'
+					&& toLower(buf[p + 2]) == 't';
+		return buf[p] == 'g' && buf[p + 1] == 'i' && buf[p + 2] == 't';
+	}
+
+	private static char toLower(byte b) {
+		if ('A' <= b && b <= 'Z')
+			return (char) (b + ('a' - 'A'));
+		return (char) b;
+	}
+
+	private static boolean isDigit(byte b) {
+		return '1' <= b && b <= '9';
 	}
 
 	/**
