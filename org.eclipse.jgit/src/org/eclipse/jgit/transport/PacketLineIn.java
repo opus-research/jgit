be77b9445504813b9ff08686f0432c81ed67cb0f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 12:53:42 +0000

    First commited as before_307_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/PacketLineIn.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/PacketLineIn.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2010, Google Inc.
+ * Copyright (C) 2008-2009, Google Inc.
  * Copyright (C) 2008-2009, Robin Rosenberg <robin.rosenberg@dewire.com>
  * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
  * and other copyright owners as documented in the project's IP log.
@@ -72,11 +72,11 @@ class PacketLineIn {
 
 	private final InputStream in;
 
-	private final byte[] lineBuffer;
+	private final byte[] lenbuffer;
 
 	PacketLineIn(final InputStream i) {
 		in = i;
-		lineBuffer = new byte[SideBandOutputStream.SMALL_BUF];
+		lenbuffer = new byte[4];
 	}
 
 	AckNackResult readACK(final MutableObjectId returnedId) throws IOException {
@@ -124,27 +124,22 @@ class PacketLineIn {
 
 		len -= 4; // length header (4 bytes)
 
-		byte[] raw;
-		if (len < lineBuffer.length)
-			raw = lineBuffer;
-		else
-			raw = new byte[len];
-
+		final byte[] raw = new byte[len];
 		IO.readFully(in, raw, 0, len);
 		return RawParseUtils.decode(Constants.CHARSET, raw, 0, len);
 	}
 
 	int readLength() throws IOException {
-		IO.readFully(in, lineBuffer, 0, 4);
+		IO.readFully(in, lenbuffer, 0, 4);
 		try {
-			final int len = RawParseUtils.parseHexInt16(lineBuffer, 0);
+			final int len = RawParseUtils.parseHexInt16(lenbuffer, 0);
 			if (len != 0 && len < 4)
 				throw new ArrayIndexOutOfBoundsException();
 			return len;
 		} catch (ArrayIndexOutOfBoundsException err) {
 			throw new IOException("Invalid packet line header: "
-					+ (char) lineBuffer[0] + (char) lineBuffer[1]
-					+ (char) lineBuffer[2] + (char) lineBuffer[3]);
+					+ (char) lenbuffer[0] + (char) lenbuffer[1]
+					+ (char) lenbuffer[2] + (char) lenbuffer[3]);
 		}
 	}
 }
