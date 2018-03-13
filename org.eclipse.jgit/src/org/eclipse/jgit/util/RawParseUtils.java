11ec519c4f6c09aaa9ddd0e859bd933e2c38506e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:13:48 +0000

    First commited as before_31802_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/util/RawParseUtils.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/RawParseUtils.java
@@ -81,10 +81,8 @@ public final class RawParseUtils {
 	private static final Map<String, Charset> encodingAliases;
 
 	static {
-		Charset latin1 = Charset.forName("ISO-8859-1"); //$NON-NLS-2$
 		encodingAliases = new HashMap<String, Charset>();
-		encodingAliases.put("iso-8859", latin1); //$NON-NLS-1$
-		encodingAliases.put("latin-1", latin1); //$NON-NLS-1$
+		encodingAliases.put("latin-1", Charset.forName("ISO-8859-1")); //$NON-NLS-1$ //$NON-NLS-2$
 
 		digits10 = new byte['9' + 1];
 		Arrays.fill(digits10, (byte) -1);
