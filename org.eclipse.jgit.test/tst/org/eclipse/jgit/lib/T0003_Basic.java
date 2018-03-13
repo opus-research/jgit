054206a8d979bfe0e0e859c8e67a93a295873f30
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 12:53:06 +0000

    First commited as before_196_rev5

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/T0003_Basic.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/lib/T0003_Basic.java
@@ -731,6 +731,17 @@ public class T0003_Basic extends SampleDataRepositoryTestCase {
 
 	}
 
+	/**
+	 * Kick the timestamp of a local file.
+	 * <p>
+	 * We shouldn't have to make these method calls. The cache is using file
+	 * system timestamps, and on many systems unit tests run faster than the
+	 * modification clock. Dumping the cache after we make an edit behind
+	 * RefDirectory's back allows the tests to pass.
+	 *
+	 * @param name
+	 *            the file in the repository to force a time change on.
+	 */
 	private void BUG_WorkAroundRacyGitIssues(String name) {
 		File path = new File(db.getDirectory(), name);
 		long old = path.lastModified();
