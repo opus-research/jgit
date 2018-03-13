267448ab52081370d291762fa90ea7c4c1e8a548
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:49:39 +0000

    First commited as before_62119_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/ChainingCredentialsProvider.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/ChainingCredentialsProvider.java
@@ -114,6 +114,9 @@ public class ChainingCredentialsProvider extends CredentialsProvider {
 		for (CredentialsProvider p : credentialProviders) {
 			if (p.supports(items)) {
 				if (!p.get(uri, items)) {
+					if (p.isInteractive()) {
+						return false; // user cancelled the request
+					}
 					continue;
 				}
 				if (isAnyNull(items)) {
@@ -122,13 +125,6 @@ public class ChainingCredentialsProvider extends CredentialsProvider {
 				return true;
 			}
 		}
-		return false;
-	}
-
-	private boolean isAnyNull(CredentialItem... items) {
-		for (CredentialItem i : items)
-			if (i == null)
-				return true;
 		return false;
 	}
 }
