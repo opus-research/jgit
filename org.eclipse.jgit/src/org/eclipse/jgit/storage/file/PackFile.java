b107e0ab774e637ff29b1abfaca91338ba77dab5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:43:17 +0000

    First commited as before_9980_rev3

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/PackFile.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/PackFile.java
@@ -54,12 +54,10 @@ import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel.MapMode;
 import java.text.MessageFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 import java.util.zip.CRC32;
 import java.util.zip.DataFormatException;
@@ -98,8 +96,6 @@ public class PackFile implements Iterable<PackIndex.MutableEntry> {
 	};
 
 	private final File packFile;
-
-	private final List<PackExt> extensions;
 
 	private File keepFile;
 
@@ -142,16 +138,10 @@ public class PackFile implements Iterable<PackIndex.MutableEntry> {
 	 *
 	 * @param packFile
 	 *            path of the <code>.pack</code> file holding the data.
-	 * @param extensions
-	 *            additional pakc file extensions with the same base as the pack
 	 */
-	public PackFile(final File packFile, Iterable<PackExt> extensions) {
+	public PackFile(final File packFile) {
 		this.packFile = packFile;
 		this.packLastModified = (int) (packFile.lastModified() >> 10);
-		this.extensions = new ArrayList<PackExt>(PackExt.values().length);
-		for (PackExt ext : extensions) {
-			this.extensions.add(ext);
-		}
 
 		// Multiply by 31 here so we can more directly combine with another
 		// value in WindowCache.hash(), without doing the multiply there.
--- org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/PackExt.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/PackExt.java
@@ -52,8 +52,6 @@ public class PackExt {
 	/** A pack index file extension. */
 	public static final PackExt INDEX = new PackExt("idx"); //$NON-NLS-1$
 
-	private static final PackExt[] VALUES = new PackExt[] { PACK, INDEX };
-
 	private final String ext;
 
 	/**
@@ -85,10 +83,5 @@ public class PackExt {
 	@Override
 	public String toString() {
 		return String.format("PackExt[%s]", getExtension()); //$NON-NLS-1$
-	}
-
-	/** @return all of the PackExt values. */
-	public static PackExt[] values() {
-		return VALUES;
 	}
 }
