dcb3c031ded9f731d98f85970f8f2ac12964fafc
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:11:23 +0000

    First commited as before_3583_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/RefDirectoryUpdate.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/RefDirectoryUpdate.java
@@ -121,7 +121,7 @@ class RefDirectoryUpdate extends RefUpdate {
 		}
 		if (!lock.commit())
 			return Result.LOCK_FAILURE;
-		database.stored(this, lock.getCommitSnapshot());
+		database.stored(this, lock.getCommitLastModified());
 		return status;
 	}
 
@@ -159,7 +159,7 @@ class RefDirectoryUpdate extends RefUpdate {
 			database.log(this, msg, false);
 		if (!lock.commit())
 			return Result.LOCK_FAILURE;
-		database.storedSymbolicRef(this, lock.getCommitSnapshot(), target);
+		database.storedSymbolicRef(this, lock.getCommitLastModified(), target);
 
 		if (getRef().getStorage() == Ref.Storage.NEW)
 			return Result.NEW;
