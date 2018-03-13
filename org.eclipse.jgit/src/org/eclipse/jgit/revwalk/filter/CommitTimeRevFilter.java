7cb710fd6167ba3d6f6597f879ea01df8cc49aa9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:06:46 +0000

    First commited as before_2461_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/revwalk/filter/CommitTimeRevFilter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/revwalk/filter/CommitTimeRevFilter.java
@@ -63,18 +63,7 @@ public abstract class CommitTimeRevFilter extends RevFilter {
 	 * @return a new filter to select commits on or before <code>ts</code>.
 	 */
 	public static final RevFilter before(final Date ts) {
-		return before(ts.getTime());
-	}
-
-	/**
-	 * Create a new filter to select commits before a given date/time.
-	 *
-	 * @param ts
-	 *            the point in time to cut on, in milliseconds
-	 * @return a new filter to select commits on or before <code>ts</code>.
-	 */
-	public static final RevFilter before(final long ts) {
-		return new Before(ts);
+		return new Before(ts.getTime());
 	}
 
 	/**
@@ -85,18 +74,7 @@ public abstract class CommitTimeRevFilter extends RevFilter {
 	 * @return a new filter to select commits on or after <code>ts</code>.
 	 */
 	public static final RevFilter after(final Date ts) {
-		return after(ts.getTime());
-	}
-
-	/**
-	 * Create a new filter to select commits after a given date/time.
-	 *
-	 * @param ts
-	 *            the point in time to cut on, in milliseconds.
-	 * @return a new filter to select commits on or after <code>ts</code>.
-	 */
-	public static final RevFilter after(final long ts) {
-		return new After(ts);
+		return new After(ts.getTime());
 	}
 
 	/**
@@ -108,19 +86,7 @@ public abstract class CommitTimeRevFilter extends RevFilter {
 	 * @return a new filter to select commits between the given date/times.
 	 */
 	public static final RevFilter between(final Date since, final Date until) {
-		return between(since.getTime(), until.getTime());
-	}
-
-	/**
-	 * Create a new filter to select commits after or equal a given date/time <code>since</code>
-	 * and before or equal a given date/time <code>until</code>.
-	 *
-	 * @param since the point in time to cut on, in milliseconds.
-	 * @param until the point in time to cut off, in millisconds.
-	 * @return a new filter to select commits between the given date/times.
-	 */
-	public static final RevFilter between(final long since, final long until) {
-		return new Between(since, until);
+		return new Between(since.getTime(), until.getTime());
 	}
 
 	final int when;
