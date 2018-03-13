e054b61b264fb16f668e8bde77257af9eeda3efb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:40:26 +0000

    First commited as before_99371_rev3

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsGarbageCollector.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsGarbageCollector.java
@@ -274,12 +274,6 @@ public class DfsGarbageCollector {
 			// Hoist all branch tips and tags earlier in the pack file
 			tagTargets.addAll(allHeadsAndTags);
 
-			// Combine the GC_REST objects into the GC pack if requested
-			if (packConfig.getSinglePack()) {
-				allHeadsAndTags.addAll(nonHeads);
-				nonHeads.clear();
-			}
-
 			boolean rollback = true;
 			try {
 				packHeads(pm);
