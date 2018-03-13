06f9919636e96850cc56f37a59bb54f330d4feec
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:46:24 +0000

    First commited as before_101222_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftable/ReftableWriter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/reftable/ReftableWriter.java
@@ -47,12 +47,18 @@ import static java.lang.Math.log;
 import static org.eclipse.jgit.internal.storage.reftable.ReftableConstants.FILE_FOOTER_LEN;
 import static org.eclipse.jgit.internal.storage.reftable.ReftableConstants.FILE_HEADER_LEN;
 import static org.eclipse.jgit.internal.storage.reftable.ReftableConstants.FILE_HEADER_MAGIC;
+import static org.eclipse.jgit.internal.storage.reftable.ReftableConstants.INDEX_BLOCK_TYPE;
 import static org.eclipse.jgit.internal.storage.reftable.ReftableConstants.VERSION_1;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.zip.CRC32;
 
+import org.eclipse.jgit.annotations.Nullable;
+import org.eclipse.jgit.internal.storage.reftable.BlockWriter.LogEntry;
+import org.eclipse.jgit.internal.storage.reftable.BlockWriter.RefEntry;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.util.NB;
 
@@ -63,26 +69,54 @@ import org.eclipse.jgit.util.NB;
  * all references. A {@link ReftableWriter} is single-use, and not thread-safe.
  */
 public class ReftableWriter {
+	private int refBlockSize = 4 << 10;
+	private int logBlockSize = 8 << 10;
 	private int restartInterval;
-	private int blockSize = 4 << 10;
 
 	private ReftableOutputStream out;
-	private IndexBlockWriter idx;
-	private RefBlockWriter cur;
+
+	private BlockWriter refIndex;
+	private BlockWriter logIndex;
+	private BlockWriter cur;
+
+	private long logOffset;
+	private long refIndexOffset;
+	private long logIndexOffset;
+
+	private long refBytes;
+	private long logBytes;
+	private int refBlocks;
+	private int refIndexSize;
+	private ObjectId hash;
 	private Stats stats;
 
 	/**
 	 * @param szBytes
-	 *            desired output block size in bytes.
+	 *            desired output block size for references, in bytes.
 	 * @return {@code this}
 	 */
-	public ReftableWriter setBlockSize(int szBytes) {
+	public ReftableWriter setRefBlockSize(int szBytes) {
 		if (out != null) {
 			throw new IllegalStateException();
 		} else if (szBytes <= 1024 || szBytes >= (1 << 24)) {
 			throw new IllegalArgumentException();
 		}
-		blockSize = szBytes;
+		refBlockSize = szBytes;
+		return this;
+	}
+
+	/**
+	 * @param szBytes
+	 *            desired output block size for log entries, in bytes.
+	 * @return {@code this}
+	 */
+	public ReftableWriter setLogBlockSize(int szBytes) {
+		if (out != null) {
+			throw new IllegalStateException();
+		} else if (szBytes <= 1024 || szBytes >= (1 << 24)) {
+			throw new IllegalArgumentException();
+		}
+		logBlockSize = szBytes;
 		return this;
 	}
 
@@ -103,11 +137,6 @@ public class ReftableWriter {
 		return this;
 	}
 
-	/** @return statistics of the last written reftable. */
-	public Stats getStats() {
-		return stats;
-	}
-
 	/**
 	 * Begin writing the reftable.
 	 * <p>
@@ -123,11 +152,13 @@ public class ReftableWriter {
 	 */
 	public ReftableWriter begin(OutputStream os) throws IOException {
 		if (restartInterval <= 0) {
-			restartInterval = blockSize < (60 << 10) ? 16 : 64;
+			restartInterval = refBlockSize < (60 << 10) ? 16 : 64;
 		}
 
-		idx = new IndexBlockWriter(restartInterval);
-		out = new ReftableOutputStream(os, blockSize);
+		int ri = restartInterval;
+		refIndex = new BlockWriter(INDEX_BLOCK_TYPE, refBlockSize, ri);
+		logIndex = new BlockWriter(INDEX_BLOCK_TYPE, logBlockSize, ri);
+		out = new ReftableOutputStream(os, refBlockSize);
 		writeFileHeader();
 		return this;
 	}
@@ -143,18 +174,68 @@ public class ReftableWriter {
 	 * @throws IOException
 	 *             reftable cannot be written.
 	 */
-	public ReftableWriter write(Ref ref) throws IOException {
-		if (cur == null) {
-			int bs = out.bytesAvailableInBlock();
-			cur = new RefBlockWriter(bs, restartInterval, ref);
-		} else if (!cur.tryAdd(ref)) {
-			idx.addBlock(cur);
-			cur.writeTo(out, true);
+	public ReftableWriter writeRef(Ref ref) throws IOException {
+		return writeEntry(refIndex, new RefEntry(ref));
+	}
 
-			int bs = out.bytesAvailableInBlock();
-			cur = new RefBlockWriter(bs, restartInterval, ref);
+	/**
+	 * Write one reflog entry to the reftable.
+	 * <p>
+	 * Reflog entries must be written in reference name and descending time
+	 * (most recent first) order.
+	 *
+	 * @param name
+	 *            name of the reference.
+	 * @param who
+	 *            committer of the reflog entry.
+	 * @param oldId
+	 *            prior id; pass {@link ObjectId#zeroId()} for creations.
+	 * @param newId
+	 *            new id; pass {@link ObjectId#zeroId()} for deletions.
+	 * @param message
+	 *            optional message (may be null).
+	 * @return {@code this}
+	 * @throws IOException
+	 *             reftable cannot be written.
+	 */
+	public ReftableWriter writeLog(String name, PersonIdent who, ObjectId oldId,
+			ObjectId newId, @Nullable String message) throws IOException {
+		String msg = message != null ? message : ""; //$NON-NLS-1$
+		beginLog();
+		return writeEntry(logIndex, new LogEntry(name, who, oldId, newId, msg));
+	}
+
+	private void beginLog() throws IOException {
+		if (logOffset == 0) {
+			finishRef(); // close prior ref blocks and their index, if present.
+			out.setBlockSize(logBlockSize);
+			logOffset = out.size();
+		}
+	}
+
+	private ReftableWriter writeEntry(
+			BlockWriter idx,
+			BlockWriter.Entry entry)
+			throws IOException {
+		if (cur == null) {
+			beginBlock(entry);
+		} else if (!cur.tryAdd(entry)) {
+			idx.addIndex(cur.lastKey(), out.size());
+			cur.writeTo(out);
+			if (entry instanceof RefEntry) {
+				out.padBetweenBlocksToNextBlock();
+			}
+			beginBlock(entry);
 		}
 		return this;
+	}
+
+	private void beginBlock(BlockWriter.Entry entry)
+			throws BlockSizeTooSmallException {
+		byte type = entry.blockType();
+		int bs = out.bytesAvailableInBlock();
+		cur = new BlockWriter(type, bs, restartInterval);
+		cur.addFirst(entry);
 	}
 
 	/**
@@ -165,71 +246,145 @@ public class ReftableWriter {
 	 *             reftable cannot be written.
 	 */
 	public ReftableWriter finish() throws IOException {
-		if (cur != null) {
-			idx.addBlock(cur);
-			boolean useIdx = idx.shouldWriteIndex();
-			cur.writeTo(out, useIdx);
-			if (useIdx) {
-				idx.writeTo(out);
-			}
-		}
+		finishRef();
+		finishLog();
 		writeFileFooter();
 
-		stats = new Stats(this, out, idx);
-		idx = null;
+		stats = new Stats(this, out, refIndex);
+		refIndex = null;
+		logIndex = null;
 		cur = null;
 		out = null;
 		return this;
 	}
 
+	private void finishRef() throws IOException {
+		if (logOffset == 0 && cur != null) {
+			refBlocks = out.blockCount() + 1;
+			refIndexOffset = finishBlockMaybeWriteIndex(refIndex);
+			if (refIndexOffset > 0) {
+				refIndexSize = (int) (out.size() - refIndexOffset);
+			}
+			refBytes = out.size();
+		}
+	}
+
+	private void finishLog() throws IOException {
+		if (logOffset > 0 && cur != null) {
+			logIndexOffset = finishBlockMaybeWriteIndex(logIndex);
+			logBytes = out.size() - refBytes;
+		}
+	}
+
+	private long finishBlockMaybeWriteIndex(BlockWriter idx)
+			throws IOException {
+		idx.addIndex(cur.lastKey(), out.size());
+		cur.writeTo(out);
+		cur = null;
+
+		int mustHaveIndex = idx == refIndex ? 4 : 1;
+		if (idx.entryCount() > mustHaveIndex) {
+			if (idx == refIndex) { // only pad before the ref_index
+				out.padBetweenBlocksToNextBlock();
+			}
+			long offset = out.size();
+			idx.writeTo(out);
+			return offset;
+		} else {
+			return 0;
+		}
+	}
+
 	private void writeFileHeader() throws IOException {
 		byte[] hdr = new byte[FILE_HEADER_LEN];
 		System.arraycopy(FILE_HEADER_MAGIC, 0, hdr, 0, 4);
-		NB.encodeInt32(hdr, 4, (VERSION_1 << 24) | blockSize);
+		NB.encodeInt32(hdr, 4, (VERSION_1 << 24) | refBlockSize);
 		out.write(hdr);
 	}
 
 	private void writeFileFooter() throws IOException {
-		byte[] ftr = new byte[FILE_FOOTER_LEN];
+		int ftrLen = FILE_FOOTER_LEN;
+		byte[] ftr = new byte[ftrLen];
 		System.arraycopy(FILE_HEADER_MAGIC, 0, ftr, 0, 4);
-		NB.encodeInt32(ftr, 4, (VERSION_1 << 24) | blockSize);
-		NB.encodeInt32(ftr, 8, idx.bytesInIndex());
+		NB.encodeInt32(ftr, 4, (VERSION_1 << 24) | refBlockSize);
+
+		NB.encodeInt64(ftr, 8, refIndexOffset);
+		NB.encodeInt64(ftr, 16, logOffset);
+		NB.encodeInt64(ftr, 24, logIndexOffset);
 
 		CRC32 crc = new CRC32();
-		crc.update(ftr, 0, 12);
-		NB.encodeInt32(ftr, 12, (int) crc.getValue());
+		crc.update(ftr, 0, ftrLen - 4);
+		NB.encodeInt32(ftr, ftrLen - 4, (int) crc.getValue());
 
 		out.write(ftr);
+		hash = out.finishFile();
+	}
+
+	/** @return statistics of the last written reftable. */
+	public Stats getStats() {
+		return stats;
 	}
 
 	/** Statistics about a written reftable. */
 	public static class Stats {
-		private final int blockSize;
+		private final ObjectId hash;
+		private final int refBlockSize;
+		private final int logBlockSize;
 		private final int restartInterval;
-		private final long totalBytes;
-		private final int blockCount;
-		private final long paddingUsed;
-		private final int indexKeys;
-		private final int indexSize;
 
-		Stats(ReftableWriter w, ReftableOutputStream o, IndexBlockWriter idx) {
-			blockSize = w.blockSize;
+		private final long refBytes;
+		private final long logBytes;
+		private final long paddingUsed;
+		private final long totalBytes;
+		private final int refBlocks;
+
+		private final int refIndexKeys;
+		private final int refIndexSize;
+
+		Stats(ReftableWriter w, ReftableOutputStream o, BlockWriter idx) {
+			hash = w.hash;
+			refBlockSize = w.refBlockSize;
+			logBlockSize = w.logBlockSize;
 			restartInterval = w.restartInterval;
-			totalBytes = o.size();
-			blockCount = o.blockCount();
+
+			refBytes = w.refBytes;
+			logBytes = w.logBytes;
 			paddingUsed = o.paddingUsed();
-			indexKeys = idx.bytesInIndex() > 0 ? idx.keysInIndex() : 0;
-			indexSize = idx.bytesInIndex();
+			totalBytes = o.size();
+			refBlocks = w.refBlocks;
+
+			refIndexKeys = w.refIndexOffset > 0 ? idx.entryCount() : 0;
+			refIndexSize = w.refIndexSize;
 		}
 
-		/** @return number of bytes in a single block. */
-		public int blockSize() {
-			return blockSize;
+		/** @return number of bytes in a ref block. */
+		public int refBlockSize() {
+			return refBlockSize;
+		}
+
+		/** @return number of bytes in a ref block. */
+		public int logBlockSize() {
+			return logBlockSize;
 		}
 
 		/** @return number of references between binary search markers. */
 		public int restartInterval() {
 			return restartInterval;
+		}
+
+		/** @return number of ref blocks in the output, excluding index. */
+		public int refBlockCount() {
+			return refBlocks;
+		}
+
+		/** @return number of bytes for references, including ref index. */
+		public long refBytes() {
+			return refBytes;
+		}
+
+		/** @return number of bytes for log, including log index. */
+		public long logBytes() {
+			return logBytes;
 		}
 
 		/** @return total number of bytes in the reftable. */
@@ -242,27 +397,27 @@ public class ReftableWriter {
 			return paddingUsed;
 		}
 
-		/** @return number of blocks in the output stream, excluding index. */
-		public int blockCount() {
-			return blockCount;
+		/** @return number of keys in the ref index; 0 if no index was used. */
+		public int refIndexKeys() {
+			return refIndexKeys;
 		}
 
-		/** @return number of keys in the index; 0 if no index was used. */
-		public int indexKeys() {
-			return indexKeys;
+		/** @return number of bytes in the ref index; 0 if no index was used. */
+		public int refIndexSize() {
+			return refIndexSize;
 		}
 
-		/** @return number of bytes in the index; 0 if no index was used. */
-		public int indexSize() {
-			return indexSize;
-		}
-
-		/** @return estimated number of disk seeks per read. */
+		/** @return estimated number of disk seeks per ref read. */
 		public double diskSeeksPerRead() {
-			if (indexKeys > 0) {
+			if (refIndexKeys() > 0) {
 				return 1;
 			}
-			return log(blockCount) / log(2);
+			return log(refBlockCount()) / log(2);
+		}
+
+		/** @return content hash of the reftable. */
+		public ObjectId hash() {
+			return hash;
 		}
 	}
 }
