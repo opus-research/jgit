f1d51fe6b8b97c1530df205c6a7d324ad92d6f23
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 12:55:31 +0000

    First commited as before_915_rev4

--- org.eclipse.jgit.http.test/tst/org/eclipse/jgit/http/test/HookMessageTest.java
+++ org.eclipse.jgit.http.test/tst/org/eclipse/jgit/http/test/HookMessageTest.java
@@ -61,10 +61,11 @@ import org.eclipse.jgit.http.test.util.AccessEvent;
 import org.eclipse.jgit.http.test.util.HttpTestCase;
 import org.eclipse.jgit.junit.TestRepository;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.FileBasedConfig;
+import org.eclipse.jgit.lib.FileRepository;
 import org.eclipse.jgit.lib.NullProgressMonitor;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.RepositoryConfig;
 import org.eclipse.jgit.revwalk.RevBlob;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.transport.PreReceiveHook;
@@ -76,14 +77,14 @@ import org.eclipse.jgit.transport.Transport;
 import org.eclipse.jgit.transport.URIish;
 
 public class HookMessageTest extends HttpTestCase {
-	private Repository remoteRepository;
+	private FileRepository remoteRepository;
 
 	private URIish remoteURI;
 
 	protected void setUp() throws Exception {
 		super.setUp();
 
-		final TestRepository src = createTestRepository();
+		final TestRepository<FileRepository> src = createTestRepository();
 		final String srcName = src.getRepository().getDirectory().getName();
 
 		ServletContextHandler app = server.addContext("/git");
@@ -124,7 +125,7 @@ public class HookMessageTest extends HttpTestCase {
 		remoteRepository = src.getRepository();
 		remoteURI = toURIish(app, srcName);
 
-		RepositoryConfig cfg = remoteRepository.getConfig();
+		FileBasedConfig cfg = remoteRepository.getConfig();
 		cfg.setBoolean("http", null, "receivepack", true);
 		cfg.save();
 	}
--- org.eclipse.jgit/src/org/eclipse/jgit/lib/RefDirectory.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/RefDirectory.java
@@ -409,12 +409,8 @@ public class RefDirectory extends RefDatabase {
 		RevObject obj = rw.parseAny(leaf.getObjectId());
 		ObjectIdRef newLeaf;
 		if (obj instanceof RevTag) {
-			do {
-				obj = rw.parseAny(((RevTag) obj).getObject());
-			} while (obj instanceof RevTag);
-
 			newLeaf = new ObjectIdRef.PeeledTag(leaf.getStorage(), leaf
-					.getName(), leaf.getObjectId(), obj.copy());
+					.getName(), leaf.getObjectId(), rw.peel(obj).copy());
 		} else {
 			newLeaf = new ObjectIdRef.PeeledNonTag(leaf.getStorage(), leaf
 					.getName(), leaf.getObjectId());
@@ -590,7 +586,7 @@ public class RefDirectory extends RefDatabase {
 	}
 
 	private boolean isLogAllRefUpdates() {
-		return parent.getConfig().getCore().isLogAllRefUpdates();
+		return parent.getConfig().get(CoreConfig.KEY).isLogAllRefUpdates();
 	}
 
 	private boolean shouldAutoCreateLog(final String refName) {
