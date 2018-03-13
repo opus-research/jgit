41b4577370bb78c6c84dbc215b92fee9072fd422
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:36:19 +0000

    First commited as before_8113_rev17

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2010, Sasa Zivkov <sasa.zivkov@sap.com>
- * Copyright (c) 2012, Research In Motion Limited
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -349,8 +348,6 @@ public class JGitText extends TranslationBundle {
 	/***/ public String mergeStrategyAlreadyExistsAsDefault;
 	/***/ public String mergeStrategyDoesNotSupportHeads;
 	/***/ public String mergeUsingStrategyResultedInDescription;
-	/***/ public String mergeRecursiveReturnedNoCommit;
-	/***/ public String mergeRecursiveTooManyMergeBasesFor;
 	/***/ public String minutesAgo;
 	/***/ public String missingAccesskey;
 	/***/ public String missingConfigurationForKey;
