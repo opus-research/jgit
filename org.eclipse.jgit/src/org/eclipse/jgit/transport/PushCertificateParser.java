f504f57d864f6262db57daef4b9f89e3685ac943
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:35:47 +0000

    First commited as before_51629_rev3

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/PushCertificateParser.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/PushCertificateParser.java
@@ -204,13 +204,7 @@ public class PushCertificateParser {
 	private final NonceGenerator nonceGenerator;
 	private final List<ReceiveCommand> commands = new ArrayList<>();
 
-	/**
-	 * @param into
-	 *            destination repository for the push.
-	 * @param cfg
-	 *            configuration for signed push.
-	 */
-	public PushCertificateParser(Repository into, SignedPushConfig cfg) {
+	PushCertificateParser(Repository into, SignedPushConfig cfg) {
 		if (cfg != null) {
 			nonceSlopLimit = cfg.getCertNonceSlopLimit();
 			nonceGenerator = cfg.getNonceGenerator();
