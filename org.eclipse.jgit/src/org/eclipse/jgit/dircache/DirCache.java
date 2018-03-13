13973e66b3557964b572b001404121edb919e044
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:18:07 +0000

    First commited as before_36968_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCache.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCache.java
@@ -672,17 +672,13 @@ public class DirCache {
 
 		if (writeTree) {
 			TemporaryBuffer bb = new TemporaryBuffer.LocalFile(dir, 5 << 20);
-			try {
-				tree.write(tmp, bb);
-				bb.close();
+			tree.write(tmp, bb);
+			bb.close();
 
-				NB.encodeInt32(tmp, 0, EXT_TREE);
-				NB.encodeInt32(tmp, 4, (int) bb.length());
-				dos.write(tmp, 0, 8);
-				bb.writeTo(dos, null);
-			} finally {
-				bb.destroy();
-			}
+			NB.encodeInt32(tmp, 0, EXT_TREE);
+			NB.encodeInt32(tmp, 4, (int) bb.length());
+			dos.write(tmp, 0, 8);
+			bb.writeTo(dos, null);
 		}
 		writeIndexChecksum = foot.digest();
 		os.write(writeIndexChecksum);
