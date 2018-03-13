bb84442f41390dfdbb1ac4bbbd564ac2486a5917
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:11:00 +0000

    First commited as before_3521_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/api/CleanCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/CleanCommand.java
@@ -47,6 +47,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.eclipse.jgit.lib.Repository;
@@ -79,15 +80,15 @@ public class CleanCommand extends GitCommand<Set<String>> {
 	 * @return a set of strings representing each file cleaned.
 	 */
 	public Set<String> call() {
-		Set<String> files = Collections.emptySet();
+		Set<String> files = new TreeSet<String>();
 		try {
 			StatusCommand command = new StatusCommand(repo);
 			Status status = command.call();
-			files = status.getUntracked();
-
-			for (String file : files) {
-				if (paths.isEmpty() || paths.contains(file))
+			for (String file : status.getUntracked()) {
+				if (paths.isEmpty() || paths.contains(file)) {
 					FileUtils.delete(new File(repo.getWorkTree(), file));
+					files.add(file);
+				}
 			}
 		} catch (IOException e) {
 			throw new JGitInternalException(e.getMessage(), e);
@@ -106,4 +107,5 @@ public class CleanCommand extends GitCommand<Set<String>> {
 		this.paths = paths;
 		return this;
 	}
+
 }
