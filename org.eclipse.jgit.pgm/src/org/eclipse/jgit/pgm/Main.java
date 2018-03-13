4c3575a9baed6110e3e0337f57fe9749121e64d9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:50:24 +0000

    First commited as before_62692_rev1

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Main.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Main.java
@@ -62,8 +62,6 @@ import org.eclipse.jgit.lib.RepositoryBuilder;
 import org.eclipse.jgit.pgm.internal.CLIText;
 import org.eclipse.jgit.pgm.opt.CmdLineParser;
 import org.eclipse.jgit.pgm.opt.SubcommandHandler;
-import org.eclipse.jgit.transport.HttpTransport;
-import org.eclipse.jgit.transport.http.apache.HttpClientConnectionFactory;
 import org.eclipse.jgit.util.CachedAuthenticator;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
@@ -97,7 +95,6 @@ public class Main {
 	 *            arguments.
 	 */
 	public static void main(final String[] argv) {
-		HttpTransport.setConnectionFactory(new HttpClientConnectionFactory());
 		new Main().run(argv);
 	}
 
