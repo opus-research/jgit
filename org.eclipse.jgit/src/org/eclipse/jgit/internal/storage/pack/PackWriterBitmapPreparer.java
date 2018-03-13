b848125fe96c66cc63949c6b75274cf99db4f9cb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:44:44 +0000

    First commited as before_59541_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/PackWriterBitmapPreparer.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/PackWriterBitmapPreparer.java
@@ -154,11 +154,6 @@ class PackWriterBitmapPreparer {
 		 * majority of calculations performed.
 		 */
 		pm.beginTask(JGitText.get().selectingCommits, ProgressMonitor.UNKNOWN);
-
-		// Build bitmaps for all of the branch tips and set up reuse of bitmaps
-		// from the previous pack. setupTipCommitBitmaps() also marks the
-		// commits corresponding to reused bitmaps as "uninteresting" in the
-		// RevWalk so that only new commits will be discovered.
 		RevWalk rw = new RevWalk(reader);
 		rw.setRetainBody(false);
 		CommitSelectionHelper selectionHelper = setupTipCommitBitmaps(rw,
@@ -183,8 +178,8 @@ class PackWriterBitmapPreparer {
 		int totalWants = selectionHelper.peeledWants.size();
 
 		for (BitmapBuilderEntry entry : selectionHelper.tipCommitBitmaps) {
-			BitmapBuilder tipBitmap = entry.getBuilder();
-			int cardinality = tipBitmap.cardinality();
+			BitmapBuilder bitmap = entry.getBuilder();
+			int cardinality = bitmap.cardinality();
 
 			// Within this branch, keep ordered lists of commits representing
 			// chains in its history, where each chain is a "sub-branch".
@@ -224,7 +219,7 @@ class PackWriterBitmapPreparer {
 				}
 
 				// Ignore commits that are not in this branch
-				if (!tipBitmap.contains(c)) {
+				if (!bitmap.contains(c)) {
 					continue;
 				}
 
@@ -260,25 +255,18 @@ class PackWriterBitmapPreparer {
 				nextFlg = nextIn == distantCommitSpan
 						? PackBitmapIndex.FLAG_REUSE : 0;
 
-				// Perform a revwalk to get the set of all reachable commits
-				// from the current commit, excluding reused commits.
-				// PackWriterBitmapWalker.newRevFilter() adds all of those
-				// reachable commits to currentCommitBitmap, which for
-				// efficiency has writeBitmaps as a backing index (via
-				// commitBitmapIndex--it's overly subtle).
-				BitmapBuilder currentCommitBitmap = commitBitmapIndex
-						.newBitmapBuilder();
+				BitmapBuilder fullBitmap = commitBitmapIndex.newBitmapBuilder();
 				rw.reset();
 				rw.markStart(c);
 				for (AnyObjectId objectId : selectionHelper.reusedCommits) {
 					rw.markUninteresting(rw.parseCommit(objectId));
 				}
-				rw.setRevFilter(PackWriterBitmapWalker.newRevFilter(null,
-						currentCommitBitmap));
+				rw.setRevFilter(
+						PackWriterBitmapWalker.newRevFilter(null, fullBitmap));
 
 				while (rw.next() != null) {
 					// The RevFilter adds the reachable commits from this
-					// selected commit to currentCommitBitmap.
+					// selected commit to fullBitmap.
 				}
 
 				// Sort the commits by independent chains in this branch's
@@ -286,7 +274,7 @@ class PackWriterBitmapPreparer {
 				List<BitmapCommit> longestAncestorChain = null;
 				for (List<BitmapCommit> chain : chains) {
 					BitmapCommit mostRecentCommit = chain.get(chain.size() - 1);
-					if (currentCommitBitmap.contains(mostRecentCommit)) {
+					if (fullBitmap.contains(mostRecentCommit)) {
 						if (longestAncestorChain == null
 								|| longestAncestorChain.size() < chain.size()) {
 							longestAncestorChain = chain;
@@ -300,10 +288,7 @@ class PackWriterBitmapPreparer {
 				}
 				longestAncestorChain.add(new BitmapCommit(
 						c, !longestAncestorChain.isEmpty(), flags));
-
-				// Adding the newly seen commits to writeBitmaps makes the
-				// next revwalk to fill in currentCommiBitmap more efficient.
-				writeBitmaps.addBitmap(c, currentCommitBitmap, 0);
+				writeBitmaps.addBitmap(c, fullBitmap, 0);
 			}
 
 			for (List<BitmapCommit> chain : chains) {
