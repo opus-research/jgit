7a60ddfce60db258c2d7a804014f7323b85b78ab
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:53:31 +0000

    First commited as before_14871_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/BasePackFetchConnection.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/BasePackFetchConnection.java
@@ -63,7 +63,6 @@ import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.Config.SectionParser;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.MutableObjectId;
-import org.eclipse.jgit.lib.NullProgressMonitor;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
 import org.eclipse.jgit.lib.ProgressMonitor;
@@ -222,8 +221,6 @@ public abstract class BasePackFetchConnection extends BasePackConnection
 
 	private boolean noDone;
 
-	private boolean noProgress;
-
 	private String lockMessage;
 
 	private PackLock packLock;
@@ -323,9 +320,7 @@ public abstract class BasePackFetchConnection extends BasePackConnection
 	 * Execute common ancestor negotiation and fetch the objects.
 	 *
 	 * @param monitor
-	 *            progress monitor to receive status updates. If the monitor is
-	 *            the {@link NullProgressMonitor#INSTANCE}, then the no-progress
-	 *            option enabled.
+	 *            progress monitor to receive status updates.
 	 * @param want
 	 *            the advertised remote references the caller wants to fetch.
 	 * @param have
@@ -342,8 +337,6 @@ public abstract class BasePackFetchConnection extends BasePackConnection
 			final Collection<Ref> want, final Set<ObjectId> have,
 			OutputStream outputStream) throws TransportException {
 		try {
-			noProgress = monitor == NullProgressMonitor.INSTANCE;
-
 			markRefsAdvertised();
 			markReachable(have, maxTimeWanted(want));
 
@@ -487,8 +480,6 @@ public abstract class BasePackFetchConnection extends BasePackConnection
 
 	private String enableCapabilities() throws TransportException {
 		final StringBuilder line = new StringBuilder();
-		if (noProgress)
-			wantCapability(line, OPTION_NO_PROGRESS);
 		if (includeTags)
 			includeTags = wantCapability(line, OPTION_INCLUDE_TAG);
 		if (allowOfsDelta)
