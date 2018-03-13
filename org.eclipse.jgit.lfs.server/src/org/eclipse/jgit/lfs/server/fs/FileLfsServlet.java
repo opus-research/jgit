bb2fc7aef39e58274b0492fa0e702f67a0e03f27
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:21:36 +0000

    First commited as before_85021_rev2

--- org.eclipse.jgit.lfs.server/src/org/eclipse/jgit/lfs/server/fs/FileLfsServlet.java
+++ org.eclipse.jgit.lfs.server/src/org/eclipse/jgit/lfs/server/fs/FileLfsServlet.java
@@ -184,7 +184,6 @@ public class FileLfsServlet extends HttpServlet {
 	 *            error message
 	 * @throws IOException
 	 *             on failure to send the response
-	 * @since 4.6
 	 */
 	protected static void sendError(HttpServletResponse rsp, int status, String message)
 			throws IOException {
