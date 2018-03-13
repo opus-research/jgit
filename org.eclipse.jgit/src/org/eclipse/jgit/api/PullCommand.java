4cd92223b3c323e3549bbed439752c44761159a4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:40:23 +0000

    First commited as before_99262_rev7

--- org.eclipse.jgit/src/org/eclipse/jgit/api/PullCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/PullCommand.java
@@ -99,8 +99,6 @@ public class PullCommand extends TransportCommand<PullCommand, PullResult> {
 
 	private TagOpt tagOption;
 
-	private FastForwardMode fastForwardMode;
-
 	private FetchRecurseSubmodulesMode submoduleRecurseMode = null;
 
 	/**
@@ -352,9 +350,11 @@ public class PullCommand extends TransportCommand<PullCommand, PullResult> {
 			result = new PullResult(fetchRes, remote, rebaseRes);
 		} else {
 			MergeCommand merge = new MergeCommand(repo);
-			MergeResult mergeRes = merge.include(upstreamName, commitToMerge)
-					.setStrategy(strategy).setProgressMonitor(monitor)
-					.setFastForward(getFastForwardMode()).call();
+			merge.include(upstreamName, commitToMerge);
+			merge.setStrategy(strategy);
+			merge.setProgressMonitor(monitor);
+			merge.setFastForward(getFastForwardMode());
+			MergeResult mergeRes = merge.call();
 			monitor.update(1);
 			result = new PullResult(fetchRes, remote, mergeRes);
 		}
@@ -437,35 +437,15 @@ public class PullCommand extends TransportCommand<PullCommand, PullResult> {
 	}
 
 	/**
-	 * Sets the fast forward mode. It is used if pull is configured to do a
-	 * merge as opposed to rebase. If non-{@code null} takes precedence over the
-	 * fast-forward mode configured in git config.
-	 *
-	 * @param fastForwardMode
-	 *            corresponds to the --ff/--no-ff/--ff-only options. If
-	 *            {@code null} use the value of {@code pull.ff} configured in
-	 *            git config. If {@code pull.ff} is not configured fall back to
-	 *            the value of {@code merge.ff}. If {@code merge.ff} is not
-	 *            configured --ff is the built-in default.
-	 * @return {@code this}
-	 * @since 4.9
-	 */
-	public PullCommand setFastForward(
-			@Nullable FastForwardMode fastForwardMode) {
-		checkCallable();
-		this.fastForwardMode = fastForwardMode;
-		return this;
-	}
-
-	/**
 	 * Set the mode to be used for recursing into submodules.
 	 *
 	 * @param recurse
 	 * @return {@code this}
 	 * @since 4.7
+	 * @see FetchCommand#setRecurseSubmodules(FetchRecurseSubmodulesMode)
 	 */
 	public PullCommand setRecurseSubmodules(
-			FetchRecurseSubmodulesMode recurse) {
+			@Nullable FetchRecurseSubmodulesMode recurse) {
 		this.submoduleRecurseMode = recurse;
 		return this;
 	}
@@ -497,9 +477,6 @@ public class PullCommand extends TransportCommand<PullCommand, PullResult> {
 	}
 
 	private FastForwardMode getFastForwardMode() {
-		if (fastForwardMode != null) {
-			return fastForwardMode;
-		}
 		Config config = repo.getConfig();
 		Merge ffMode = config.getEnum(Merge.values(),
 				ConfigConstants.CONFIG_PULL_SECTION, null,
