50d47b098c5193625c4d0d82c82be8aa4b0a29ac
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:07:28 +0000

    First commited as before_75357_rev5

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Config.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Config.java
@@ -67,7 +67,6 @@ import org.eclipse.jgit.events.ConfigChangedListener;
 import org.eclipse.jgit.events.ListenerHandle;
 import org.eclipse.jgit.events.ListenerList;
 import org.eclipse.jgit.internal.JGitText;
-import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.IO;
 import org.eclipse.jgit.util.RawParseUtils;
 import org.eclipse.jgit.util.StringUtils;
@@ -1118,9 +1117,9 @@ public class Config {
 			throw new ConfigInvalidException(
 					JGitText.get().invalidLineInConfigFile);
 		}
-		File toInclude = getIncludedFile(line.value);
+		File path = new File(line.value);
 		try {
-			byte[] bytes = IO.readFully(toInclude);
+			byte[] bytes = IO.readFully(path);
 			String decoded;
 			if (isUtf8(bytes)) {
 				decoded = RawParseUtils.decode(RawParseUtils.UTF8_CHARSET,
@@ -1130,25 +1129,15 @@ public class Config {
 			}
 			newEntries.addAll(fromTextRecurse(decoded, depth + 1));
 		} catch (FileNotFoundException fnfe) {
-			if (toInclude.exists()) {
-				throw new ConfigInvalidException(MessageFormat.format(
-						JGitText.get().cannotReadFile, toInclude), fnfe);
+			if (path.exists()) {
+				throw new ConfigInvalidException(MessageFormat
+						.format(JGitText.get().cannotReadFile, path), fnfe);
 			}
 		} catch (IOException ioe) {
-			throw new ConfigInvalidException(MessageFormat
-					.format(JGitText.get().cannotReadFile, toInclude), ioe);
+			throw new ConfigInvalidException(
+					MessageFormat.format(JGitText.get().cannotReadFile, path),
+					ioe);
 		}
-	}
-
-	private File getIncludedFile(String path) {
-		File file = new File(path);
-		if (file.getPath().startsWith("~/")) { //$NON-NLS-1$
-			File userHome = FS.DETECTED.userHome();
-			if (userHome != null) {
-				file = new File(userHome, file.getPath().substring(2));
-			}
-		}
-		return file;
 	}
 
 	private ConfigSnapshot newState() {
