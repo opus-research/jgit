7cd06497d0d27261bbd9ab4cd121198f5676274a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:29:40 +0000

    First commited as before_6362_rev5

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/CLIText.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/CLIText.java
@@ -93,8 +93,6 @@ public class CLIText extends TranslationBundle {
 	/***/ public String dateInfo;
 	/***/ public String deletedBranch;
 	/***/ public String deletedRemoteBranch;
-	/***/ public String didSubscribe;
-	/***/ public String didUnsubscribe;
 	/***/ public String doesNotExist;
 	/***/ public String dontOverwriteLocalChanges;
 	/***/ public String everythingUpToDate;
@@ -156,8 +154,6 @@ public class CLIText extends TranslationBundle {
 	/***/ public String mostCommonlyUsedCommandsAre;
 	/***/ public String needApprovalToDestroyCurrentRepository;
 	/***/ public String noGitRepositoryConfigured;
-	/***/ public String noRemoteUriSubscribe;
-	/***/ public String noRemoteUriUnsubscribe;
 	/***/ public String noSuchFile;
 	/***/ public String noTREESectionInIndex;
 	/***/ public String nonFastForward;
@@ -184,7 +180,6 @@ public class CLIText extends TranslationBundle {
 	/***/ public String repaint;
 	/***/ public String serviceNotSupported;
 	/***/ public String skippingObject;
-	/***/ public String subscriptionDoesNotExist;
 	/***/ public String timeInMilliSeconds;
 	/***/ public String tooManyRefsGiven;
 	/***/ public String unknownMergeStratey;
