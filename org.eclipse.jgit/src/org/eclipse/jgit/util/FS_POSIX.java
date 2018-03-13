92935643ce9fe6a6f2d52d47a90e4ab6eb2f3f03
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:06:44 +0000

    First commited as before_2451_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/util/FS_POSIX.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/FS_POSIX.java
@@ -66,12 +66,7 @@ abstract class FS_POSIX extends FS {
 			String w = readPipe(userHome(), //
 					new String[] { "bash", "--login", "-c", "which git" }, //
 					Charset.defaultCharset().name());
-			if (w == null || w.length() == 0)
-				return null;
-			File parentFile = new File(w).getParentFile();
-			if (parentFile == null)
-				return null;
-			return parentFile.getParentFile();
+			return new File(w).getParentFile().getParentFile();
 		}
 
 		return null;
