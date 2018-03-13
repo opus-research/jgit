4b4db5cbbb823cf3b5bb2ae0e253155f0b7a37d6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 12:57:25 +0000

    First commited as before_1212_rev1

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/treewalk/FileTreeIteratorWithTimeControl.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/treewalk/FileTreeIteratorWithTimeControl.java
@@ -65,7 +65,7 @@ import org.eclipse.jgit.util.FS;
  * This class was written especially to test racy-git problems
  */
 public class FileTreeIteratorWithTimeControl extends FileTreeIterator {
-	private TreeSet<Long> modTimes;
+	private TreeSet<Long> modTimes = new TreeSet<Long>();
 
 	public FileTreeIteratorWithTimeControl(FileTreeIterator p, Repository repo,
 			TreeSet<Long> modTimes) {
@@ -99,9 +99,7 @@ public class FileTreeIteratorWithTimeControl extends FileTreeIterator {
 
 	@Override
 	public long getEntryLastModified() {
-		if (modTimes == null)
-			return 0;
-		Long cutOff = Long.valueOf(super.getEntryLastModified() + 1);
+		Long cutOff = Long.valueOf(super.getEntryLastModified());
 		SortedSet<Long> head = modTimes.headSet(cutOff);
 		return head.isEmpty() ? 0 : head.last().longValue();
 	}
