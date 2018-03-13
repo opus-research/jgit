fddfd2dc25ff8a11f3b73b413f15ebcfb6acceda
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 16:06:41 +0000

    First commited as before_111364_rev5

--- org.eclipse.jgit/src/org/eclipse/jgit/hooks/PrePushHook.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/hooks/PrePushHook.java
@@ -127,13 +127,6 @@ public class PrePushHook extends GitHook<String> {
 	}
 
 	/**
-	 * @return remote name or null
-	 */
-	protected String getRemoteName() {
-		return remoteName;
-	}
-
-	/**
 	 * @param location
 	 */
 	public void setRemoteLocation(String location) {
