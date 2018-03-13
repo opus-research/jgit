97d51e3b74a0eef11870ef322b7fbf5f13f37a54
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:29:48 +0000

    First commited as before_6377_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/merge/ResolveMerger.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/merge/ResolveMerger.java
@@ -78,6 +78,7 @@ import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectInserter;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
@@ -130,6 +131,8 @@ public class ResolveMerger extends ThreeWayMerger {
 
 	private Map<String, MergeFailureReason> failingPaths = new HashMap<String, MergeFailureReason>();
 
+	private ObjectInserter oi;
+
 	private boolean enterSubtree;
 
 	private boolean inCore;
@@ -152,6 +155,7 @@ public class ResolveMerger extends ThreeWayMerger {
 				SupportedAlgorithm.HISTOGRAM);
 		mergeAlgorithm = new MergeAlgorithm(DiffAlgorithm.getAlgorithm(diffAlg));
 		commitNames = new String[] { "BASE", "OURS", "THEIRS" };
+		oi = getObjectInserter();
 		this.inCore = inCore;
 
 		if (inCore) {
@@ -222,7 +226,7 @@ public class ResolveMerger extends ThreeWayMerger {
 			}
 
 			if (getUnmergedPaths().isEmpty()) {
-				resultTree = dircache.writeTree(getObjectInserter());
+				resultTree = dircache.writeTree(oi);
 				return true;
 			} else {
 				resultTree = null;
@@ -620,8 +624,7 @@ public class ResolveMerger extends ThreeWayMerger {
 			dce.setLength((int) of.length());
 			InputStream is = new FileInputStream(of);
 			try {
-				dce.setObjectId(getObjectInserter().insert(
-				    Constants.OBJ_BLOB, of.length(), is));
+				dce.setObjectId(oi.insert(Constants.OBJ_BLOB, of.length(), is));
 			} finally {
 				is.close();
 				if (inCore)
