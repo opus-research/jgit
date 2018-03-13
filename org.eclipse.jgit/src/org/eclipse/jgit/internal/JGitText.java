d973d9d2f14e01895a439df77dc51d4c4f1f88ce
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:06:07 +0000

    First commited as before_74570_rev9

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -419,6 +419,8 @@ public class JGitText extends TranslationBundle {
 	/***/ public String invalidStageForPath;
 	/***/ public String invalidTagOption;
 	/***/ public String invalidTimeout;
+	/***/ public String invalidTimeUnitValue2;
+	/***/ public String invalidTimeUnitValue3;
 	/***/ public String invalidURL;
 	/***/ public String invalidWildcards;
 	/***/ public String invalidRefSpec;
@@ -555,7 +557,6 @@ public class JGitText extends TranslationBundle {
 	/***/ public String pushCertificateInvalidSignature;
 	/***/ public String pushIsNotSupportedForBundleTransport;
 	/***/ public String pushNotPermitted;
-	/***/ public String pushOptionsNotSupported;
 	/***/ public String rawLogMessageDoesNotParseAsLogEntry;
 	/***/ public String readingObjectsFromLocalRepositoryFailed;
 	/***/ public String readTimedOut;
