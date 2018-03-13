2725575d93a95997c98ec8c10c21183634f19897
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:02:38 +0000

    First commited as before_1968_rev3

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/IndexDiff.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/IndexDiff.java
@@ -65,17 +65,15 @@ import org.eclipse.jgit.treewalk.filter.SkipWorkTreeFilter;
 import org.eclipse.jgit.treewalk.filter.TreeFilter;
 
 /**
- * Compares the index, a tree, and the working directory Ignored files are not
- * taken into account. The following information is retrieved:
- * <ul>
- * <li>added files</li>
- * <li>changed files</li>
- * <li>removed files</li>
- * <li>missing files</li>
- * <li>modified files</li>
- * <li>untracked files</li>
- * <li>files with assume-unchanged flag</li>
- * </ul>
+ * Compares the index, a tree, and the working directory
+ * Ignored files are not taken into account.
+ * The following information is retrieved:
+ * <li> added files
+ * <li> changed files
+ * <li> removed files
+ * <li> missing files
+ * <li> modified files
+ * <li> untracked files
  */
 public class IndexDiff {
 
@@ -104,8 +102,6 @@ public class IndexDiff {
 	private Set<String> modified = new HashSet<String>();
 
 	private Set<String> untracked = new HashSet<String>();
-
-	private HashSet<String> assumeUnchanged = new HashSet<String>();
 
 	/**
 	 * Construct an IndexDiff
@@ -195,10 +191,6 @@ public class IndexDiff {
 					WorkingTreeIterator.class);
 			FileMode fileModeTree = treeWalk.getFileMode(TREE);
 
-			if (dirCacheIterator != null) {
-				if (dirCacheIterator.getDirCacheEntry().isAssumeValid())
-					assumeUnchanged.add(dirCacheIterator.getEntryPathString());
-			}
 			if (treeIterator != null) {
 				if (dirCacheIterator != null) {
 					if (!treeIterator.getEntryObjectId().equals(
@@ -289,13 +281,6 @@ public class IndexDiff {
 	 */
 	public Set<String> getUntracked() {
 		return untracked;
-	}
-
-	/**
-	 * @return list of files with the flag assume-unchanged
-	 */
-	public Set<String> getAssumeUnchanged() {
-		return assumeUnchanged;
 	}
 
 }
