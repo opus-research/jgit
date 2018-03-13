930c974c5d93744e3f82adfef9fcc7b17c890b73
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:03:20 +0000

    First commited as before_71984_rev7

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Constants.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Constants.java
@@ -392,14 +392,7 @@ public final class Constants {
 	public static final String ATTR_FILTER_TYPE_SMUDGE = "smudge";
 
 	/**
-	 * whether to use JGits builtin implementation of filters and hooks
-	 *
-	 * @since 4.5
-	 */
-	public static final String ATTR_FILTER_USE_BUILTIN = "useJGitBuiltin";
-
-	/**
-	 * Builtin commands are starting with this prefix
+	 * Builtin filter commands are starting with this prefix
 	 *
 	 * @since 4.5
 	 */
