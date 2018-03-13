04c01fba76d28050b60a7b52817e5bb350ecc5a7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:00:14 +0000

    First commited as before_1614_rev23

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/CoreConfig.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/CoreConfig.java
@@ -1,5 +1,4 @@
 /*
- * Copyright (C) 2013, Gunnar Wagenknecht
  * Copyright (C) 2010, Chris Aniszczyk <caniszczyk@gmail.com>
  * Copyright (C) 2009, Christian Halstrick <christian.halstrick@sap.com>
  * Copyright (C) 2009, Google Inc.
@@ -102,8 +101,6 @@ public class CoreConfig {
 
 	private final String excludesfile;
 
-	private final String attributesfile;
-
 	/**
 	 * Options for symlink handling
 	 *
@@ -139,8 +136,6 @@ public class CoreConfig {
 				ConfigConstants.CONFIG_KEY_LOGALLREFUPDATES, true);
 		excludesfile = rc.getString(ConfigConstants.CONFIG_CORE_SECTION, null,
 				ConfigConstants.CONFIG_KEY_EXCLUDESFILE);
-		attributesfile = rc.getString(ConfigConstants.CONFIG_CORE_SECTION,
-				null, ConfigConstants.CONFIG_KEY_ATTRIBUTESFILE);
 	}
 
 	/**
@@ -169,13 +164,5 @@ public class CoreConfig {
 	 */
 	public String getExcludesFile() {
 		return excludesfile;
-	}
-
-	/**
-	 * @return path of attributesfile
-	 * @since 3.7
-	 */
-	public String getAttributesFile() {
-		return attributesfile;
 	}
 }
