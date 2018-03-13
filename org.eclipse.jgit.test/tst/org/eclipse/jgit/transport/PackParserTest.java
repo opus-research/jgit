218852e59e75917976b7e16f02928f8120ea9ec3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 16:06:44 +0000

    First commited as before_111374_rev1

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/PackParserTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/PackParserTest.java
@@ -47,6 +47,7 @@
 package org.eclipse.jgit.transport;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
