e0cc537c87cfbdecd92a4d3a8a5861b2816d888d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:02:24 +0000

    First commited as before_1910_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/api/RebaseResult.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/RebaseResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008, Google Inc.
+ * Copyright (C) 2010, Mathias Kinzler <mathias.kinzler@sap.com>
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -40,59 +40,65 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.eclipse.jgit.api;
 
-package org.eclipse.jgit.pgm;
+import org.eclipse.jgit.revwalk.RevCommit;
 
-import java.io.File;
-
-import org.kohsuke.args4j.Argument;
-import org.kohsuke.args4j.Option;
-import org.kohsuke.args4j.spi.StopOptionHandler;
-import org.eclipse.jgit.dircache.DirCache;
-import org.eclipse.jgit.dircache.DirCacheBuildIterator;
-import org.eclipse.jgit.dircache.DirCacheBuilder;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.FileMode;
-import org.eclipse.jgit.pgm.opt.PathTreeFilterHandler;
-import org.eclipse.jgit.treewalk.TreeWalk;
-import org.eclipse.jgit.treewalk.filter.TreeFilter;
-
-@Command(usage = "usage_StopTrackingAFile", common = true)
-class Rm extends TextBuiltin {
-	@Argument(metaVar = "metaVar_path", usage = "usage_path", multiValued = true, required = true, handler = PathTreeFilterHandler.class)
-	@Option(name = "--", handler = StopOptionHandler.class)
-	private TreeFilter paths;
-
-	private File root;
-
-	@Override
-	protected void run() throws Exception {
-		root = db.getWorkTree();
-
-		final DirCache dirc = db.lockDirCache();
-		final DirCacheBuilder edit = dirc.builder();
-
-		final TreeWalk walk = new TreeWalk(db);
-		walk.reset(); // drop the first empty tree, which we do not need here
-		walk.setRecursive(true);
-		walk.setFilter(paths);
-		walk.addTree(new DirCacheBuildIterator(edit));
-
-		while (walk.next()) {
-			final File path = new File(root, walk.getPathString());
-			final FileMode mode = walk.getFileMode(0);
-			if (mode.getObjectType() == Constants.OBJ_BLOB) {
-				// Deleting a blob is simply a matter of removing
-				// the file or symlink named by the tree entry.
-				delete(path);
-			}
-		}
-
-		edit.commit();
+/**
+ * The result of a {@link RebaseCommand} execution
+ */
+public class RebaseResult {
+	/**
+	 * The overall status
+	 */
+	public enum Status {
+		/**
+		 * Rebase was successful, HEAD points to the new commit
+		 */
+		OK,
+		/**
+		 * Aborted; the original HEAD was restored
+		 */
+		ABORTED,
+		/**
+		 * Stopped due to a conflict; must either abort or resolve or skip
+		 */
+		STOPPED,
+		/**
+		 * Already up-to-date
+		 */
+		UP_TO_DATE;
 	}
 
-	private void delete(File p) {
-		while (p != null && !p.equals(root) && p.delete())
-			p = p.getParentFile();
+	static final RebaseResult UP_TO_DATE_RESULT = new RebaseResult(
+			Status.UP_TO_DATE);
+
+	private final Status mySatus;
+
+	private final RevCommit currentCommit;
+
+	RebaseResult(Status status) {
+		this.mySatus = status;
+		currentCommit = null;
+	}
+
+	RebaseResult(RevCommit commit) {
+		this.mySatus = Status.STOPPED;
+		currentCommit = commit;
+	}
+
+	/**
+	 * @return the overall status
+	 */
+	public Status getStatus() {
+		return mySatus;
+	}
+
+	/**
+	 * @return the current commit if status is {@link Status#STOPPED}, otherwise
+	 *         <code>null</code>
+	 */
+	public RevCommit getCurrentCommit() {
+		return currentCommit;
 	}
 }
