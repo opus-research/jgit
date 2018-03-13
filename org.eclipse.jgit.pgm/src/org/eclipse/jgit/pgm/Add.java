7073402de71f812faa20586032e23435c0cf3d2f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:02:03 +0000

    First commited as before_1863_rev2

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Add.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Add.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
+ * Copyright (C) 2010, Sasa Zivkov <sasa.zivkov@sap.com>
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -41,84 +41,31 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.eclipse.jgit.pgm.debug;
+package org.eclipse.jgit.pgm;
 
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.Git;
+import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
-import org.eclipse.jgit.pgm.CLIText;
-import org.eclipse.jgit.pgm.Command;
-import org.eclipse.jgit.pgm.CommandCatalog;
-import org.eclipse.jgit.pgm.CommandRef;
-import org.eclipse.jgit.pgm.TextBuiltin;
 
-@Command(usage = "usage_displayAListOfAllRegisteredJgitCommands")
-class ShowCommands extends TextBuiltin {
-	@Option(name = "--pretty", usage = "usage_alterTheDetailShown")
-	private Format pretty = Format.USAGE;
+@Command(common = true, usage = "usage_addFileContentsToTheIndex")
+class Add extends TextBuiltin {
+
+	@Option(name = "--update", aliases = { "-u" }, usage = "usage_onlyMatchAgainstAlreadyTrackedFiles")
+	private boolean update = false;
+
+	@Argument(required = true, metaVar = "metavar_filepattern", usage = "usage_filesToAddContentFrom")
+	private List<String> filepatterns = new ArrayList<String>();
 
 	@Override
 	protected void run() throws Exception {
-		final CommandRef[] list = CommandCatalog.all();
-
-		int width = 0;
-		for (final CommandRef c : list)
-			width = Math.max(width, c.getName().length());
-		width += 2;
-
-		for (final CommandRef c : list) {
-			System.err.print(c.isCommon() ? '*' : ' ');
-			System.err.print(' ');
-
-			System.err.print(c.getName());
-			for (int i = c.getName().length(); i < width; i++)
-				System.err.print(' ');
-
-			pretty.print(c);
-			System.err.println();
-		}
-		System.err.println();
-	}
-
-	static enum Format {
-		/** */
-		USAGE {
-			void print(final CommandRef c) {
-				String usage = c.getUsage();
-				if (usage != null && usage.length() > 0)
-					System.err.print(CLIText.get().resourceBundle().getString(usage));
-			}
-		},
-
-		/** */
-		CLASSES {
-			void print(final CommandRef c) {
-				System.err.print(c.getImplementationClassName());
-			}
-		},
-
-		/** */
-		URLS {
-			void print(final CommandRef c) {
-				final ClassLoader ldr = c.getImplementationClassLoader();
-
-				String cn = c.getImplementationClassName();
-				cn = cn.replace('.', '/') + ".class";
-
-				final URL url = ldr.getResource(cn);
-				if (url == null) {
-					System.err.print(CLIText.get().notFound);
-					return;
-				}
-
-				String rn = url.toExternalForm();
-				if (rn.endsWith(cn))
-					rn = rn.substring(0, rn.length() - cn.length());
-
-				System.err.print(rn);
-			}
-		};
-
-		abstract void print(CommandRef c);
+		AddCommand addCmd = new Git(db).add();
+		addCmd.setUpdate(update);
+		for (String p : filepatterns)
+			addCmd.addFilepattern(p);
+		addCmd.call();
 	}
 }
