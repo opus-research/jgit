f21fe63f6d08a17872e05b10f81d95f9e0495f6e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:53:34 +0000

    First commited as before_14871_rev5

--- org.eclipse.jgit/src/org/eclipse/jgit/api/Git.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/Git.java
@@ -653,16 +653,6 @@ public class Git {
 	}
 
 	/**
-	 * Returns a command object to come up with a short name that describes a commit
-	 * in terms of the nearest git tag.
-	 *
-	 * @return 3.1
-	 */
-	public DescribeCommand describe() {
-		return new DescribeCommand(repo);
-	}
-
-	/**
 	 * @return the git repository this class is interacting with
 	 */
 	public Repository getRepository() {
