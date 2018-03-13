f147a74a9e3e487140074d1f518e649bc02164d6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:28:41 +0000

    First commited as before_48142_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/util/FS.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/FS.java
@@ -504,13 +504,8 @@ public abstract class FS {
 		return p.value;
 	}
 
-	/** @return the path to the Git executable. */
-	protected abstract File discoverGitExe();
-
 	/** @return the $prefix directory C Git would use. */
-	protected File discoverGitPrefix() {
-		return resolveGrandparentFile(discoverGitExe());
-	}
+	protected abstract File discoverGitPrefix();
 
 	protected static File resolveGrandparentFile(File grandchild) {
 		if (grandchild != null) {
