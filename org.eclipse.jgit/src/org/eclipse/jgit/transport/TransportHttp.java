b01d7fe9c6066af80842d848f98abbf7d739fb0c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:04:11 +0000

    First commited as before_22160_rev11

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/TransportHttp.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/TransportHttp.java
@@ -247,6 +247,8 @@ public class TransportHttp extends HttpTransport implements WalkTransport,
 
 	private HttpAuthMethod authMethod = HttpAuthMethod.NONE;
 
+	private Map<String, String> headers;
+
 	TransportHttp(final Repository local, final URIish uri)
 			throws NotSupportedException {
 		super(local, uri);
@@ -425,6 +427,18 @@ public class TransportHttp extends HttpTransport implements WalkTransport,
 		// No explicit connections are maintained.
 	}
 
+	/**
+	 * Set additional headers on the HTTP connection
+	 *
+	 * @param headers
+	 *            a map of name:values that are to be set as headers on the HTTP
+	 *            connection
+	 * @since 3.4
+	 */
+	public void setAdditionalHeaders(Map<String, String> headers) {
+		this.headers = headers;
+	}
+
 	private HttpConnection connect(final String service)
 			throws TransportException, NotSupportedException {
 		final URL u;
@@ -472,13 +486,13 @@ public class TransportHttp extends HttpTransport implements WalkTransport,
 						throw new TransportException(uri, MessageFormat.format(
 								JGitText.get().authenticationNotSupported, uri));
 					CredentialsProvider credentialsProvider = getCredentialsProvider();
+					if (authAttempts > 1)
+						credentialsProvider.reset(uri);
 					if (3 < authAttempts
 							|| !authMethod.authorize(uri, credentialsProvider)) {
-						credentialsProvider.reset(uri);
 						throw new TransportException(uri,
 								JGitText.get().notAuthorized);
 					}
-					credentialsProvider.reset(uri);
 					authAttempts++;
 					continue;
 
@@ -532,6 +546,10 @@ public class TransportHttp extends HttpTransport implements WalkTransport,
 			int effTimeOut = timeOut * 1000;
 			conn.setConnectTimeout(effTimeOut);
 			conn.setReadTimeout(effTimeOut);
+		}
+		if (this.headers != null && !this.headers.isEmpty()) {
+			for (Map.Entry<String, String> entry : this.headers.entrySet())
+				conn.setRequestProperty(entry.getKey(), entry.getValue());
 		}
 		authMethod.configureRequest(conn);
 		return conn;
