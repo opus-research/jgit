5c57b31eee9b67295ea573ff8510b6666d384776
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:29:34 +0000

    First commited as before_91116_rev5

--- org.eclipse.jgit.archive/src/org/eclipse/jgit/archive/TarFormat.java
+++ org.eclipse.jgit.archive/src/org/eclipse/jgit/archive/TarFormat.java
@@ -57,7 +57,6 @@ import org.apache.commons.compress.archivers.tar.TarConstants;
 import org.eclipse.jgit.api.ArchiveCommand;
 import org.eclipse.jgit.archive.internal.ArchiveText;
 import org.eclipse.jgit.lib.FileMode;
-import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectLoader;
 
 /**
@@ -85,17 +84,8 @@ public final class TarFormat extends BaseFormat implements
 		return applyFormatOptions(out, o);
 	}
 
-	/**
-	 * @deprecated
-	 */
 	public void putEntry(ArchiveOutputStream out,
 			String path, FileMode mode, ObjectLoader loader)
-			throws IOException {
-		putEntry(out, null, path, mode,loader);
-	}
-
-	public void putEntry(ArchiveOutputStream out,
-			ObjectId tree, String path, FileMode mode, ObjectLoader loader)
 			throws IOException {
 		if (mode == FileMode.SYMLINK) {
 			final TarArchiveEntry entry = new TarArchiveEntry(
