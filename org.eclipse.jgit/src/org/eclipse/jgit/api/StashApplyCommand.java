873eb8759474a1ddb28d3abea2299755c7547c9f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:24:48 +0000

    First commited as before_45703_rev3

--- org.eclipse.jgit/src/org/eclipse/jgit/api/StashApplyCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/StashApplyCommand.java
@@ -166,8 +166,9 @@ public class StashApplyCommand extends GitCommand<ObjectId> {
 					JGitText.get().stashApplyOnUnsafeRepository,
 					repo.getRepositoryState()));
 
-		try (ObjectReader reader = repo.newObjectReader();
-				RevWalk revWalk = new RevWalk(reader)) {
+		ObjectReader reader = repo.newObjectReader();
+		try {
+			RevWalk revWalk = new RevWalk(reader);
 
 			ObjectId headCommit = repo.resolve(Constants.HEAD);
 			if (headCommit == null)
@@ -249,6 +250,8 @@ public class StashApplyCommand extends GitCommand<ObjectId> {
 			throw e;
 		} catch (IOException e) {
 			throw new JGitInternalException(JGitText.get().stashApplyFailed, e);
+		} finally {
+			reader.release();
 		}
 	}
 
@@ -283,9 +286,11 @@ public class StashApplyCommand extends GitCommand<ObjectId> {
 
 	private void resetIndex(RevTree tree) throws IOException {
 		DirCache dc = repo.lockDirCache();
-		try (TreeWalk walk = new TreeWalk(repo)) {
+		TreeWalk walk = null;
+		try {
 			DirCacheBuilder builder = dc.builder();
 
+			walk = new TreeWalk(repo);
 			walk.addTree(tree);
 			walk.addTree(new DirCacheIterator(dc));
 			walk.setRecursive(true);
@@ -316,13 +321,15 @@ public class StashApplyCommand extends GitCommand<ObjectId> {
 			builder.commit();
 		} finally {
 			dc.unlock();
+			if (walk != null)
+				walk.release();
 		}
 	}
 
 	private void resetUntracked(RevTree tree) throws CheckoutConflictException,
 			IOException {
-		// TODO maybe NameConflictTreeWalk ?
-		try (TreeWalk walk = new TreeWalk(repo)) {
+		TreeWalk walk = new TreeWalk(repo); // maybe NameConflictTreeWalk;
+		try {
 			walk.addTree(tree);
 			walk.addTree(new FileTreeIterator(repo));
 			walk.setRecursive(true);
@@ -352,6 +359,8 @@ public class StashApplyCommand extends GitCommand<ObjectId> {
 
 				checkoutPath(entry, reader);
 			}
+		} finally {
+			walk.release();
 		}
 	}
 
