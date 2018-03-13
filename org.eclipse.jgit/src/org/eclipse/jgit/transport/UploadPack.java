a5e5484104c34503b6f1d798088667bb051c95b4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 16:02:58 +0000

    First commited as before_106246_rev5

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/UploadPack.java
@@ -81,7 +81,6 @@ import org.eclipse.jgit.internal.storage.pack.PackWriter;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.NullProgressMonitor;
 import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.ProgressMonitor;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RefDatabase;
@@ -207,9 +206,6 @@ public class UploadPack {
 	/** Database we read the objects from. */
 	private final Repository db;
 
-	/** reader to load the objects from database. */
-	private final ObjectReader reader;
-
 	/** Revision traversal support over {@link #db}. */
 	private final RevWalk walk;
 
@@ -330,7 +326,6 @@ public class UploadPack {
 	public UploadPack(final Repository copyFrom) {
 		db = copyFrom;
 		walk = new RevWalk(db);
-		reader = walk.getObjectReader();
 		walk.setRetainBody(false);
 
 		WANT = walk.newFlag("WANT"); //$NON-NLS-1$
@@ -356,14 +351,6 @@ public class UploadPack {
 	/** @return the RevWalk instance used by this connection. */
 	public final RevWalk getRevWalk() {
 		return walk;
-	}
-
-	/**
-	 * @return the reader that is using to load objects by the RevWalk.
-	 * @since 4.9
-	 */
-	public ObjectReader getObjectReader() {
-		return reader;
 	}
 
 	/**
@@ -1274,7 +1261,7 @@ public class UploadPack {
 		@Override
 		public void checkWants(UploadPack up, List<ObjectId> wants)
 				throws PackProtocolException, IOException {
-			checkNotAdvertisedWants(up, wants,
+			checkNotAdvertisedWants(up.getRevWalk(), wants,
 					refIdSet(up.getAdvertisedRefs().values()));
 		}
 	}
@@ -1311,7 +1298,7 @@ public class UploadPack {
 		@Override
 		public void checkWants(UploadPack up, List<ObjectId> wants)
 				throws PackProtocolException, IOException {
-			checkNotAdvertisedWants(up, wants,
+			checkNotAdvertisedWants(up.getRevWalk(), wants,
 					refIdSet(up.getRepository().getRefDatabase().getRefs(ALL).values()));
 		}
 	}
@@ -1329,7 +1316,7 @@ public class UploadPack {
 		}
 	}
 
-	private static void checkNotAdvertisedWants(UploadPack up,
+	private static void checkNotAdvertisedWants(RevWalk walk,
 			List<ObjectId> notAdvertisedWants, Set<ObjectId> reachableFrom)
 			throws MissingObjectException, IncorrectObjectTypeException, IOException {
 		// Walk the requested commits back to the provided set of commits. If any
@@ -1338,34 +1325,32 @@ public class UploadPack {
 		// into an advertised branch it will be marked UNINTERESTING and no commits
 		// return.
 
-		try (RevWalk walk = new RevWalk(up.getObjectReader())) {
-			AsyncRevObjectQueue q = walk.parseAny(notAdvertisedWants, true);
+		AsyncRevObjectQueue q = walk.parseAny(notAdvertisedWants, true);
+		try {
+			RevObject obj;
+			while ((obj = q.next()) != null) {
+				if (!(obj instanceof RevCommit))
+					throw new WantNotValidException(obj);
+				walk.markStart((RevCommit) obj);
+			}
+		} catch (MissingObjectException notFound) {
+			throw new WantNotValidException(notFound.getObjectId(), notFound);
+		} finally {
+			q.release();
+		}
+		for (ObjectId id : reachableFrom) {
 			try {
-				RevObject obj;
-				while ((obj = q.next()) != null) {
-					if (!(obj instanceof RevCommit))
-						throw new WantNotValidException(obj);
-					walk.markStart((RevCommit) obj);
-				}
-			} catch (MissingObjectException notFound) {
-				throw new WantNotValidException(notFound.getObjectId(),
-						notFound);
-			} finally {
-				q.release();
-			}
-			for (ObjectId id : reachableFrom) {
-				try {
-					walk.markUninteresting(walk.parseCommit(id));
-				} catch (IncorrectObjectTypeException notCommit) {
-					continue;
-				}
-			}
-
-			RevCommit bad = walk.next();
-			if (bad != null) {
-				throw new WantNotValidException(bad);
+				walk.markUninteresting(walk.parseCommit(id));
+			} catch (IncorrectObjectTypeException notCommit) {
+				continue;
 			}
 		}
+
+		RevCommit bad = walk.next();
+		if (bad != null) {
+			throw new WantNotValidException(bad);
+		}
+		walk.reset();
 	}
 
 	private void addCommonBase(final RevObject o) {
