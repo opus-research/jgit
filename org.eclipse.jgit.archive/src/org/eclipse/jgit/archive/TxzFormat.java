0b07ff1474efa5e2f1087371551d0559d835e56f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:22:38 +0000

    First commited as before_43743_rev2

--- org.eclipse.jgit.archive/src/org/eclipse/jgit/archive/TxzFormat.java
+++ org.eclipse.jgit.archive/src/org/eclipse/jgit/archive/TxzFormat.java
@@ -71,9 +71,6 @@ public final class TxzFormat extends BaseFormat implements
 				Collections.<String, Object> emptyMap());
 	}
 
-	/**
-	 * @since 4.0
-	 */
 	public ArchiveOutputStream createArchiveOutputStream(OutputStream s,
 			Map<String, Object> o) throws IOException {
 		XZCompressorOutputStream out = new XZCompressorOutputStream(s);
