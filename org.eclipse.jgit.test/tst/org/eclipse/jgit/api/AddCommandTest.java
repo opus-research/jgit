f44f411b11e00d3112da17aeaf3131499cf2614c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:27:44 +0000

    First commited as before_6034_rev1

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/AddCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/AddCommandTest.java
@@ -628,11 +628,6 @@ public class AddCommandTest extends RepositoryTestCase {
 			public boolean canExecute(File f) {
 				return true;
 			}
-
-			@Override
-			public boolean isCaseSensitive() {
-				return false;
-			}
 		};
 
 		Git git = Git.open(db.getDirectory(), executableFs);
@@ -671,11 +666,6 @@ public class AddCommandTest extends RepositoryTestCase {
 			}
 
 			public boolean canExecute(File f) {
-				return false;
-			}
-
-			@Override
-			public boolean isCaseSensitive() {
 				return false;
 			}
 		};
