592c4048d2d04c1f67b91ab878db0abbbaa7b974
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:01:28 +0000

    First commited as before_70819_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/errors/CheckoutConflictException.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/errors/CheckoutConflictException.java
@@ -56,8 +56,6 @@ import org.eclipse.jgit.internal.JGitText;
 public class CheckoutConflictException extends IOException {
 	private static final long serialVersionUID = 1L;
 
-	private final String[] conflicting;
-
 	/**
 	 * Construct a CheckoutConflictException for the specified file
 	 *
@@ -65,7 +63,6 @@ public class CheckoutConflictException extends IOException {
 	 */
 	public CheckoutConflictException(String file) {
 		super(MessageFormat.format(JGitText.get().checkoutConflictWithFile, file));
-		conflicting = new String[] { file };
 	}
 
 	/**
@@ -75,16 +72,6 @@ public class CheckoutConflictException extends IOException {
 	 */
 	public CheckoutConflictException(String[] files) {
 		super(MessageFormat.format(JGitText.get().checkoutConflictWithFiles, buildList(files)));
-		conflicting = files;
-	}
-
-	/**
-	 * @return the relative paths of the conflicting files (relative to the
-	 *         working directory root).
-	 * @since 4.4
-	 */
-	public String[] getConflictingFiles() {
-		return conflicting;
 	}
 
 	private static String buildList(String[] files) {
