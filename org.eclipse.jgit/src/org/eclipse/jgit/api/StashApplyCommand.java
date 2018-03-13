3b491de0ac16c1a52408a7d55ea54a1a6e896f72
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:00:02 +0000

    First commited as before_18776_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/api/StashApplyCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/StashApplyCommand.java
@@ -185,7 +185,6 @@ public class StashApplyCommand extends GitCommand<ObjectId> {
 							.newMerger(repo, true);
 					ixMerger.setCommitNames(new String[] { "stashed HEAD",
 							"HEAD", "stashed index" });
-					ixMerger.setBase(stashHeadCommit);
 					boolean ok = ixMerger.merge(headCommit, stashIndexCommit);
 					if (ok) {
 						resetIndex(revWalk
