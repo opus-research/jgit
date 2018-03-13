649c4f22341e44d2cde8e4efd1e89b2b43204319
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 12:54:08 +0000

    First commited as before_523_rev4

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -60,6 +60,7 @@ import java.util.Set;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.eclipse.jgit.dircache.DirCache;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.RevisionSyntaxException;
@@ -95,6 +96,8 @@ public class Repository {
 	private final AtomicInteger useCnt = new AtomicInteger(1);
 
 	private final File gitDir;
+
+	private final FileBasedConfig userConfig;
 
 	private final RepositoryConfig config;
 
@@ -191,26 +194,11 @@ public class Repository {
 				throw new IllegalArgumentException("Either GIT_DIR or GIT_WORK_TREE must be passed to Repository constructor");
 		}
 
-		final FileBasedConfig userConfig;
 		userConfig = SystemReader.getInstance().openUserConfig();
-		try {
-			userConfig.load();
-		} catch (ConfigInvalidException e1) {
-			IOException e2 = new IOException("User config file "
-					+ userConfig.getFile().getAbsolutePath() + " invalid: "
-					+ e1);
-			e2.initCause(e1);
-			throw e2;
-		}
 		config = new RepositoryConfig(userConfig, FS.resolve(gitDir, "config"));
 
-		try {
-			getConfig().load();
-		} catch (ConfigInvalidException e1) {
-			IOException e2 = new IOException("Unknown repository format");
-			e2.initCause(e1);
-			throw e2;
-		}
+		loadUserConfig();
+		loadConfig();
 
 		if (workDir == null) {
 			String workTreeConfig = getConfig().getString("core", null, "worktree");
@@ -243,6 +231,29 @@ public class Repository {
 			}
 		}
 	}
+
+	private void loadUserConfig() throws IOException {
+		try {
+			userConfig.load();
+		} catch (ConfigInvalidException e1) {
+			IOException e2 = new IOException("User config file "
+					+ userConfig.getFile().getAbsolutePath() + " invalid: "
+					+ e1);
+			e2.initCause(e1);
+			throw e2;
+		}
+	}
+
+	private void loadConfig() throws IOException {
+		try {
+			config.load();
+		} catch (ConfigInvalidException e1) {
+			IOException e2 = new IOException("Unknown repository format");
+			e2.initCause(e1);
+			throw e2;
+		}
+	}
+
 
 	/**
 	 * Create a new Git repository initializing the necessary files and
@@ -320,6 +331,20 @@ public class Repository {
 	 * @return the configuration of this repository
 	 */
 	public RepositoryConfig getConfig() {
+		if (userConfig.isOutdated()) {
+			try {
+				loadUserConfig();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		if (config.isOutdated()) {
+				try {
+					loadConfig();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+		}
 		return config;
 	}
 
@@ -1093,8 +1118,22 @@ public class Repository {
 			return RepositoryState.REBASING_MERGE;
 
 		// Both versions
-		if (new File(gitDir,"MERGE_HEAD").exists())
+		if (new File(gitDir, "MERGE_HEAD").exists()) {
+			// we are merging - now check whether we have unmerged paths
+			try {
+				if (!DirCache.read(this).hasUnmergedPaths()) {
+					// no unmerged paths -> return the MERGING_RESOLVED state
+					return RepositoryState.MERGING_RESOLVED;
+				}
+			} catch (IOException e) {
+				// Can't decide whether unmerged paths exists. Return
+				// MERGING state to be on the safe side (in state MERGING
+				// you are not allow to do anything)
+				e.printStackTrace();
+			}
 			return RepositoryState.MERGING;
+		}
+
 		if (new File(gitDir,"BISECT_LOG").exists())
 			return RepositoryState.BISECTING;
 
