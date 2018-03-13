6c059b42210aff783d900554dbd5ca19e9583763
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:25:53 +0000

    First commited as before_5531_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/WindowCacheConfig.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/WindowCacheConfig.java
@@ -43,9 +43,8 @@
 
 package org.eclipse.jgit.storage.file;
 
-import org.eclipse.jgit.lib.CoreConfig;
 import org.eclipse.jgit.lib.Config;
-import org.eclipse.jgit.lib.ConfigConstants;
+import org.eclipse.jgit.storage.pack.PackConfig;
 
 /** Configuration parameters for {@link WindowCache}. */
 public class WindowCacheConfig {
@@ -74,7 +73,7 @@ public class WindowCacheConfig {
 		packedGitWindowSize = 8 * KB;
 		packedGitMMAP = false;
 		deltaBaseCacheLimit = 10 * MB;
-		streamFileThreshold = CoreConfig.getDefaultStreamFileThreshold();
+		streamFileThreshold = PackConfig.DEFAULT_BIG_FILE_THRESHOLD;
 	}
 
 	/**
@@ -197,9 +196,7 @@ public class WindowCacheConfig {
 		setDeltaBaseCacheLimit(rc.getInt("core", null, "deltabasecachelimit", getDeltaBaseCacheLimit()));
 
 		long maxMem = Runtime.getRuntime().maxMemory();
-		long sft = rc.getLong("core", null,
-				ConfigConstants.CONFIG_KEY_STREAM_FILE_TRESHOLD.toLowerCase(),
-				getStreamFileThreshold());
+		long sft = rc.getLong("core", null, "streamfilethreshold", getStreamFileThreshold());
 		sft = Math.min(sft, maxMem / 4); // don't use more than 1/4 of the heap
 		sft = Math.min(sft, Integer.MAX_VALUE); // cannot exceed array length
 		setStreamFileThreshold((int) sft);
