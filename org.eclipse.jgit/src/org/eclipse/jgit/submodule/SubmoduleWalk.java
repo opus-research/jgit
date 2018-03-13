30409a3f2a036389b9148d1624a09d8fbb29b942
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:37:43 +0000

    First commited as before_53005_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/submodule/SubmoduleWalk.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/submodule/SubmoduleWalk.java
@@ -465,7 +465,8 @@ public class SubmoduleWalk implements AutoCloseable {
 	public static boolean containsGitModulesFile(Repository repository)
 			throws IOException {
 		if (repository.isBare()) {
-			return false;
+			DirCache dc = repository.readDirCache();
+			return (dc.findEntry(Constants.DOT_GIT_MODULES) >= 0);
 		}
 		File modulesFile = new File(repository.getWorkTree(),
 				Constants.DOT_GIT_MODULES);
