164c5ced9ff8aacca3502ddf9ab40ad6b4aa9eae
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:13:12 +0000

    First commited as before_3997_rev4

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/PackWriter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/pack/PackWriter.java
@@ -99,7 +99,6 @@ import org.eclipse.jgit.revwalk.RevObject;
 import org.eclipse.jgit.revwalk.RevSort;
 import org.eclipse.jgit.revwalk.RevTag;
 import org.eclipse.jgit.revwalk.RevTree;
-import org.eclipse.jgit.storage.file.PackIndex;
 import org.eclipse.jgit.storage.file.PackIndexWriter;
 import org.eclipse.jgit.util.BlockList;
 import org.eclipse.jgit.util.TemporaryBuffer;
@@ -157,10 +156,6 @@ public class PackWriter {
 	private List<CachedPack> cachedPacks = new ArrayList<CachedPack>(2);
 
 	private Set<ObjectId> tagTargets = Collections.emptySet();
-
-	private PackIndex[] excludeInPacks;
-
-	private PackIndex excludeInPackLast;
 
 	private Deflater myDeflater;
 
@@ -431,25 +426,6 @@ public class PackWriter {
 	}
 
 	/**
-	 * Add a pack index whose contents should be excluded from the result.
-	 *
-	 * @param idx
-	 *            objects in this index will not be in the output pack.
-	 */
-	public void excludeObjects(PackIndex idx) {
-		if (excludeInPacks == null) {
-			excludeInPacks = new PackIndex[] { idx };
-			excludeInPackLast = idx;
-		} else {
-			int cnt = excludeInPacks.length;
-			PackIndex[] newList = new PackIndex[cnt + 1];
-			System.arraycopy(excludeInPacks, 0, newList, 0, cnt);
-			newList[cnt] = idx;
-			excludeInPacks = newList;
-		}
-	}
-
-	/**
 	 * Prepare the list of objects to be written to the pack stream.
 	 * <p>
 	 * Iterator <b>exactly</b> determines which objects are included in a pack
@@ -501,10 +477,90 @@ public class PackWriter {
 	 *            points of graph traversal).
 	 * @throws IOException
 	 *             when some I/O problem occur during reading objects.
+	 * @deprecated to be removed in 2.0; use the Set version of this method.
 	 */
+	@Deprecated
 	public void preparePack(ProgressMonitor countingMonitor,
 			final Collection<? extends ObjectId> want,
 			final Collection<? extends ObjectId> have) throws IOException {
+		preparePack(countingMonitor, ensureSet(want), ensureSet(have));
+	}
+
+	/**
+	 * Prepare the list of objects to be written to the pack stream.
+	 * <p>
+	 * Basing on these 2 sets, another set of objects to put in a pack file is
+	 * created: this set consists of all objects reachable (ancestors) from
+	 * interesting objects, except uninteresting objects and their ancestors.
+	 * This method uses class {@link ObjectWalk} extensively to find out that
+	 * appropriate set of output objects and their optimal order in output pack.
+	 * Order is consistent with general git in-pack rules: sort by object type,
+	 * recency, path and delta-base first.
+	 * </p>
+	 *
+	 * @param countingMonitor
+	 *            progress during object enumeration.
+	 * @param walk
+	 *            ObjectWalk to perform enumeration.
+	 * @param interestingObjects
+	 *            collection of objects to be marked as interesting (start
+	 *            points of graph traversal).
+	 * @param uninterestingObjects
+	 *            collection of objects to be marked as uninteresting (end
+	 *            points of graph traversal).
+	 * @throws IOException
+	 *             when some I/O problem occur during reading objects.
+	 * @deprecated to be removed in 2.0; use the Set version of this method.
+	 */
+	@Deprecated
+	public void preparePack(ProgressMonitor countingMonitor,
+			final ObjectWalk walk,
+			final Collection<? extends ObjectId> interestingObjects,
+			final Collection<? extends ObjectId> uninterestingObjects)
+			throws IOException {
+		preparePack(countingMonitor, walk,
+				ensureSet(interestingObjects),
+				ensureSet(uninterestingObjects));
+	}
+
+	@SuppressWarnings("unchecked")
+	private static Set<ObjectId> ensureSet(Collection<? extends ObjectId> objs) {
+		Set<ObjectId> set;
+		if (objs instanceof Set<?>)
+			set = (Set<ObjectId>) objs;
+		else if (objs == null)
+			set = Collections.emptySet();
+		else
+			set = new HashSet<ObjectId>(objs);
+		return set;
+	}
+
+	/**
+	 * Prepare the list of objects to be written to the pack stream.
+	 * <p>
+	 * Basing on these 2 sets, another set of objects to put in a pack file is
+	 * created: this set consists of all objects reachable (ancestors) from
+	 * interesting objects, except uninteresting objects and their ancestors.
+	 * This method uses class {@link ObjectWalk} extensively to find out that
+	 * appropriate set of output objects and their optimal order in output pack.
+	 * Order is consistent with general git in-pack rules: sort by object type,
+	 * recency, path and delta-base first.
+	 * </p>
+	 *
+	 * @param countingMonitor
+	 *            progress during object enumeration.
+	 * @param want
+	 *            collection of objects to be marked as interesting (start
+	 *            points of graph traversal).
+	 * @param have
+	 *            collection of objects to be marked as uninteresting (end
+	 *            points of graph traversal).
+	 * @throws IOException
+	 *             when some I/O problem occur during reading objects.
+	 */
+	public void preparePack(ProgressMonitor countingMonitor,
+			Set<? extends ObjectId> want,
+			Set<? extends ObjectId> have) throws IOException {
 		ObjectWalk ow = new ObjectWalk(reader);
 		preparePack(countingMonitor, ow, want, have);
 	}
@@ -536,8 +592,8 @@ public class PackWriter {
 	 */
 	public void preparePack(ProgressMonitor countingMonitor,
 			final ObjectWalk walk,
-			final Collection<? extends ObjectId> interestingObjects,
-			final Collection<? extends ObjectId> uninterestingObjects)
+			final Set<? extends ObjectId> interestingObjects,
+			final Set<? extends ObjectId> uninterestingObjects)
 			throws IOException {
 		if (countingMonitor == null)
 			countingMonitor = NullProgressMonitor.INSTANCE;
@@ -590,10 +646,10 @@ public class PackWriter {
 	/**
 	 * Create an index file to match the pack file just written.
 	 * <p>
-	 * This method can only be invoked after {@link #preparePack(Iterator)} or
-	 * {@link #preparePack(ProgressMonitor, Collection, Collection)} has been
-	 * invoked and completed successfully. Writing a corresponding index is an
-	 * optional feature that not all pack users may require.
+	 * This method can only be invoked after
+	 * {@link #writePack(ProgressMonitor, ProgressMonitor, OutputStream)} has
+	 * been invoked and completed successfully. Writing a corresponding index is
+	 * an optional feature that not all pack users may require.
 	 *
 	 * @param indexStream
 	 *            output for the index data. Caller is responsible for closing
@@ -664,9 +720,6 @@ public class PackWriter {
 			compressMonitor = NullProgressMonitor.INSTANCE;
 		if (writeMonitor == null)
 			writeMonitor = NullProgressMonitor.INSTANCE;
-
-		excludeInPacks = null;
-		excludeInPackLast = null;
 
 		boolean needSearchForReuse = reuseSupport != null && (
 				   reuseDeltas
@@ -1283,20 +1336,16 @@ public class PackWriter {
 	}
 
 	private void findObjectsToPack(final ProgressMonitor countingMonitor,
-			final ObjectWalk walker, Collection<? extends ObjectId> want,
-			Collection<? extends ObjectId> have)
+			final ObjectWalk walker, final Set<? extends ObjectId> want,
+			Set<? extends ObjectId> have)
 			throws MissingObjectException, IOException,
 			IncorrectObjectTypeException {
 		final long countingStart = System.currentTimeMillis();
 		countingMonitor.beginTask(JGitText.get().countingObjects,
 				ProgressMonitor.UNKNOWN);
 
-		if (!(want instanceof Set<?>))
-			want = new HashSet<ObjectId>(want);
 		if (have == null)
 			have = Collections.emptySet();
-		else if (!(have instanceof Set<?>))
-			have = new HashSet<ObjectId>(have);
 
 		stats.interestingObjects = Collections.unmodifiableSet(new HashSet<ObjectId>(want));
 		stats.uninterestingObjects = Collections.unmodifiableSet(new HashSet<ObjectId>(have));
@@ -1410,8 +1459,6 @@ public class PackWriter {
 		BlockList<RevCommit> commits = new BlockList<RevCommit>();
 		RevCommit c;
 		while ((c = walker.next()) != null) {
-			if (exclude(c))
-				continue;
 			if (c.has(inCachedPack)) {
 				CachedPack pack = tipToPack.get(c);
 				if (includesAllTips(pack, include, walker)) {
@@ -1477,8 +1524,6 @@ public class PackWriter {
 			while ((o = walker.nextObject()) != null) {
 				if (o.has(RevFlag.UNINTERESTING))
 					continue;
-				if (exclude(o))
-					continue;
 
 				int pathHash = walker.getPathHashCode();
 				byte[] pathBuf = walker.getPathBuffer();
@@ -1491,8 +1536,6 @@ public class PackWriter {
 			RevObject o;
 			while ((o = walker.nextObject()) != null) {
 				if (o.has(RevFlag.UNINTERESTING))
-					continue;
-				if (exclude(o))
 					continue;
 				addObject(o, walker.getPathHashCode());
 				countingMonitor.update(1);
@@ -1565,8 +1608,7 @@ public class PackWriter {
 	 */
 	public void addObject(final RevObject object)
 			throws IncorrectObjectTypeException {
-		if (!exclude(object))
-			addObject(object, 0);
+		addObject(object, 0);
 	}
 
 	private void addObject(final RevObject object, final int pathHashCode) {
@@ -1578,20 +1620,6 @@ public class PackWriter {
 		otp.setPathHash(pathHashCode);
 		objectsLists[object.getType()].add(otp);
 		objectsMap.add(otp);
-	}
-
-	private boolean exclude(AnyObjectId objectId) {
-		if (excludeInPacks == null)
-			return false;
-		if (excludeInPackLast.hasObject(objectId))
-			return true;
-		for (PackIndex idx : excludeInPacks) {
-			if (idx.hasObject(objectId)) {
-				excludeInPackLast = idx;
-				return true;
-			}
-		}
-		return false;
 	}
 
 	/**
