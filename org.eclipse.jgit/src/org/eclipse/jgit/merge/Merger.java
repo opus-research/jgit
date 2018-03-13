47f052ef5cfdb71f1db8dc1731c36a1af610b838
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:36:11 +0000

    First commited as before_94409_rev5

--- org.eclipse.jgit/src/org/eclipse/jgit/merge/Merger.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/merge/Merger.java
@@ -47,7 +47,6 @@ package org.eclipse.jgit.merge;
 import java.io.IOException;
 import java.text.MessageFormat;
 
-import org.eclipse.jgit.annotations.Nullable;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.NoMergeBaseException;
 import org.eclipse.jgit.errors.NoMergeBaseException.MergeBaseFailureReason;
@@ -71,15 +70,7 @@ import org.eclipse.jgit.treewalk.CanonicalTreeParser;
  * Instance of a specific {@link MergeStrategy} for a single {@link Repository}.
  */
 public abstract class Merger {
-	/**
-	 * The repository this merger operates on.
-	 * <p>
-	 * Null if and only if the merger was constructed with {@link
-	 * #Merger(ObjectInserter)}. Callers that want to assume the repo is not null
-	 * (e.g. because of a previous check that the merger is not in-core) may use
-	 * {@link #nonNullRepo()}.
-	 */
-	@Nullable
+	/** The repository this merger operates on. */
 	protected final Repository db;
 
 	/** Reader to support {@link #walk} and other object loading. */
@@ -113,55 +104,20 @@ public abstract class Merger {
 	 *            the repository this merger will read and write data on.
 	 */
 	protected Merger(final Repository local) {
-		if (local == null) {
-			throw new NullPointerException(JGitText.get().repositoryIsRequired);
-		}
 		db = local;
-		inserter = local.newObjectInserter();
+		inserter = db.newObjectInserter();
 		reader = inserter.newReader();
-		walk = new RevWalk(reader);
-	}
-
-	/**
-	 * Create a new in-core merge instance from an inserter.
-	 *
-	 * @param oi
-	 *            the inserter to write objects to. Will be closed at the
-	 *            conclusion of {@code merge}, unless {@code flush} is false.
-	 * @since 4.8
-	 */
-	protected Merger(ObjectInserter oi) {
-		db = null;
-		inserter = oi;
-		reader = oi.newReader();
 		walk = new RevWalk(reader);
 	}
 
 	/**
 	 * @return the repository this merger operates on.
 	 */
-	@Nullable
 	public Repository getRepository() {
 		return db;
 	}
 
-	/**
-	 * @return non-null repository instance
-	 * @throws IllegalArgumentException
-	 *             if the merger was constructed without a repository.
-	 * @since 4.8
-	 */
-	protected Repository nonNullRepo() {
-		if (db == null) {
-			throw new NullPointerException(JGitText.get().repositoryIsRequired);
-		}
-		return db;
-	}
-
-	/**
-	 * @return an object writer to create objects, writing objects to {@link
-	 * #getRepository()} (if a repository was provided).
-	 */
+	/** @return an object writer to create objects in {@link #getRepository()}. */
 	public ObjectInserter getObjectInserter() {
 		return inserter;
 	}
@@ -175,9 +131,7 @@ public abstract class Merger {
 	 *
 	 * @param oi
 	 *            the inserter instance to use. Must be associated with the
-	 *            repository instance returned by {@link #getRepository()} (if a
-	 *            repository was provided). Will be closed at the conclusion of
-	 *            {@code merge}, unless {@code flush} is false.
+	 *            repository instance returned by {@link #getRepository()}.
 	 */
 	public void setObjectInserter(ObjectInserter oi) {
 		walk.close();
@@ -219,9 +173,9 @@ public abstract class Merger {
 	 *
 	 * @since 3.5
 	 * @param flush
-	 *            whether to flush and close the underlying object inserter when
-	 *            finished to store any content-merged blobs and virtual merged
-	 *            bases; if false, callers are responsible for flushing.
+	 *            whether to flush the underlying object inserter when finished to
+	 *            store any content-merged blobs and virtual merged bases; if
+	 *            false, callers are responsible for flushing.
 	 * @param tips
 	 *            source trees to be combined together. The merge base is not
 	 *            included in this set.
