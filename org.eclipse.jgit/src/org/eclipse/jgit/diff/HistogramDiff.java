b2c7c1fad62893ecdaf11924538117ac3d9d3629
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:00:29 +0000

    First commited as before_1644_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/diff/HistogramDiff.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diff/HistogramDiff.java
@@ -78,13 +78,6 @@ public class HistogramDiff extends DiffAlgorithm {
 		return s.edits;
 	}
 
-	<S extends Sequence> void diffNonCommon(EditList edits,
-			HashedSequenceComparator<S> cmp, HashedSequence<S> a,
-			HashedSequence<S> b, Edit region) {
-		State<S> s = new State<S>(edits, cmp, a, b);
-		s.diffReplace(region);
-	}
-
 	private class State<S extends Sequence> {
 		private final HashedSequenceComparator<S> cmp;
 
@@ -100,14 +93,6 @@ public class HistogramDiff extends DiffAlgorithm {
 			this.a = p.getA();
 			this.b = p.getB();
 			this.edits = new EditList();
-		}
-
-		State(EditList edits, HashedSequenceComparator<S> cmp,
-				HashedSequence<S> a, HashedSequence<S> b) {
-			this.cmp = cmp;
-			this.a = a;
-			this.b = b;
-			this.edits = edits;
 		}
 
 		void diffReplace(Edit r) {
