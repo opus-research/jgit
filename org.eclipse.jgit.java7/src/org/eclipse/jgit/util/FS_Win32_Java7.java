e9d3f15c3fe5739eb9a23b27da4060dc73f1a858
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:02:15 +0000

    First commited as before_20543_rev1

--- org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FS_Win32_Java7.java
+++ org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FS_Win32_Java7.java
@@ -51,8 +51,6 @@ import java.io.IOException;
  */
 public class FS_Win32_Java7 extends FS_Win32 {
 
-	private volatile Boolean supportSymlinks;
-
 	FS_Win32_Java7(FS src) {
 		super(src);
 	}
@@ -67,29 +65,7 @@ public class FS_Win32_Java7 extends FS_Win32 {
 
 	@Override
 	public boolean supportsSymlinks() {
-		if (supportSymlinks == null)
-			detectSymlinkSupport();
-		return Boolean.TRUE.equals(supportSymlinks);
-	}
-
-	private void detectSymlinkSupport() {
-		File tempFile = null;
-		try {
-			tempFile = File.createTempFile("tempsymlinktarget", ""); //$NON-NLS-1$ //$NON-NLS-2$
-			File linkName = new File(tempFile.getParentFile(), "tempsymlink"); //$NON-NLS-1$
-			FileUtil.createSymLink(linkName, tempFile.getPath());
-			supportSymlinks = Boolean.TRUE;
-			linkName.delete();
-		} catch (IOException e) {
-			supportSymlinks = Boolean.FALSE;
-		} finally {
-			if (tempFile != null)
-				try {
-					FileUtils.delete(tempFile);
-				} catch (IOException e) {
-					throw new RuntimeException(e); // panic
-				}
-		}
+		return true;
 	}
 
 	@Override
--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -65,8 +65,6 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.eclipse.jgit.dircache.DirCache;
-import org.eclipse.jgit.dircache.DirCacheCheckout;
-import org.eclipse.jgit.dircache.InvalidPathException;
 import org.eclipse.jgit.errors.AmbiguousObjectException;
 import org.eclipse.jgit.errors.CorruptObjectException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
@@ -1152,14 +1150,6 @@ public abstract class Repository {
 			return false;
 		if (refName.endsWith(".lock")) //$NON-NLS-1$
 			return false;
-
-		// Borrow logic for filterig out invalid paths. These
-		// are also invalid ref
-		try {
-			DirCacheCheckout.checkValidPath(refName);
-		} catch (InvalidPathException e) {
-			return false;
-		}
 
 		int components = 1;
 		char p = '\0';
