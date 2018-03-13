4963a471b6a97ff63cd50490515435e70e25583e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:27:45 +0000

    First commited as before_89793_rev1

--- org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/LocalDiskRepositoryTestCase.java
+++ org.eclipse.jgit.junit/src/org/eclipse/jgit/junit/LocalDiskRepositoryTestCase.java
@@ -53,10 +53,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeSet;
 
 import org.eclipse.jgit.dircache.DirCache;
@@ -108,7 +106,7 @@ public abstract class LocalDiskRepositoryTestCase {
 	 */
 	protected MockSystemReader mockSystemReader;
 
-	private final Set<Repository> toClose = new HashSet<>();
+	private final List<Repository> toClose = new ArrayList<Repository>();
 	private File tmp;
 
 	@Before
@@ -364,7 +362,7 @@ public abstract class LocalDiskRepositoryTestCase {
 		FileRepository db = new FileRepository(gitdir);
 		assertFalse(gitdir.exists());
 		db.create(bare);
-		addRepoToClose(db);
+		toClose.add(db);
 		return db;
 	}
 
