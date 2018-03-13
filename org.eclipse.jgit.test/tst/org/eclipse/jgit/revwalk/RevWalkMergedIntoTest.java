6db91567f3f8be49468622c3441393c6790844b3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:07:06 +0000

    First commited as before_24678_rev3

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/RevWalkMergedIntoTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/revwalk/RevWalkMergedIntoTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014, Google Inc.
+ * Copyright (C) 2014, Sven Selberg <sven.selberg@sonymobile.com>
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -40,26 +40,41 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.eclipse.jgit.revwalk;
 
-package org.eclipse.jgit.gitrepo.internal;
+import static org.junit.Assert.assertTrue;
 
-import org.eclipse.jgit.nls.NLS;
-import org.eclipse.jgit.nls.TranslationBundle;
+import org.junit.Test;
 
-/**
- * Translation bundle for repo command
- */
-public class RepoText extends TranslationBundle {
+public class RevWalkMergedIntoTest extends RevWalkTestCase {
 
-	/**
-	 * @return an instance of this translation bundle
-	 */
-	public static RepoText get() {
-		return NLS.getBundleFor(RepoText.class);
+	@Test
+	public void testOldCommitWalk() throws Exception {
+		/*
+		 * Sometimes a merge is performed on a machine with faulty time.
+		 * This makes the traversal of the graph, when trying to find out if B
+		 * is merged into T, complex since the algorithm uses the time stamps
+		 * of commits to find the best route.
+		 * When for example O(ld) has a very old time stamp compared to one of the
+		 * commits (N(ew)) on the upper route between T and F(alse base), the route
+		 * to False is deemed the better option even though the alternate route leeds
+		 * to B(ase) which was the commit we were after.
+		 *
+		 *             o---o---o---o---N
+		 *            /                 \
+		 *           /   o---o---o---O---T
+		 *          /   /
+		 *      ---F---B
+		 *
+		 * This test is asserting that isMergedInto(B, T) returns true even
+		 * under those circumstances.
+		 */
+		final int threeDaysInSecs = 3 * 24 * 60 * 60;
+		final RevCommit f = commit();
+		final RevCommit b = commit(f);
+		final RevCommit o = commit(-threeDaysInSecs, commit(commit(commit(b))));
+		final RevCommit n = commit(commit(commit(commit(commit(f)))));
+		final RevCommit t = commit(n, o);
+		assertTrue(rw.isMergedInto(b, t));
 	}
-
-	// @formatter:off
-	/***/ public String errorNoDefault;
-	/***/ public String errorParsingManifestFile;
-	/***/ public String invalidManifest;
 }
