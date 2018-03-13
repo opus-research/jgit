c113d81bcfa2c77b94452d9c19c60736d2a90fd2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:45:50 +0000

    First commited as before_59994_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/util/FS.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/FS.java
@@ -65,7 +65,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import org.eclipse.jgit.annotations.Nullable;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.eclipse.jgit.internal.JGitText;
 import org.eclipse.jgit.lib.Constants;
@@ -404,8 +404,7 @@ public abstract class FS {
 	 *            as component array
 	 * @param encoding
 	 *            to be used to parse the command's output
-	 * @return the one-line output of the command or {@code null} if there is
-	 *         none
+	 * @return the one-line output of the command
 	 */
 	@Nullable
 	protected static String readPipe(File dir, String[] command, String encoding) {
@@ -424,8 +423,7 @@ public abstract class FS {
 	 * @param env
 	 *            Map of environment variables to be merged with those of the
 	 *            current process
-	 * @return the one-line output of the command or {@code null} if there is
-	 *         none
+	 * @return the one-line output of the command
 	 * @since 4.0
 	 */
 	@Nullable
