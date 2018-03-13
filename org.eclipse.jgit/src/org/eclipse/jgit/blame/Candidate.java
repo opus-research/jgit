634f81969f26855f31a2e82946b2f3ce9db048ee
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:07:52 +0000

    First commited as before_25249_rev3

--- org.eclipse.jgit/src/org/eclipse/jgit/blame/Candidate.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/blame/Candidate.java
@@ -146,17 +146,6 @@ class Candidate {
 		return sourceCommit.getAuthorIdent();
 	}
 
-	int sourceEnd() {
-		Region r = regionList;
-		while (r != null) {
-			Region n = r.next;
-			if (n == null)
-				return r.sourceStart + r.length;
-			r = n;
-		}
-		return 0;
-	}
-
 	void setSourcePath(PathFilter path) {
 		sourcePath = path;
 		recursivePath = path.shouldBeRecursive();
