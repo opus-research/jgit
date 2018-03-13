9ce824e121c9629bca0dd6bdc66a0e4b2545a00a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:47:38 +0000

    First commited as before_11731_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/RefUpdate.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/RefUpdate.java
@@ -638,7 +638,6 @@ public abstract class RefUpdate {
 	 * are checked explicitly.
 	 *
 	 * @param check
-	 * @since 3.0
 	 */
 	public void setCheckConflicting(boolean check) {
 		checkConflicting = check;
