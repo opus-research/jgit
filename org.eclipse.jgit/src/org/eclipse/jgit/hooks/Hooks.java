0c5cb46b7fd781112964eaa5fe1a78061bf24c8b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:02:56 +0000

    First commited as before_71982_rev10

--- org.eclipse.jgit/src/org/eclipse/jgit/hooks/Hooks.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/hooks/Hooks.java
@@ -68,6 +68,18 @@ public class Hooks {
 	 * @param repo
 	 * @param outputStream
 	 *            The output stream, or {@code null} to use {@code System.out}
+	 * @return The post-commit hook for the given repository.
+	 * @since 4.5
+	 */
+	public static PostCommitHook postCommit(Repository repo,
+			PrintStream outputStream) {
+		return new PostCommitHook(repo, outputStream);
+	}
+
+	/**
+	 * @param repo
+	 * @param outputStream
+	 *            The output stream, or {@code null} to use {@code System.out}
 	 * @return The commit-msg hook for the given repository.
 	 */
 	public static CommitMsgHook commitMsg(Repository repo,
--- org.eclipse.jgit/src/org/eclipse/jgit/hooks/PostCommitHook.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/hooks/PostCommitHook.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016, Christian Halstrick <christian.halstrick@sap.com>
+ * Copyright (C) 2015 Obeo.
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -40,55 +40,45 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.eclipse.jgit.util;
+package org.eclipse.jgit.hooks;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.eclipse.jgit.api.errors.AbortedByHookException;
+import org.eclipse.jgit.lib.Repository;
 
 /**
- * An abstraction for JGit's builtin implementations for hooks and filters.
- * Instead of spawning an external processes to start a filter/hook and to pump
- * data from/to stdin/stdout these builtin commmands may be used. They are
- * constructed by {@link FilterCommandFactory}.
+ * The <code>post-commit</code> hook implementation. This hook is run after the
+ * commit was successfully executed.
  *
  * @since 4.5
  */
-public abstract class FilterCommand {
-	/**
-	 * The {@link InputStream} this command should read from
-	 */
-	protected InputStream in;
+public class PostCommitHook extends GitHook<Void> {
+
+	/** The post-commit hook name. */
+	public static final String NAME = "post-commit"; //$NON-NLS-1$
 
 	/**
-	 * The {@link OutputStream} this command should write to
+	 * @param repo
+	 *            The repository
+	 * @param outputStream
+	 *            The output stream the hook must use. {@code null} is allowed,
+	 *            in which case the hook will use {@code System.out}.
 	 */
-	protected OutputStream out;
-
-	/**
-	 * @param in
-	 *            The {@link InputStream} this command should read from
-	 * @param out
-	 *            The {@link OutputStream} this command should write to
-	 */
-	public FilterCommand(InputStream in, OutputStream out) {
-		this.in = in;
-		this.out = out;
+	protected PostCommitHook(Repository repo, PrintStream outputStream) {
+		super(repo, outputStream);
 	}
 
-	/**
-	 * Execute the command. The command is supposed to read data from
-	 * {@link #in} and to write the result to {@link #out}. It returns the
-	 * number of bytes it read from {@link #in}. It should be called in a loop
-	 * until it returns -1 signaling that the {@link InputStream} is completely
-	 * processed.
-	 *
-	 * @return the number of bytes read from the {@link InputStream} or -1. -1
-	 *         means that the {@link InputStream} is completely processed.
-	 * @throws IOException
-	 *             when {@link IOException} occured while reading from
-	 *             {@link #in} or writing to {@link #out}
-	 *
-	 */
-	public abstract int run() throws IOException;
+	@Override
+	public Void call() throws IOException, AbortedByHookException {
+		doRun();
+		return null;
+	}
+
+	@Override
+	public String getHookName() {
+		return NAME;
+	}
+
 }
