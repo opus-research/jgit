8d09491fe68dc0095ae52bbcc442200245623e7a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:34:12 +0000

    First commited as before_7851_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/GC.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/GC.java
@@ -838,15 +838,12 @@ public class GC {
 
 	/**
 	 * During gc() or prune() each unreferenced, loose object which has been
-	 * created or modified after or at <code>expire</code> will not be pruned.
-	 * Only older objects may be pruned. If set to null then every object is a
+	 * created or modified after <code>expire</code> will not be pruned. Only
+	 * older objects may be pruned. If set to null then every object is a
 	 * candidate for pruning.
 	 *
 	 * @param expire
-	 *            instant in time which defines object expiration
-	 *            objects with modification time before this instant are expired
-	 *            objects with modification time newer or equal to this instant
-	 *            are not expired
+	 *            minimal age of objects to be pruned in milliseconds.
 	 */
 	public void setExpire(Date expire) {
 		this.expire = expire;
