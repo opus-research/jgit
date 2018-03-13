d0a2f4b5a869e69a7f352f3f4afed91b911edbe9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:22:35 +0000

    First commited as before_85527_rev4

--- org.eclipse.jgit.http.apache/src/org/eclipse/jgit/transport/http/apache/HttpClientConnection.java
+++ org.eclipse.jgit.http.apache/src/org/eclipse/jgit/transport/http/apache/HttpClientConnection.java
@@ -323,20 +323,8 @@ public class HttpClientConnection implements HttpConnection {
 	}
 
 	public int getContentLength() {
-		Header contentLength = resp.getFirstHeader("content-length"); //$NON-NLS-1$
-		if (contentLength == null) {
-			return -1;
-		}
-
-		try {
-			long l = Long.parseLong(contentLength.getValue());
-			if (l > Integer.MAX_VALUE) {
-				return -1;
-			}
-			return (int) l;
-		} catch (NumberFormatException e) {
-			return -1;
-		}
+		return Integer.parseInt(resp.getFirstHeader("content-length") //$NON-NLS-1$
+				.getValue());
 	}
 
 	public void setInstanceFollowRedirects(boolean followRedirects) {
