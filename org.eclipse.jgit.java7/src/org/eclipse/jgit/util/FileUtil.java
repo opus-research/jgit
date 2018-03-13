8f5391107cf47f953c04ed9ab00ba972820e25a3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:55:47 +0000

    First commited as before_15395_rev2

--- org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FileUtil.java
+++ org.eclipse.jgit.java7/src/org/eclipse/jgit/util/FileUtil.java
@@ -190,4 +190,22 @@ class FileUtil {
 		}
 	}
 
+	public static File normalize(File file) {
+		if (SystemReader.getInstance().isMacOS()) {
+				String normalized = Normalizer.normalize(file.getPath(),
+						Normalizer.Form.NFC);
+				return new File(normalized);
+		}
+		return file;
+	}
+
+	public static String normalize(String name) {
+		if (SystemReader.getInstance().isMacOS()) {
+			if (name == null)
+				return null;
+			return Normalizer.normalize(name, Normalizer.Form.NFC);
+		}
+		return name;
+	}
+
 }
