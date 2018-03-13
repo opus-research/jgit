9666bb8f933ce4e33cbb2bc5f576d41a939aec74
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:10:13 +0000

    First commited as before_28080_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/RepoCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/gitrepo/RepoCommand.java
@@ -46,7 +46,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.channels.FileChannel;
@@ -115,7 +114,6 @@ public class RepoCommand extends GitCommand<RevCommit> {
 	private String branch;
 	private PersonIdent author;
 	private RemoteReader callback;
-	private InputStream inputStream;
 
 	private List<Project> bareProjects;
 	private Git git;
@@ -251,7 +249,7 @@ public class RepoCommand extends GitCommand<RevCommit> {
 
 	private static class XmlManifest extends DefaultHandler {
 		private final RepoCommand command;
-		private final InputStream inputStream;
+		private final String filename;
 		private final String baseUrl;
 		private final Map<String, String> remotes;
 		private final List<Project> projects;
@@ -261,10 +259,9 @@ public class RepoCommand extends GitCommand<RevCommit> {
 		private String defaultRevision;
 		private Project currentProject;
 
-		XmlManifest(RepoCommand command, InputStream inputStream,
-				String baseUrl, String groups) {
+		XmlManifest(RepoCommand command, String filename, String baseUrl, String groups) {
 			this.command = command;
-			this.inputStream = inputStream;
+			this.filename = filename;
 			this.baseUrl = baseUrl;
 			remotes = new HashMap<String, String>();
 			projects = new ArrayList<Project>();
@@ -291,15 +288,16 @@ public class RepoCommand extends GitCommand<RevCommit> {
 				throw new IOException(JGitText.get().noXMLParserAvailable);
 			}
 			xr.setContentHandler(this);
+			final FileInputStream in = new FileInputStream(filename);
 			try {
-				xr.parse(new InputSource(inputStream));
+				xr.parse(new InputSource(in));
 			} catch (SAXException e) {
-				IOException error = new IOException(
-							RepoText.get().errorParsingManifestFile);
+				IOException error = new IOException(MessageFormat.format(
+							RepoText.get().errorParsingManifestFile, filename));
 				error.initCause(e);
 				throw error;
 			} finally {
-				inputStream.close();
+				in.close();
 			}
 		}
 
@@ -348,7 +346,8 @@ public class RepoCommand extends GitCommand<RevCommit> {
 		@Override
 		public void endDocument() throws SAXException {
 			if (defaultRemote == null) {
-				throw new SAXException(RepoText.get().errorNoDefault);
+				throw new SAXException(MessageFormat.format(
+						RepoText.get().errorNoDefault, filename));
 			}
 			final String remoteUrl;
 			try {
@@ -415,20 +414,6 @@ public class RepoCommand extends GitCommand<RevCommit> {
 	 */
 	public RepoCommand setPath(final String path) {
 		this.path = path;
-		return this;
-	}
-
-	/**
-	 * Set the input stream to the manifest XML.
-	 *
-	 * Setting inputStream will ignore the path set.
-	 * It will be closed in {@link #call}.
-	 *
-	 * @param inputStream
-	 * @return this command
-	 */
-	public RepoCommand setInputStream(final InputStream inputStream) {
-		this.inputStream = inputStream;
 		return this;
 	}
 
@@ -512,19 +497,10 @@ public class RepoCommand extends GitCommand<RevCommit> {
 	@Override
 	public RevCommit call() throws GitAPIException {
 		checkCallable();
+		if (path == null || path.length() == 0)
+			throw new IllegalArgumentException(JGitText.get().pathNotConfigured);
 		if (uri == null || uri.length() == 0)
 			throw new IllegalArgumentException(JGitText.get().uriNotConfigured);
-		if (inputStream == null) {
-			if (path == null || path.length() == 0)
-				throw new IllegalArgumentException(
-						JGitText.get().pathNotConfigured);
-			try {
-				inputStream = new FileInputStream(path);
-			} catch (IOException e) {
-				throw new IllegalArgumentException(
-						JGitText.get().pathNotConfigured);
-			}
-		}
 
 		if (repo.isBare()) {
 			bareProjects = new ArrayList<Project>();
@@ -535,7 +511,7 @@ public class RepoCommand extends GitCommand<RevCommit> {
 		} else
 			git = new Git(repo);
 
-		XmlManifest manifest = new XmlManifest(this, inputStream, uri, groups);
+		XmlManifest manifest = new XmlManifest(this, path, uri, groups);
 		try {
 			manifest.read();
 		} catch (IOException e) {
