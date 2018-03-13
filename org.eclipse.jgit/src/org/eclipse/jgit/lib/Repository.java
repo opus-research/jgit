7aec5f3d40a98b9ce4efdc4d33bdc75efdda0f84
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 12:55:08 +0000

    First commited as before_836_rev5

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -245,8 +245,7 @@ public class Repository {
 		loadConfig();
 
 		if (workDir == null) {
-			String workTreeConfig = getConfig().getString(
-					ConfigConstants.CONFIG_CORE_SECTION, null, "worktree");
+			String workTreeConfig = getConfig().getString("core", null, "worktree");
 			if (workTreeConfig != null) {
 				workDir = fs.resolve(d, workTreeConfig);
 			} else {
@@ -269,12 +268,10 @@ public class Repository {
 
 		if (objectDatabase.exists()) {
 			final String repositoryFormatVersion = getConfig().getString(
-					ConfigConstants.CONFIG_CORE_SECTION, null,
-					"repositoryFormatVersion");
+					"core", null, "repositoryFormatVersion");
 			if (!"0".equals(repositoryFormatVersion)) {
 				throw new IOException(MessageFormat.format(
-						JGitText.get().unknownRepositoryFormat2,
-						repositoryFormatVersion));
+						JGitText.get().unknownRepositoryFormat2, repositoryFormatVersion));
 			}
 		}
 	}
@@ -283,9 +280,8 @@ public class Repository {
 		try {
 			userConfig.load();
 		} catch (ConfigInvalidException e1) {
-			IOException e2 = new IOException(MessageFormat.format(JGitText
-					.get().userConfigFileInvalid, userConfig.getFile()
-					.getAbsolutePath(), e1));
+			IOException e2 = new IOException(MessageFormat.format(
+					JGitText.get().userConfigFileInvalid, userConfig.getFile().getAbsolutePath(), e1));
 			e2.initCause(e1);
 			throw e2;
 		}
@@ -338,17 +334,12 @@ public class Repository {
 		head.disableRefLog();
 		head.link(Constants.R_HEADS + Constants.MASTER);
 
-		cfg.setInt(ConfigConstants.CONFIG_CORE_SECTION, null,
-				"repositoryformatversion", 0);
-		cfg.setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null, "filemode",
-				true);
+		cfg.setInt("core", null, "repositoryformatversion", 0);
+		cfg.setBoolean("core", null, "filemode", true);
 		if (bare)
-			cfg.setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null, "bare",
-					true);
-		cfg.setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null,
-				"logallrefupdates", !bare);
-		cfg.setBoolean(ConfigConstants.CONFIG_CORE_SECTION, null, "autocrlf",
-				false);
+			cfg.setBoolean("core", null, "bare", true);
+		cfg.setBoolean("core", null, "logallrefupdates", !bare);
+		cfg.setBoolean("core", null, "autocrlf", false);
 		cfg.save();
 	}
 
@@ -1127,9 +1118,6 @@ public class Repository {
 	 * @throws IOException
 	 */
 	public GitIndex getIndex() throws IOException {
-		if (isBare())
-			throw new IllegalStateException(
-					JGitText.get().bareRepositoryNoWorkdirAndIndex);
 		if (index == null) {
 			index = new GitIndex(this);
 			index.read();
@@ -1143,9 +1131,6 @@ public class Repository {
 	 * @return the index file location
 	 */
 	public File getIndexFile() {
-		if (isBare())
-			throw new IllegalStateException(
-					JGitText.get().bareRepositoryNoWorkdirAndIndex);
 		return indexFile;
 	}
 
@@ -1285,38 +1270,9 @@ public class Repository {
 	}
 
 	/**
-	 * @return the "bare"-ness of this Repository
-	 */
-	public boolean isBare() {
-		// first, check if the configuration was set to "bare":
-		boolean bare = getConfig().getBoolean(
-				ConfigConstants.CONFIG_CORE_SECTION,
-				ConfigConstants.CONFIG_KEY_BARE, false);
-		if (bare)
-			return true;
-		// now, let's see if a worktree was configured
-		String workTree = getConfig().getString(
-				ConfigConstants.CONFIG_CORE_SECTION, null, "worktree");
-		if (workTree != null)
-			// if the worktree was configured, we assume that we are not bare
-			return false;
-
-		// if the directory is .git, then we are probably not bare
-		if (getDirectory().getName().equals(Constants.DOT_GIT))
-			return false;
-
-		return true;
-	}
-
-	/**
 	 * @return the workdir file, i.e. where the files are checked out
-	 * @throws IllegalStateException
-	 *             if the repository is "bare"
 	 */
-	public File getWorkDir() throws IllegalStateException {
-		if (isBare())
-			throw new IllegalStateException(
-					JGitText.get().bareRepositoryNoWorkdirAndIndex);
+	public File getWorkDir() {
 		return workDir;
 	}
 
