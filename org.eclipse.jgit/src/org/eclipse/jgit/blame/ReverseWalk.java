4ecd60b3bcb8bd158e91f288e4414069639cad98
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:11:07 +0000

    First commited as before_3558_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/blame/ReverseWalk.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/blame/ReverseWalk.java
@@ -41,21 +41,73 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.eclipse.jgit.transport;
+package org.eclipse.jgit.blame;
 
 import java.io.IOException;
 
-/** UploadPack has already reported an error to the client. */
-public class UploadPackInternalServerErrorException extends IOException {
-	private static final long serialVersionUID = 1L;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 
-	/**
-	 * Initialize a new exception.
-	 *
-	 * @param why
-	 *            root cause.
-	 */
-	public UploadPackInternalServerErrorException(Throwable why) {
-		super(why);
+final class ReverseWalk extends RevWalk {
+	ReverseWalk(Repository repo) {
+		super(repo);
+	}
+
+	@Override
+	public ReverseCommit next() throws MissingObjectException,
+			IncorrectObjectTypeException, IOException {
+		ReverseCommit c = (ReverseCommit) super.next();
+		if (c == null)
+			return null;
+		for (int pIdx = 0; pIdx < c.getParentCount(); pIdx++)
+			((ReverseCommit) c.getParent(pIdx)).addChild(c);
+		return c;
+	}
+
+	@Override
+	protected RevCommit createCommit(AnyObjectId id) {
+		return new ReverseCommit(id);
+	}
+
+	static final class ReverseCommit extends RevCommit {
+		private static final ReverseCommit[] NO_CHILDREN = {};
+
+		private ReverseCommit[] children = NO_CHILDREN;
+
+		ReverseCommit(AnyObjectId id) {
+			super(id);
+		}
+
+		void addChild(ReverseCommit c) {
+			// Always put the most recent child onto the front of the list.
+			// This works correctly because our ReverseWalk parent (above)
+			// runs in COMMIT_TIME_DESC order. Older commits will be popped
+			// later and should go in front of the children list so they are
+			// visited first by BlameGenerator when considering candidates.
+
+			int cnt = children.length;
+			if (cnt == 0)
+				children = new ReverseCommit[] { c };
+			else if (cnt == 1)
+				children = new ReverseCommit[] { c, children[0] };
+			else {
+				ReverseCommit[] n = new ReverseCommit[1 + cnt];
+				n[0] = c;
+				System.arraycopy(children, 0, n, 1, cnt);
+				children = n;
+			}
+		}
+
+		int getChildCount() {
+			return children.length;
+		}
+
+		ReverseCommit getChild(final int nth) {
+			return children[nth];
+		}
 	}
 }
