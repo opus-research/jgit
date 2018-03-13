f0d24f25c7bfc5f8c462b92acd4efabd7d342594
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:05:45 +0000

    First commited as before_2311_rev3

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/RemoteSession.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/RemoteSession.java
@@ -1,6 +1,10 @@
 /*
- * Copyright (C) 2011, Garmin International
- * Copyright (C) 2011, Jesse Greenwald <jesse.greenwald@gmail.com>
+ * Copyright (C) 2009, Constantine Plotnikov <constantine.plotnikov@gmail.com>
+ * Copyright (C) 2008-2009, Google Inc.
+ * Copyright (C) 2009, Google, Inc.
+ * Copyright (C) 2009, JetBrains s.r.o.
+ * Copyright (C) 2008, Robin Rosenberg <robin.rosenberg@dewire.com>
+ * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -42,17 +46,41 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.eclipse.jgit.subtree;
+package org.eclipse.jgit.transport;
 
 import java.io.IOException;
 
 /**
- * This exception can occur if there was an error parsing Sub-Tree footers.
+ * Create a remote "session" for executing remote commands.
+ * <p>
+ * Clients should subclass RemoteSession to create an alternate way for JGit to
+ * execute remote commands. (The client application may already have this
+ * functionality available.) Note that this class is just a factory for creating
+ * remote processes. If the application already has a persistent connection to
+ * the remote machine, RemoteSession may do nothing more than return a new
+ * RemoteProcess when exec is called.
  */
-@SuppressWarnings("serial")
-public class SubtreeFooterException extends IOException {
+public interface RemoteSession {
+	/**
+	 * Generate a new remote process to execute the given command. This function
+	 * should also start execution and may need to create the streams prior to
+	 * execution.
+	 * @param commandName
+	 *            command to execute
+	 * @param timeout
+	 *            timeout value, in seconds, for command execution
+	 * @return a new remote process
+	 * @throws IOException
+	 *             may be thrown in several cases. For example, on problems
+	 *             opening input or output streams or on problems connecting or
+	 *             communicating with the remote host. For the latter two cases,
+	 *             a TransportException may be thrown (a subclass of
+	 *             IOException).
+	 */
+	public Process exec(String commandName, int timeout) throws IOException;
 
-	SubtreeFooterException(String msg) {
-		super(msg);
-	}
+	/**
+	 * Disconnect the remote session
+	 */
+	public void disconnect();
 }
