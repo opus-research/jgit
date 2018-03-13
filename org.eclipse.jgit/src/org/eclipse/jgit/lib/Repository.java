7dc609c6755ea6728e3a9585845194c167d5f484
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:18:01 +0000

    First commited as before_4732_rev4

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/Repository.java
@@ -86,6 +86,7 @@ import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.FileUtils;
 import org.eclipse.jgit.util.IO;
 import org.eclipse.jgit.util.RawParseUtils;
+import org.eclipse.jgit.util.io.SafeBufferedOutputStream;
 
 /**
  * Represents a Git repository.
@@ -496,14 +497,17 @@ public abstract class Repository {
 					if (!Character.isDigit(rev[l]))
 						break;
 				}
-				String distnum = new String(rev, i + 1, l - i - 1);
 				int dist;
-				try {
-					dist = Integer.parseInt(distnum);
-				} catch (NumberFormatException e) {
-					throw new RevisionSyntaxException(
-							JGitText.get().invalidAncestryLength, revstr);
-				}
+				if (l - i > 1) {
+					String distnum = new String(rev, i + 1, l - i - 1);
+					try {
+						dist = Integer.parseInt(distnum);
+					} catch (NumberFormatException e) {
+						throw new RevisionSyntaxException(
+								JGitText.get().invalidAncestryLength, revstr);
+					}
+				} else
+					dist = 1;
 				while (dist > 0) {
 					RevCommit commit = (RevCommit) ref;
 					if (commit.getParentCount() == 0) {
@@ -556,7 +560,7 @@ public abstract class Repository {
 					tree = rw.parseTree(ref);
 				}
 
-				if (i == rev.length - i)
+				if (i == rev.length - 1)
 					return tree.copy();
 
 				TreeWalk tw = TreeWalk.forPath(rw.getObjectReader(),
@@ -1274,7 +1278,7 @@ public abstract class Repository {
 			throws FileNotFoundException, IOException {
 		File headsFile = new File(getDirectory(), filename);
 		if (heads != null) {
-			BufferedOutputStream bos = new BufferedOutputStream(
+			BufferedOutputStream bos = new SafeBufferedOutputStream(
 					new FileOutputStream(headsFile));
 			try {
 				for (ObjectId id : heads) {
