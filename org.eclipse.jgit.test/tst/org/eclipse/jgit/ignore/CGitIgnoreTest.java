06ceb35b62cb9235bab1410715f784db2b2afae1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 16:01:21 +0000

    First commited as before_104222_rev1

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/ignore/CGitIgnoreTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/ignore/CGitIgnoreTest.java
@@ -105,12 +105,11 @@ public class CGitIgnoreTest extends RepositoryTestCase {
 		ProcessBuilder builder = fs.runInShell("git", new String[] { "ls-files",
 				"--ignored", "--exclude-standard", "-o" });
 		builder.directory(db.getWorkTree());
-		builder.environment().put("HOME", fs.userHome().getAbsolutePath());
 		ExecutionResult result = fs.execute(builder,
 				new ByteArrayInputStream(new byte[0]));
-		String errorOut = toString(result.getStderr());
-		assertEquals("External git failed", "exit 0\n",
-				"exit " + result.getRc() + '\n' + errorOut);
+		assertEquals("External git failed", 0, result.getRc());
+		assertEquals("External git reported errors", "",
+				toString(result.getStderr()));
 		try (BufferedReader r = new BufferedReader(new InputStreamReader(
 				new BufferedInputStream(result.getStdout().openInputStream()),
 				Constants.CHARSET))) {
