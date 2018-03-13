73220078557d7190153b3bfb54a5c6785d8c7347
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:34:20 +0000

    First commited as before_7882_rev3

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/JGitText.java
@@ -146,6 +146,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String commitAlreadyExists;
 	/***/ public String commitMessageNotSpecified;
 	/***/ public String commitOnRepoWithoutHEADCurrentlyNotSupported;
+	/***/ public String commitAmendOnInitialNotPossible;
 	/***/ public String compressingObjects;
 	/***/ public String connectionFailed;
 	/***/ public String connectionTimeOut;
@@ -437,6 +438,7 @@ public class JGitText extends TranslationBundle {
 	/***/ public String receivePackObjectTooLarge2;
 	/***/ public String receivingObjects;
 	/***/ public String refAlreadyExists;
+	/***/ public String refAlreadyExists1;
 	/***/ public String reflogEntryNotFound;
 	/***/ public String refNotResolved;
 	/***/ public String refUpdateReturnCodeWas;
