61076939ca0b1832dd415c4db7b1669e025168bb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:15:53 +0000

    First commited as before_35477_rev3

--- org.eclipse.jgit/src/org/eclipse/jgit/api/RebaseCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/RebaseCommand.java
@@ -461,7 +461,7 @@ public class RebaseCommand extends GitCommand<RebaseResult> {
 			String newMessage = interactiveHandler
 					.modifyCommitMessage(oldMessage);
 			newHead = new Git(repo).commit().setMessage(newMessage)
-					.setAmend(true).setNoVerify(true).call();
+					.setAmend(true).call();
 			return null;
 		case EDIT:
 			rebaseState.createFile(AMEND, commitToPick.name());
@@ -767,14 +767,15 @@ public class RebaseCommand extends GitCommand<RebaseResult> {
 			}
 			retNewHead = new Git(repo).commit()
 					.setMessage(stripCommentLines(commitMessage))
-					.setAmend(true).setNoVerify(true).call();
+					.setAmend(true).call();
 			rebaseState.getFile(MESSAGE_SQUASH).delete();
 			rebaseState.getFile(MESSAGE_FIXUP).delete();
 
 		} else {
 			// Next step is either Squash or Fixup
-			retNewHead = new Git(repo).commit().setMessage(commitMessage)
-					.setAmend(true).setNoVerify(true).call();
+			retNewHead = new Git(repo).commit()
+					.setMessage(commitMessage).setAmend(true)
+					.call();
 		}
 		return retNewHead;
 	}
