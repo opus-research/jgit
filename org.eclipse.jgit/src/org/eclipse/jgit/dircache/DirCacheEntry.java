fcdaba7d78d00fe61ce9ee87640af2b7414f61d8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:27:12 +0000

    First commited as before_5963_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheEntry.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/dircache/DirCacheEntry.java
@@ -141,7 +141,8 @@ public class DirCacheEntry {
 	private byte inCoreFlags;
 
 	DirCacheEntry(final byte[] sharedInfo, final MutableInteger infoAt,
-			final InputStream in, final MessageDigest md) throws IOException {
+			final InputStream in, final MessageDigest md, final int smudge_s,
+			final int smudge_ns) throws IOException {
 		info = sharedInfo;
 		infoOffset = infoAt.value;
 
@@ -199,6 +200,10 @@ public class DirCacheEntry {
 			IO.skipFully(in, padLen);
 			md.update(nullpad, 0, padLen);
 		}
+
+		if (mightBeRacilyClean(smudge_s, smudge_ns))
+			smudgeRacilyClean();
+
 	}
 
 	/**
@@ -719,8 +724,7 @@ public class DirCacheEntry {
 			case ':':
 				// Tree's never have a backslash in them, not even on Windows
 				// but even there we regard it as an invalid path
-				if ("Windows".equals(SystemReader.getInstance().getProperty(
-						"os.name")))
+				if (SystemReader.getInstance().isWindows())
 					return false;
 				//$FALL-THROUGH$
 			default:
