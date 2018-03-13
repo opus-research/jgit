8c228beb5101d6a323293274ee9d0fdbf3e7c33b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:24:58 +0000

    First commited as before_88603_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsPackFile.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsPackFile.java
@@ -499,7 +499,6 @@ public final class DfsPackFile {
 				rc.setReadAheadBytes(ctx.getOptions().getStreamPackBufferSize());
 			long position = 12;
 			long remaining = length - (12 + 20);
-			boolean packHeadSkipped = false;
 			while (0 < remaining) {
 				DfsBlock b = cache.get(key, alignToBlock(position));
 				if (b != null) {
@@ -509,7 +508,6 @@ public final class DfsPackFile {
 					position += n;
 					remaining -= n;
 					rc.position(position);
-					packHeadSkipped = true;
 					continue;
 				}
 
@@ -519,14 +517,7 @@ public final class DfsPackFile {
 					throw packfileIsTruncated();
 				else if (n > remaining)
 					n = (int) remaining;
-
-				if (!packHeadSkipped) {
-					// Need skip the 'PACK' header for the first read
-					out.write(buf.array(), 12, n - 12);
-					packHeadSkipped = true;
-				} else {
-					out.write(buf.array(), 0, n);
-				}
+				out.write(buf.array(), 0, n);
 				position += n;
 				remaining -= n;
 			}
