2f4d42189aa8a23318025030341a2cc760148d23
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:56:10 +0000

    First commited as before_15396_rev10

--- org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeIterator.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/treewalk/WorkingTreeIterator.java
@@ -889,9 +889,8 @@ public abstract class WorkingTreeIterator extends AbstractTreeIterator {
 			return false;
 		} else {
 			if (mode == FileMode.SYMLINK.getBits())
-				return !new File(FS.detect().normalize(
-						readContentAsString(current()))).equals(new File((FS
-						.detect().normalize(readContentAsString(entry)))));
+				return !new File(readContentAsString(current()))
+						.equals(new File(readContentAsString(entry)));
 			// Content differs: that's a real change!
 			return true;
 		}
