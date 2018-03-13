e4e50f920f3842b9c7c441f057a74ce849316743
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:51:27 +0000

    First commited as before_13307_rev2

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Archive.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Archive.java
@@ -46,7 +46,8 @@ package org.eclipse.jgit.pgm;
 import org.eclipse.jgit.api.ArchiveCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.archive.FormatActivator;
+import org.eclipse.jgit.archive.TarFormat;
+import org.eclipse.jgit.archive.ZipFormat;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.pgm.TextBuiltin;
 import org.eclipse.jgit.pgm.internal.CLIText;
@@ -56,7 +57,8 @@ import org.kohsuke.args4j.Option;
 @Command(common = true, usage = "usage_archive")
 class Archive extends TextBuiltin {
 	static {
-		FormatActivator.start();
+		ArchiveCommand.registerFormat("tar", new TarFormat());
+		ArchiveCommand.registerFormat("zip", new ZipFormat());
 	}
 
 	@Argument(index = 0, metaVar = "metaVar_treeish")
