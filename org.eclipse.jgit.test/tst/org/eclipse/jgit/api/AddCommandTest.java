4f775d71046920f5dc85639d6de5ea445d9cda7e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:31:55 +0000

    First commited as before_7116_rev1

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/api/AddCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/api/AddCommandTest.java
@@ -631,11 +631,6 @@ public class AddCommandTest extends RepositoryTestCase {
 			public boolean canExecute(File f) {
 				return true;
 			}
-
-			@Override
-			public boolean isCaseSensitive() {
-				return false;
-			}
 		};
 
 		Git git = Git.open(db.getDirectory(), executableFs);
@@ -674,11 +669,6 @@ public class AddCommandTest extends RepositoryTestCase {
 			}
 
 			public boolean canExecute(File f) {
-				return false;
-			}
-
-			@Override
-			public boolean isCaseSensitive() {
 				return false;
 			}
 		};
--- org.eclipse.jgit/src/org/eclipse/jgit/util/SystemReader.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/SystemReader.java
@@ -49,8 +49,6 @@ package org.eclipse.jgit.util;
 import java.io.File;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
@@ -243,12 +241,7 @@ public abstract class SystemReader {
 	 * @return true if we are running on Mac OS X
 	 */
 	public boolean isMacOS() {
-		String osDotName = AccessController
-				.doPrivileged(new PrivilegedAction<String>() {
-					public String run() {
-						return getProperty("os.name");
-					}
-				});
+		String osDotName = getProperty("os.name");
 		return "Mac OS X".equals(osDotName) || "Darwin".equals(osDotName);
 	}
 
