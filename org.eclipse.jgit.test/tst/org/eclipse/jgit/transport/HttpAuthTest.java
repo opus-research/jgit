b8dc3bd734eaaf7394193bd69da31ce55ae0ee16
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:01:46 +0000

    First commited as before_71028_rev9

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/HttpAuthTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/transport/HttpAuthTest.java
@@ -100,7 +100,7 @@ public class HttpAuthTest {
 		} catch (IOException e) {
 			fail("Couldn't instantiate AuthHeadersResponse: " + e.toString());
 		}
-		HttpAuthMethod authMethod = HttpAuthMethod.scanResponse(response, null);
+		HttpAuthMethod authMethod = HttpAuthMethod.scanResponse(response);
 
 		if (!expectedAuthMethod.equals(getAuthMethodName(authMethod))) {
 			fail("Wrong authentication method: expected " + expectedAuthMethod
