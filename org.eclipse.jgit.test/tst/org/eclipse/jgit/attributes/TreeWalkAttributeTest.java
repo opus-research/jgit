775bb36df7e0f59dc22c5204e216e59e53050223
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:46:57 +0000

    First commited as before_60601_rev4

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/TreeWalkAttributeTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/TreeWalkAttributeTest.java
@@ -802,9 +802,8 @@ public class TreeWalkAttributeTest extends RepositoryTestCase {
 		assertEquals(type, walk.getFileMode(0));
 
 		assertEquals(checkinAttributes,
-				asSet(ci_walk.getAttributes().getAttributes()));
-		assertEquals(checkoutAttributes,
-				asSet(walk.getAttributes().getAttributes()));
+				asSet(ci_walk.getAttributes().values()));
+		assertEquals(checkoutAttributes, asSet(walk.getAttributes().values()));
 
 		if (D.equals(type)) {
 			walk.enterSubtree();
