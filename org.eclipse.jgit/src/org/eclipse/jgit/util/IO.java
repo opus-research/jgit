e6d5f75b4e2e867d24d760847f440b1bfc03c7d8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:23:59 +0000

    First commited as before_5276_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/util/IO.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/util/IO.java
@@ -137,18 +137,12 @@ public class IO {
 			throws FileNotFoundException, IOException {
 		final FileInputStream in = new FileInputStream(path);
 		try {
-			final long sz = path.length();
+			final long sz = in.getChannel().size();
 			if (sz > max)
 				throw new IOException(MessageFormat.format(
 						JGitText.get().fileIsTooLarge, path));
 			final byte[] buf = new byte[(int) sz];
-			int actSz = IO.readFully(in, buf, 0);
-
-			if (actSz == sz) {
-				byte[] ret = new byte[actSz];
-				System.arraycopy(buf, 0, ret, 0, actSz);
-				return ret;
-			}
+			IO.readFully(in, buf, 0);
 			return buf;
 		} finally {
 			try {
