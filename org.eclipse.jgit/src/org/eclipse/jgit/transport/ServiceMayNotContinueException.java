752210cab195a62a556a0b355b9fa26735b74124
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:58:01 +0000

    First commited as before_17145_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/ServiceMayNotContinueException.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/ServiceMayNotContinueException.java
@@ -90,8 +90,7 @@ public class ServiceMayNotContinueException extends IOException {
 	 *            the cause of the exception.
 	 */
 	public ServiceMayNotContinueException(Throwable cause) {
-		super(JGitText.get().internalServerError);
-		initCause(cause);
+		this(JGitText.get().internalServerError, cause);
 	}
 
 	/** @return true if the message was already output to the client. */
