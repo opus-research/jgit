ca5a53304e41adbed493395375e704cd031ca154
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 14:42:27 +0000

    First commited as before_57925_rev2

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/RepoCommandTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/gitrepo/RepoCommandTest.java
@@ -722,23 +722,18 @@ public class RepoCommandTest extends RepositoryTestCase {
 				.call();
 			// Clone it
 			File directory = createTempDirectory("testBareRepo");
-			try (Repository localDb = Git.cloneRepository()
-					.setDirectory(directory)
+			Repository localDb = Git.cloneRepository().setDirectory(directory)
 					.setURI(remoteDb.getDirectory().toURI().toString()).call()
-					.getRepository();) {
-				// The .gitmodules file should exist
-				File gitmodules = new File(localDb.getWorkTree(),
-						".gitmodules");
-				assertTrue("The .gitmodules file should exist",
-						gitmodules.exists());
-				FileBasedConfig c = new FileBasedConfig(gitmodules,
-						FS.DETECTED);
-				c.load();
-				assertEquals("standard branches work", "master",
-						c.getString("submodule", "with-branch", "branch"));
-				assertEquals("long branches work", "refs/heads/master",
-						c.getString("submodule", "with-long-branch", "branch"));
-			}
+					.getRepository();
+			// The .gitmodules file should exist
+			File gitmodules = new File(localDb.getWorkTree(), ".gitmodules");
+			assertTrue("The .gitmodules file should exist", gitmodules.exists());
+			FileBasedConfig c = new FileBasedConfig(gitmodules, FS.DETECTED);
+			c.load();
+			assertEquals("standard branches work", "master",
+				c.getString("submodule", "with-branch", "branch"));
+			assertEquals("long branches work", "refs/heads/master",
+				c.getString("submodule", "with-long-branch", "branch"));
 		}
 	}
 
