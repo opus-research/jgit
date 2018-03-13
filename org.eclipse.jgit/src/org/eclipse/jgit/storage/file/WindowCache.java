4fe11ff1c8a023ed5ecb63e8d30227561421fde9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:07:18 +0000

    First commited as before_2629_rev1

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/WindowCache.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/WindowCache.java
@@ -313,13 +313,14 @@ public class WindowCache {
 				return pack.mmap(offset, windowSize);
 			return pack.read(offset, windowSize);
 		} catch (IOException e) {
+			close(pack);
 			throw e;
 		} catch (RuntimeException e) {
+			close(pack);
 			throw e;
 		} catch (Error e) {
-			throw e;
-		} finally {
 			close(pack);
+			throw e;
 		}
 	}
 
