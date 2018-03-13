c2aa2dbf906b65fb101a812e8923f3858845f835
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:38:40 +0000

    First commited as before_97404_rev5

--- org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsGarbageCollector.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/dfs/DfsGarbageCollector.java
@@ -53,7 +53,6 @@ import static org.eclipse.jgit.internal.storage.dfs.DfsObjDatabase.PackSource.UN
 import static org.eclipse.jgit.internal.storage.pack.PackExt.BITMAP_INDEX;
 import static org.eclipse.jgit.internal.storage.pack.PackExt.INDEX;
 import static org.eclipse.jgit.internal.storage.pack.PackExt.PACK;
-import static org.eclipse.jgit.internal.storage.pack.PackWriter.NONE;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -112,8 +111,7 @@ public class DfsGarbageCollector {
 	private List<DfsPackFile> packsBefore;
 	private List<DfsPackFile> expiredGarbagePacks;
 
-	private Set<ObjectId> allHeadsAndTags;
-	private Set<ObjectId> allTags;
+	private Set<ObjectId> allHeads;
 	private Set<ObjectId> nonHeads;
 	private Set<ObjectId> txnHeads;
 	private Set<ObjectId> tagTargets;
@@ -243,36 +241,23 @@ public class DfsGarbageCollector {
 			Collection<Ref> refsBefore = getAllRefs();
 			readPacksBefore();
 
-			Set<ObjectId> allHeads = new HashSet<>();
-			allHeadsAndTags = new HashSet<>();
-			allTags = new HashSet<>();
+			allHeads = new HashSet<>();
 			nonHeads = new HashSet<>();
 			txnHeads = new HashSet<>();
 			tagTargets = new HashSet<>();
 			for (Ref ref : refsBefore) {
-				if (ref.isSymbolic() || ref.getObjectId() == null) {
+				if (ref.isSymbolic() || ref.getObjectId() == null)
 					continue;
-				}
-				if (isHead(ref)) {
+				if (isHead(ref) || isTag(ref))
 					allHeads.add(ref.getObjectId());
-				} else if (isTag(ref)) {
-					allTags.add(ref.getObjectId());
-				} else if (RefTreeNames.isRefTree(refdb, ref.getName())) {
+				else if (RefTreeNames.isRefTree(refdb, ref.getName()))
 					txnHeads.add(ref.getObjectId());
-				} else {
+				else
 					nonHeads.add(ref.getObjectId());
-				}
-				if (ref.getPeeledObjectId() != null) {
+				if (ref.getPeeledObjectId() != null)
 					tagTargets.add(ref.getPeeledObjectId());
-				}
 			}
-			// Don't exclude tags that are also branch tips.
-			allTags.removeAll(allHeads);
-			allHeadsAndTags.addAll(allHeads);
-			allHeadsAndTags.addAll(allTags);
-
-			// Hoist all branch tips and tags earlier in the pack file
-			tagTargets.addAll(allHeadsAndTags);
+			tagTargets.addAll(allHeads);
 
 			boolean rollback = true;
 			try {
@@ -428,12 +413,12 @@ public class DfsGarbageCollector {
 	}
 
 	private void packHeads(ProgressMonitor pm) throws IOException {
-		if (allHeadsAndTags.isEmpty())
+		if (allHeads.isEmpty())
 			return;
 
 		try (PackWriter pw = newPackWriter()) {
 			pw.setTagTargets(tagTargets);
-			pw.preparePack(pm, allHeadsAndTags, NONE, NONE, allTags);
+			pw.preparePack(pm, allHeads, PackWriter.NONE);
 			if (0 < pw.getObjectCount())
 				writePack(GC, pw, pm,
 						estimateGcPackSize(INSERT, RECEIVE, COMPACT, GC));
@@ -447,7 +432,7 @@ public class DfsGarbageCollector {
 		try (PackWriter pw = newPackWriter()) {
 			for (ObjectIdSet packedObjs : newPackObj)
 				pw.excludeObjects(packedObjs);
-			pw.preparePack(pm, nonHeads, allHeadsAndTags);
+			pw.preparePack(pm, nonHeads, allHeads);
 			if (0 < pw.getObjectCount())
 				writePack(GC_REST, pw, pm,
 						estimateGcPackSize(INSERT, RECEIVE, COMPACT, GC_REST));
@@ -461,7 +446,7 @@ public class DfsGarbageCollector {
 		try (PackWriter pw = newPackWriter()) {
 			for (ObjectIdSet packedObjs : newPackObj)
 				pw.excludeObjects(packedObjs);
-			pw.preparePack(pm, txnHeads, NONE);
+			pw.preparePack(pm, txnHeads, PackWriter.NONE);
 			if (0 < pw.getObjectCount())
 				writePack(GC_TXN, pw, pm, 0 /* unknown pack size */);
 		}
