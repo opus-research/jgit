c47df3f5772aa3a34daa6885988cd89d990f3992
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 15:20:57 +0000

    First commited as before_84934_rev1

--- org.eclipse.jgit.lfs.server/src/org/eclipse/jgit/lfs/server/fs/FileLfsServlet.java
+++ org.eclipse.jgit.lfs.server/src/org/eclipse/jgit/lfs/server/fs/FileLfsServlet.java
@@ -173,19 +173,7 @@ public class FileLfsServlet extends HttpServlet {
 		}
 	}
 
-	/**
-	 * Send an error response.
-	 *
-	 * @param rsp
-	 *            the servlet response
-	 * @param status
-	 *            HTTP status code
-	 * @param message
-	 *            error message
-	 * @throws IOException
-	 *             on failure to send the response
-	 */
-	protected static void sendError(HttpServletResponse rsp, int status, String message)
+	static void sendError(HttpServletResponse rsp, int status, String message)
 			throws IOException {
 		rsp.setStatus(status);
 		PrintWriter writer = rsp.getWriter();
