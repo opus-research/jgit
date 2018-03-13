01540b7d9ed425a61fb87dac20f1a8047f4d3473
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:58:56 +0000

    First commited as before_17949_rev5

--- org.eclipse.jgit/src/org/eclipse/jgit/api/ArchiveCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/ArchiveCommand.java
@@ -138,7 +138,7 @@ public class ArchiveCommand extends GitCommand<OutputStream> {
 		 *            archive object from createArchiveOutputStream
 		 * @param path
 		 *            full filename relative to the root of the archive
-                 *            (with trailing '/' for directories)
+		 *            (with trailing '/' for directories)
 		 * @param mode
 		 *            mode (for example FileMode.REGULAR_FILE or
 		 *            FileMode.SYMLINK)
@@ -279,18 +279,11 @@ public class ArchiveCommand extends GitCommand<OutputStream> {
 				walk.reset(rw.parseTree(tree));
 				while (walk.next()) {
 					final String name = pfx + walk.getPathString();
-					FileMode mode = walk.getFileMode(0);
+					final FileMode mode = walk.getFileMode(0);
 
-					if (walk.isSubtree())
-						walk.enterSubtree();
-
-					if (FileMode.GITLINK.equals(mode))
-						// TODO(jrn): Take a callback to recurse
-						// into submodules.
-						mode = FileMode.TREE;
-
-					if (FileMode.TREE.equals(mode)) {
+					if (walk.isSubtree()) {
 						fmt.putEntry(outa, name + "/", mode, null);
+						walk.enterSubtree();
 						continue;
 					}
 					walk.getObjectId(idBuf, 0);
