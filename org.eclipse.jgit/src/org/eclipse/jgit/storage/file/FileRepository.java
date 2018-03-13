b84b00792742be69589ce06fbc691d33048206e0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 13:14:29 +0000

    First commited as before_4218_rev2

--- org.eclipse.jgit/src/org/eclipse/jgit/storage/file/FileRepository.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/storage/file/FileRepository.java
@@ -56,7 +56,6 @@ import org.eclipse.jgit.JGitText;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.events.ConfigChangedEvent;
 import org.eclipse.jgit.events.ConfigChangedListener;
-import org.eclipse.jgit.events.IndexChangedEvent;
 import org.eclipse.jgit.lib.BaseRepositoryBuilder;
 import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
@@ -105,8 +104,6 @@ public class FileRepository extends Repository {
 	private final RefDatabase refs;
 
 	private final ObjectDirectory objectDatabase;
-
-	private FileSnapshot snapshot;
 
 	/**
 	 * Construct a representation of a Git repository.
@@ -190,8 +187,6 @@ public class FileRepository extends Repository {
 						repositoryFormatVersion));
 			}
 		}
-
-		snapshot = FileSnapshot.save(getIndexFile());
 	}
 
 	private void loadSystemConfig() throws IOException {
@@ -376,26 +371,15 @@ public class FileRepository extends Repository {
 		objectDatabase.openPack(pack, idx);
 	}
 
-	@Override
+	/**
+	 * Force a scan for changed refs.
+	 *
+	 * @throws IOException
+	 */
 	public void scanForRepoChanges() throws IOException {
 		getAllRefs(); // This will look for changes to refs
-		detectIndexChanges();
-	}
-
-	/**
-	 * Detect index changes.
-	 */
-	private void detectIndexChanges() {
-		if (isBare())
-			return;
-
-		File indexFile = getIndexFile();
-		if (snapshot == null)
-			snapshot = FileSnapshot.save(indexFile);
-		else if (snapshot.isModified(indexFile)) {
-			snapshot = FileSnapshot.save(indexFile);
-			fireEvent(new IndexChangedEvent());
-		}
+		if (!isBare())
+			getIndex(); // This will detect changes in the index
 	}
 
 	/**
